351dfbdc0e2401b660f96c84f9766136c5fb7c06
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-17 23:21:08 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_069_C.py
+++ pgm/ABC_Python/ABC_069_C.py
@@ -1,21 +1,16 @@
 N = int(input())
-an = map(int, input().split())
+an = [int(i) % 4 for i in input().split()]
 
 
 def main():
-    ar = [0] * 3
-    for ai in an:
-        if ai % 4 == 0:
-            ar[2] += 1
-        elif ai % 2 == 0:
-            ar[1] += 1
-        else:
-            ar[0] += 1
+    two = an.count(2)
+    four = an.count(0)
+    other = N - two - four
 
-    if 0 < ar[1]:
-        ar[0] += 1
+    if 0 < two:
+        other += 1
 
-    print("Yes" if ar[0] - 1 <= ar[2] else "No")
+    print("Yes" if other - 1 <= four else "No")
     return
 
 
