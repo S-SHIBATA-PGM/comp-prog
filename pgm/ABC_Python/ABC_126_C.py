00eac48676f0f2fadf2a2e75aad0548c6ec83fd7
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-17 22:33:51 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_126_C.py
+++ pgm/ABC_Python/ABC_126_C.py
@@ -1,12 +1,20 @@
-import math
+from functools import reduce
+from math import log2
+from operator import mul
+
+
+def end_of_loop():
+    raise StopIteration
 
 
 def main():
     N, K = map(int, input().split())
-    print((max(0, N - K + 1) + sum(
-        max(0, N - (math.ceil(K * (1 / 2) ** i)) + 1) * (1 / 2) ** i
-        - max(0, N - (math.ceil(K * (1 / 2) ** (i - 1))) + 1) * (1 / 2) ** i
-        for i in range(1, int(math.log2(K)) + 1 + 1))) / N)
+    MAX_SHIFT = int(log2(K) + 1)
+    print(sum(reduce(mul, (0.5
+                           if i << j < K
+                           else end_of_loop()
+                           for j in range(MAX_SHIFT)), 1)
+              for i in range(1, N + 1)) / N)
     return
 
 
