9b4fa45abc0911e9928655b45829547ceaa66e76
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-17 22:51:56 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_142_C.py
+++ pgm/ABC_Python/ABC_142_C.py
@@ -1,10 +1,9 @@
-from operator import itemgetter
-
-
 def main():
-    A = {k + 1: v for k, v in enumerate(map(int, open(0).read().split()[1:]))}
-    # value 昇順
-    print(" ".join([str(i[0]) for i in sorted(A.items(), key=itemgetter(1))]))
+    N, *A = map(int, open(0).read().split())
+    ans = [0] * N
+    for k, v in enumerate(A, 1):
+        ans[v - 1] = k
+    print(" ".join(map(str, ans)))
     return
 
 
