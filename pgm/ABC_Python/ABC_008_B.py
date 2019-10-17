0934bd5559ac9a2604d3cd6e9d37b507e5118c5b
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-17 22:59:15 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_008_B.py
+++ pgm/ABC_Python/ABC_008_B.py
@@ -6,6 +6,6 @@ S = defaultdict(lambda: 0)
 for i in range(N):
     S[input()] += 1
 
-for k, v in sorted(S.items(), key=lambda x: -x[1]):
-    print(k)
-    break
+# value 降順
+S = sorted(S.items(), key=lambda x: -x[1])
+print(S[0][0])
