c45884368ac9a9d2bf98b7b89e21039a46073ad0
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 06:40:07 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_075_C.py
+++ pgm/ABC_Python/ABC_075_C.py
@@ -1,11 +1,3 @@
-N, M = map(int, input().split())
-
-graph = [[False for j in range(N)] for i in range(N)]
-
-am = [0] * M
-bm = [0] * M
-
-
 def judge(visited):
     for v in visited:
         if not v:
@@ -15,22 +7,28 @@ def judge(visited):
         return False
 
 
-def dfs(visited, n):
-    visited[n] = True
-
-    for k, v in enumerate(graph[n]):
-        if not v:
-            continue
-
-        if visited[k]:
-            continue
-
-        visited = dfs(visited, k)
-
-    return visited
-
-
 def main():
+    def dfs(visited, n):
+        visited[n] = True
+
+        for k, v in enumerate(graph[n]):
+            if not v:
+                continue
+
+            if visited[k]:
+                continue
+
+            visited = dfs(visited, k)
+
+        return visited
+
+    N, M = map(int, input().split())
+
+    graph = [[False for j in range(N)] for i in range(N)]
+
+    am = [0] * M
+    bm = [0] * M
+
     for i in range(M):
         # 0-indexed
         ai, bi = map(int, input().split())
@@ -50,9 +48,7 @@ def main():
         graph[bi][ai] = False
 
         # 訪問済 All False
-        visited = dfs([False] * N, 0)
-
-        if judge(visited):
+        if judge(dfs([False] * N, 0)):
             ans += 1
 
         # 元に戻す
