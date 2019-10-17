65afa40d8f8a70c7ca6e64a227a3596027f62101
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-17 23:04:48 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_018_B.py
+++ pgm/ABC_Python/ABC_018_B.py
@@ -4,19 +4,13 @@ N = int(input())
 for i in range(N):
     l, r = map(int, input().split())
 
-    T = S[l - 1:r]
-
-    for j in range(len(T)):
+    for j in range(l - 1, r):
         f = j
-        t = len(T) - 1 - j
+        t = r - 1 - (j - (l - 1))
 
         if t <= f:
             break
 
-        T[f], T[t] = T[t], T[f]
-
-    T.extend(S[r:])
-    S = S[:l - 1]
-    S.extend(T)
+        S[f], S[t] = S[t], S[f]
 
 print(''.join(S))
