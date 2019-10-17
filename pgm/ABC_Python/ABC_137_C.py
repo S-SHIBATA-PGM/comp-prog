f96844199f22f46c96f6b86ef94ddd51cdce7cea
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-17 22:44:56 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_137_C.py
+++ pgm/ABC_Python/ABC_137_C.py
@@ -1,16 +1,14 @@
-from collections import Counter
-# バージョン0.14.0まで scipy.misc.comb()
-# バージョン0.14.0から scipy.special.comb()
-from scipy.misc import comb
-# from scipy.special import comb
+from collections import defaultdict
 
 
 def main():
-    s = Counter(["".join(sorted(i)) for i in open(0).read().split()[1:]])
+    s = ["".join(sorted(i)) for i in open(0).read().split()[1:]]
+    d = defaultdict(lambda: 0)
     ans = 0
-    for v in s.values():
-        ans += comb(v, 2, exact=True)
-    print(int(ans))
+    for i in s:
+        ans += d[i]
+        d[i] += 1
+    print(ans)
     return
 
 
