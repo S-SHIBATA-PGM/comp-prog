c04005c54fd31fad6f436eb1134a675610316fd7
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:21:42 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_109_C.py
+++ pgm/ABC_Python/ABC_109_C.py
@@ -1,46 +1,11 @@
 from sys import stdin
+from functools import reduce
+# バージョン 3.4.X 以前
+import fractions
+# バージョン 3.4.X より 後
+# import math
 
 input = stdin.readline
-
-
-def gcd(a, b):
-    result = a
-    k = 0
-    n = b
-
-    while True:
-        k = result % n
-        result = n
-        n = k
-        if k == 0:
-            break
-
-    return result
-
-
-def gcd_n(numbers):
-    l = numbers[0]
-
-    for i in range(len(numbers)):
-        l = gcd(l, numbers[i])
-
-    return l
-
-
-def lcm(a, b):
-    g = gcd(a, b)
-
-    return a * (b / g)
-
-
-def lcm_n(numbers):
-    l = numbers[0]
-
-    for i in range(len(numbers)):
-        l = lcm(l, numbers[i])
-
-    return l
-
 
 N, X = map(int, input().split())
 
@@ -48,7 +13,7 @@ xn = [abs(X - int(i)) for i in input().split()]
 
 
 def main():
-    print(gcd_n(xn))
+    print(reduce(fractions.gcd, xn))
     return
 
 
