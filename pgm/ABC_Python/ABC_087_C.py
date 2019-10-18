f11d8fe70f84519034c088d5d2b4c5f8f42810af
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 22:49:38 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_087_C.py
+++ pgm/ABC_Python/ABC_087_C.py
@@ -7,14 +7,11 @@ def main():
 
     for i in range(N - 1):
         A1[i + 1] += A1[i]
-        A2[i + 1] += A2[i]
 
-    ans = -1
+    for i in range(N - 1, 0, -1):
+        A2[i - 1] += A2[i]
 
-    for i in range(N):
-        ans = max(ans, A1[i] + (A2[-1] if i == 0 else A2[-1] - A2[i - 1]))
-
-    print(ans)
+    print(max([A1[i] + A2[i] for i in range(N)]))
 
     return
 
