09c839495d9319ec43b575213048a12b264315ea
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 22:34:22 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_080_C.py
+++ pgm/ABC_Python/ABC_080_C.py
@@ -2,24 +2,27 @@ MAX_TIME = 10
 
 N = int(input())
 
-F = [int(input().replace(" ", ""), 2) for i in range(N)]
+# F[店のインデックス]
+# 他の店
+# 選択する時間帯を2進法で表現したもの
+F = [sum(v << MAX_TIME - k - 1 for k, v
+         in enumerate(map(int, input().split())))
+     for i in range(N)]
 
+# P[店のインデックス][合致した時間帯の数]
+# 利益
 P = [[int(j) for j in input().split()] for i in range(N)]
 
 
 def main():
-    # 1 から 2 ** MAX_TIME - 1
-    # 営業時間帯 最低でも ひとつの時間帯
-    # 選択する時間帯を2進法で表現したもの
+    # 予めビットをカウントしておく
+    pattern = [sum(i >> j & 1 for j in range(MAX_TIME))
+               for i in range(2 ** MAX_TIME)]
 
-    # F[店のインデックス]
-    # 他の店
-    # 選択する時間帯を2進法で表現したもの
-
-    # P[店のインデックス][合致した時間帯の数]
-    # 利益
+    # 営業時間帯 すべて試す
+    # 最低でも ひとつの時間帯
     print(max(sum(
-        [p[bin(f & i).count("1")] for f, p in zip(F, P)])
+        P[j][pattern[F[j] & i]] for j in range(N))
         for i in range(1, 2 ** MAX_TIME)))
 
     return
