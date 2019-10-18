e4f1ad79a5378e6e808c2278d89d1be99942fc2c
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:43:34 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_114_C.py
+++ pgm/ABC_Python/ABC_114_C.py
@@ -1,9 +1,11 @@
 class Main:
+    N = None
+
     def __init__(self):
-        self.N = int(input())
+        Main.N = int(input())
 
     def dfs(self, ret, cur, f1, f2, f3):
-        if self.N < cur:
+        if Main.N < cur:
             return ret
 
         ret += 1 if f1 and f2 and f3 else 0
