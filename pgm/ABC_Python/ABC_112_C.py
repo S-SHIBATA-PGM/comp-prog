ead64ab31734d88080f7267f10833c7ca16d1550
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:30:37 +0900

    Arrangement.

---
+++ pgm/ABC_Python/ABC_112_C.py
@@ -0,0 +1,47 @@
+N = int(input())
+
+xn = [0] * N
+yn = [0] * N
+hn = [0] * N
+
+for i in range(N):
+    xi, yi, hi = map(int, input().split())
+    xn[i] = xi
+    yn[i] = yi
+    hn[i] = hi
+
+for Cx in range(101):
+    flg = True
+
+    for Cy in range(101):
+
+        for i in range(N):
+            if 0 < hn[i]:
+                h = abs(xn[i] - Cx) + abs(yn[i] - Cy) + hn[i]
+
+                if flg:
+                    H = h
+                    flg = False
+                else:
+                    if (H != h):
+                        flg = True
+                        break
+
+        if flg:
+            continue
+
+        for i in range(N):
+            if 0 == hn[i]:
+                h = abs(xn[i] - Cx) + abs(yn[i] - Cy)
+                if h < H:
+                    flg = True
+                    break
+
+        if flg:
+            continue
+
+        print(Cx, Cy, H)
+        break
+
+    if not flg:
+        break
