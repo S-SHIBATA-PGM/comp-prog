070c43f74f4f897f39c85b5cce997ed491486fd9
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:04:26 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_090_C.py
+++ pgm/ABC_Python/ABC_090_C.py
@@ -1,11 +1,13 @@
-N, M = map(int, input().split())
-
-
 def main():
-    if N - 2 < 0 or M - 2 < 0:
-        print(max(1, N - 2) * max(1, M - 2))
-    else:
-        print((N - 2) * (M - 2))
+    N, M = map(int, input().split())
+
+    if 1 < N:
+        N -= 2
+
+    if 1 < M:
+        M -= 2
+
+    print(N * M)
 
     return
 
