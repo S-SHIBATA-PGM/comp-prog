cc53f8e2cd0bee00d48e9d5f6fa18b860b430952
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:13:45 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_101_C.py
+++ pgm/ABC_Python/ABC_101_C.py
@@ -1,12 +1,11 @@
-import math
-
 input = open(0).read
 
 N, K, *An = map(int, input().split())
 
 
 def main():
-    print(math.ceil((N - 1) / (K - 1)))
+    num = (N - 1) / (K - 1)
+    print(int(num) + 1 if num != int(num) else int(num))
     return
 
 
