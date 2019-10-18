113dd8a9fc2430c86244ce81b61cb4842c2ab8c5
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 22:13:05 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_075_C.py
+++ pgm/ABC_Python/ABC_075_C.py
@@ -3,20 +3,18 @@ N, M = map(int, input().split())
 graph = [[] for i in range(N)]
 
 for i in range(M):
-    ai, bi = map(int, input().split())
-    ai -= 1
-    bi -= 1
+    ai, bi = map(lambda x: int(x) - 1, input().split())
     graph[ai].append(bi)
     graph[bi].append(ai)
 
-rank = [N] * N
 low = [N] * N
+rank = [N] * N
 visited = [False] * N
 
 
 def dfs(k, pos, parent):
-    rank[pos] = k
     low[pos] = k
+    rank[pos] = k
 
     k += 1
 
@@ -35,14 +33,9 @@ def dfs(k, pos, parent):
 def main():
     dfs(0, 0, -1)
 
-    ans = 0
+    print(sum(
+        [1 for i in range(N) for j in graph[i] if rank[j] < low[i]]))
 
-    for i in range(N):
-        for j in graph[i]:
-            if rank[j] < low[i]:
-                ans += 1
-
-    print(ans)
     return
 
 
