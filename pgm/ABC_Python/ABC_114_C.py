6b9b64a2ac682bab7acba1b3b9a1061333ed2115
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:42:11 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_114_C.py
+++ pgm/ABC_Python/ABC_114_C.py
@@ -1,22 +1,16 @@
-def f(n, f1, f2, f3):
-    ret = 0
-
-    def dfs(n, f1, f2, f3):
-        if N < n:
-            return 0
-
-        nonlocal ret
-        ret += 1 if f1 and f2 and f3 else 0
-
-        dfs(n * 10 + 7, True, f2, f3)
-        dfs(n * 10 + 5, f1, True, f3)
-        dfs(n * 10 + 3, f1, f2, True)
-
+def dfs(ret, cur, f1, f2, f3):
+    if N < cur:
         return ret
 
-    return dfs(n, f1, f2, f3)
+    ret += 1 if f1 and f2 and f3 else 0
+
+    ret = dfs(ret, cur * 10 + 7, True, f2, f3)
+    ret = dfs(ret, cur * 10 + 5, f1, True, f3)
+    ret = dfs(ret, cur * 10 + 3, f1, f2, True)
+
+    return ret
 
 
 N = int(input())
 
-print(f(0, False, False, False))
+print(dfs(0, 0, False, False, False))
