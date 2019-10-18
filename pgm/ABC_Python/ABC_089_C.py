bbcfaaebf94f9a15a8b0e0275c88e242ae6ab78c
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 22:52:40 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_089_C.py
+++ pgm/ABC_Python/ABC_089_C.py
@@ -3,14 +3,13 @@ from itertools import combinations
 input = open(0).read
 
 Sn = [i[0] for i in input().split() if i[0] in "MARCH"]
-st = set(Sn)
 
 
 def main():
 
     print(
         sum(x * y * z for x, y, z
-            in combinations([Sn.count(s) for s in st], 3)))
+            in combinations([Sn.count(s) for s in "MARCH"], 3)))
 
     return
 
