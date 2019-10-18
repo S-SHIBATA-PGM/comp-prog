840f623603461cb2740a11f8609d93f9f42ca184
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:09:08 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_092_C.py
+++ pgm/ABC_Python/ABC_092_C.py
@@ -1,22 +1,12 @@
-input = open(0).read
-
-N, *A = [int(i) for i in input().split()]
-
-
 def main():
-    l = [abs(A[i + 1] - A[i]) for i in range(N - 1)]
-
+    N = int(input())
+    A = [0] + [int(i) for i in input().split()] + [0]
+    l = [abs(A[i + 1] - A[i]) for i in range(N + 1)]
     num = sum(l)
+    print("\n".join(
+        [str(num - l[i] - l[i + 1] + abs(A[i] - A[i + 2]))
+         for i in range(N)]))
 
-    print(num - l[0] + abs(A[1]) + abs(A[-1]))
-
-    if N != 2:
-        print("\n".join(
-            [str(num - l[i] - l[i + 1]
-                 + abs(A[i] - A[i + 2])
-                 + abs(A[0]) + abs(A[-1])) for i in range(N - 2)]))
-
-    print(num - l[-1] + abs(A[0]) + abs(A[-2]))
     return
 
 
