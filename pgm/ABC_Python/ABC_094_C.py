8bd9ebb20f767c64ebb43211fd6f4445de7e3ab6
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:10:58 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_094_C.py
+++ pgm/ABC_Python/ABC_094_C.py
@@ -1,13 +1,10 @@
-import copy
-
 input = open(0).read
 
 N, *X = map(int, input().split())
 
 
 def main():
-    a = copy.copy(X)
-    l, r = sorted(a)[N // 2 - 1:N // 2 + 1]
+    l, r = sorted(X)[N // 2 - 1:N // 2 + 1]
 
     print('\n'.join([str(r) if x < r else str(l) for x in X]))
     return
