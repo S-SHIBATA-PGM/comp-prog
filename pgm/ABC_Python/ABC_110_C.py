2e1b10c0ac626e9dc1f56a58ca7797f89be4075f
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:27:04 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_110_C.py
+++ pgm/ABC_Python/ABC_110_C.py
@@ -1,21 +1,26 @@
-# aacd
-# cdaa
-# Yes‚É‚È‚é
-
-from sys import stdin
-from collections import Counter
-
-input = stdin.readline
-
-S = list(input())
-T = list(input())
+input = open(0).read
+S, T = input().split()
 
 
 def main():
-    C1 = sorted(Counter(S).values())
-    C2 = sorted(Counter(T).values())
+    C1 = [None] * (ord('z') - ord('a') + 1)
+    C2 = [None] * (ord('z') - ord('a') + 1)
 
-    print("Yes" if C1 == C2 else "No")
+    flg = True
+
+    for i in range(len(S)):
+        c1 = ord(S[i]) - ord('a')
+        c2 = ord(T[i]) - ord('a')
+
+        if C1[c1] or C2[c2]:
+            if c1 != C2[c2] or c2 != C1[c1]:
+                flg = False
+                break
+
+        C1[c1] = c2
+        C2[c2] = c1
+
+    print("Yes" if flg else "No")
     return
 
 
