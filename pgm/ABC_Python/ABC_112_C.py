0d53761eb974d64cdd794e946e899e4c34664861
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:33:55 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_112_C.py
+++ pgm/ABC_Python/ABC_112_C.py
@@ -1,25 +1,24 @@
-def main():
-    N = int(input())
+N = int(input())
 
-    xn = []
-    yn = []
-    hn = []
+xn = []
+yn = []
+hn = []
 
-    for i in range(N):
-        xi, yi, hi = map(int, input().split())
-        if hi:
-            xn.append(xi)
-            yn.append(yi)
-            hn.append(hi)
+for i in range(N):
+    xi, yi, hi = map(int, input().split())
+    if hi:
+        xn.append(xi)
+        yn.append(yi)
+        hn.append(hi)
 
-    if len(xn) == 1:
-        print(xn[0], yn[0], hn[0])
-        return
-
+if len(xn) == 1:
+    print(xn[0], yn[0], hn[0])
+else:
     for Cx in range(101):
         for Cy in range(101):
 
             last = None
+            flg = False
 
             for i in range(len(xn)):
                 H = abs(xn[i] - Cx) + abs(yn[i] - Cy) + hn[i]
@@ -27,13 +26,14 @@ def main():
                 if H < 0:
                     continue
                 if last:
-                    if (last != H):
+                    if (H != last):
                         break
 
                 last = H
             else:
+                flg = True
                 print(Cx, Cy, H)
-                return
+                break
 
-
-main()
+        if flg:
+            break
