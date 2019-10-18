784e5d83f1ddcef67f1a3d6ad9f04936e764340f
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 22:27:58 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_079_C.py
+++ pgm/ABC_Python/ABC_079_C.py
@@ -1,27 +1,22 @@
+from itertools import product
+
 MAX_ITEM = 4
 NUM = 7
+OPS = ("+", "-")
 
-ABCD = [int(i) for i in list(input())]
+ABCD = list(input())
 
 
 def main():
-    for i in range(2 ** (MAX_ITEM - 1)):
-        ans = ABCD[0]
+    for p in product(OPS, repeat=3):
+        eq = ABCD[0]
 
-        for j in range(MAX_ITEM - 2, -1, -1):
-            if i >> j & 1:
-                ans += ABCD[j + 1]
+        for o, abcd in zip(p, ABCD[1:]):
+            eq += o
+            eq += abcd
 
-            else:
-                ans -= ABCD[j + 1]
-
-        if NUM == ans:
-            print("".join(
-                ["+" if k % 2 and i >> k // 2 & 1 else
-                 "-" if k % 2 else
-                 str(ABCD[k // 2]) for k in range(2 * MAX_ITEM - 1)])
-                + "=" + str(NUM))
-
+        if eval(eq) == NUM:
+            print(eq + "=" + str(NUM))
             break
 
     return
