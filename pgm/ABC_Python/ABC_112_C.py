715a8ac827e5bb54fe9d2e3ecfd350872d1942b7
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:35:39 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_112_C.py
+++ pgm/ABC_Python/ABC_112_C.py
@@ -1,56 +1,54 @@
-def main():
-    N = int(input())
+import sys
 
-    xn = []
-    yn = []
-    hn = []
+N = int(input())
 
-    for i in range(N):
-        xi, yi, hi = map(int, input().split())
+xn = []
+yn = []
+hn = []
 
-        xn.append(xi)
-        yn.append(yi)
-        hn.append(hi)
+for i in range(N):
+    xi, yi, hi = map(int, input().split())
 
-    for Cx in range(101):
-        for Cy in range(101):
+    xn.append(xi)
+    yn.append(yi)
+    hn.append(hi)
 
-            last = None
-            flg = True
+for Cx in range(101):
+    for Cy in range(101):
 
-            for i in range(N):
+        last = None
+        flg = True
 
-                if 0 == hn[i]:
-                    continue
+        for i in range(N):
 
-                H = abs(xn[i] - Cx) + abs(yn[i] - Cy) + hn[i]
-
-                if H < 0:
-                    continue
-
-                if last:
-                    if H != last:
-                        flg = False
-                        break
-
-                last = H
-
-            if not flg:
+            if 0 == hn[i]:
                 continue
 
-            for i in range(N):
+            H = abs(xn[i] - Cx) + abs(yn[i] - Cy) + hn[i]
 
-                if 0 == hn[i]:
-                    h = abs(xn[i] - Cx) + abs(yn[i] - Cy)
-                    if h < H:
-                        flg = False
-                        break
-
-            if not flg:
+            if H < 0:
                 continue
 
-            print(Cx, Cy, H)
-            return
+            if last:
+                if H != last:
+                    flg = False
+                    break
 
+            last = H
 
-main()
+        if not flg:
+            continue
+
+        for i in range(N):
+
+            if 0 == hn[i]:
+                h = abs(xn[i] - Cx) + abs(yn[i] - Cy)
+                if h < H:
+                    flg = False
+                    break
+
+        if not flg:
+            continue
+
+        print(Cx, Cy, H)
+        sys.exit()
