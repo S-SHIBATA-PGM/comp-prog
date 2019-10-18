3620781f2f717d7593fc8213fdc2562f29ded2fa
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:17:32 +0900

    Arrangement.

---
+++ pgm/ABC_Python/ABC_107_C.py
@@ -0,0 +1,41 @@
+MAX = 300000001
+
+N, K = map(int, input().split())
+xn = [int(i) for i in input().split()]
+
+
+def main():
+    if 0 <= xn[0]:
+        print(xn[K - 1])
+        return
+
+    if xn[N - 1] < 0:
+        print(abs(xn[N - K]))
+        return
+
+    ans = MAX
+
+    for i in range(N):
+        if N - 1 < i + K - 1:
+            break
+
+        if xn[i] < 0:
+            if xn[i + K - 1] < 0 and i + K <= N - 1 and 0 < xn[i + K]:
+                ans = min(ans, abs(xn[i]))
+
+            elif 0 <= xn[i + K - 1]:
+                ans = min(ans,
+                          2 * abs(xn[i]) + abs(xn[i + K - 1]),
+                          abs(xn[i]) + 2 * abs(xn[i + K - 1]))
+
+        elif 0 == xn[i]:
+            ans = min(ans, xn[i + K - 1])
+
+        elif 0 != i and xn[i - 1] < 0:
+            ans = min(ans, xn[i + K - 1])
+
+    print(ans)
+    return
+
+
+main()
