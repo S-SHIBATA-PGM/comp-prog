62bfe5984a00a8c8c5543a0394ebfc8c3382ae02
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-18 23:49:54 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_117_C.py
+++ pgm/ABC_Python/ABC_117_C.py
@@ -1,12 +1,10 @@
-import numpy as np
-
 N, M = map(int, input().split())
-X = list(set(map(int, input().split())))
+X = [int(i) for i in input().split()]
 
 X.sort()
 
-D = np.diff(X)
+D = [X[i+1] - X[i] for i in range(M - 1)]
 
 D.sort()
 
-print(0 if M <= N else np.sum(D[:M-N]))
+print(0 if M <= N else sum(D[:M-N]))
