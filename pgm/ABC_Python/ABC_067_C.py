60258eabfc4f59d6dc1afb4c7d6b3261e2746fce
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-19 20:55:09 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_067_C.py
+++ pgm/ABC_Python/ABC_067_C.py
@@ -5,8 +5,9 @@ def main():
     N, *a = map(int, open(0).read().split())
     a = list(accumulate(a))
     total = a[-1]
-    print(min(abs(total - 2 * i) for i in a[:-1]))
+    print(min(abs(2 * i - total) for i in a[:-1]))
     return
 
 
 main()
+
