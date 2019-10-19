6dbea7130fe7e0cfb3efe3e65577e6f0f97c41d8
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-19 11:13:23 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_129_B.py
+++ pgm/ABC_Python/ABC_129_B.py
@@ -1,17 +1,10 @@
-from sys import stdin
-from numpy import cumsum
+N = int(input())
 
-lines = stdin.readlines
-
-W = [int(i) for i in lines()[1].split()]
-
-total = sum(W)
-
-W = cumsum(W)
+W = [int(i) for i in input().split()]
 
 ans = 1000000007
 
-for w in W:
-    ans = min(ans, abs(total - 2 * w))
+for i in range(N):
+    ans = min(ans, abs(sum(W[:i]) - sum(W[i:])))
 
 print(ans)
