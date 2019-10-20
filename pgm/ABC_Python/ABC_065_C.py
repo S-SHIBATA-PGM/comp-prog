a978f9c4597dd151b600418feb7f0ad8bf1d739b
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-20 19:09:22 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_065_C.py
+++ pgm/ABC_Python/ABC_065_C.py
@@ -1,5 +1,5 @@
+from functools import reduce
 INF = int(1e9 + 7)
-FCT = [0] * (int(1e5) + 1)
 
 
 def modmulti(a, b):
@@ -8,21 +8,17 @@ def modmulti(a, b):
 
 
 def factorial(x):
-    FCT[0] = 1
-    FCT[1] = 1
-    for i in range(2, x + 1):
-        FCT[i] = modmulti(FCT[i - 1], i)
+    FCT = 1
+    FCT = reduce(modmulti, range(2, x + 1), FCT)
+    return FCT
 
 
 def main():
     N, M = map(int, input().split())
+    MIN_NM = factorial(min(N, M))
     if N == M or N + 1 == M or N == M + 1:
-        if N < M:
-            factorial(M)
-        else:
-            factorial(N)
-        print(modmulti(2, modmulti(FCT[N], FCT[M]))
-              if N == M else modmulti(FCT[N], FCT[M]))
+        print(modmulti(2, modmulti(MIN_NM, MIN_NM))
+              if N == M else modmulti(MIN_NM, modmulti(MIN_NM, max(N, M))))
     else:
         print(0)
 
@@ -30,4 +26,3 @@ def main():
 
 
 main()
-
