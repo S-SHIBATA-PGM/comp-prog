e2334f11c2a6826d39417def8209262d08081b3b
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-20 21:14:35 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_064_C.py
+++ pgm/ABC_Python/ABC_064_C.py
@@ -1,21 +1,18 @@
-from collections import defaultdict
-
-
-def check(ai, ratemax, d):
+def check(ai, ratemax, lst):
     if 3199 < ai:
         ratemax += 1
     else:
-        d[ai // 400] += 1
+        lst[ai // 400] = 1
     return ratemax
 
 
 def main():
     N, *a = map(int, open(0).read().split())
-    d = defaultdict(lambda: 0)
+    lst = [0] * 8
     ratemax = 0
     for ai in a:
-        ratemax = check(ai, ratemax, d)
-    print(len(d) if len(d) else 1, len(d) + ratemax)
+        ratemax = check(ai, ratemax, lst)
+    print(sum(lst) if sum(lst) else 1, sum(lst) + ratemax)
     return
 
 
