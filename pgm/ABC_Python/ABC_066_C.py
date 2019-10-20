9119217b34f83fb040a1747e03e04b309ab8a3b4
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-20 12:53:41 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_066_C.py
+++ pgm/ABC_Python/ABC_066_C.py
@@ -1,16 +1,11 @@
-from collections import deque
-
-
 def main():
     n, *a = open(0).read().split()
-    dq = deque()
-    n = int(n) - 1
-    for i, ai in enumerate(a):
-        if i % 2 == n % 2:
-            dq.appendleft(ai)
-        else:
-            dq.append(ai)
-    print(" ".join(dq))
+    if int(n) % 2:
+        print(" ".join(reversed(a[0::2])), end=" ")
+        print(" ".join(a[1::2]))
+    else:
+        print(" ".join(reversed(a[1::2])), end=" ")
+        print(" ".join(a[0::2]))
     return
 
 
