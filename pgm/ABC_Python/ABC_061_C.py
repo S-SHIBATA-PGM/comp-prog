85cb7ddf584031ad91e0da4408631083ba03ad4d
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-23 22:54:46 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_061_C.py
+++ pgm/ABC_Python/ABC_061_C.py
@@ -1,11 +1,12 @@
 from sys import stdin
+from operator import itemgetter
 
 
 def main():
     lines = stdin.readlines()
     N, K = map(int, lines[0].split())
     abn = [[int(abi) for abi in line.split()] for line in lines[1:]]
-    abn.sort()
+    abn.sort(key=itemgetter(0))
     cnt = 0
     for ai, bi in abn:
         cnt += bi
@@ -16,3 +17,4 @@ def main():
 
 
 main()
+
