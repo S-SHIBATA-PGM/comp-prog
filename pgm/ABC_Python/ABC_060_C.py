91d7945bdffc3a726e03534651806ad1d429da5b
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-24 22:19:43 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_060_C.py
+++ pgm/ABC_Python/ABC_060_C.py
@@ -1,15 +1,10 @@
-from sys import stdin
-
-
 def main():
-    lines = stdin.readlines()
-    T = int(lines[0].split()[1])
-    tn = [int(ti) for ti in lines[1].split()]
+    T, *tn = map(int, open(0).read().split()[1:])
     print(tn[-1] + T - sum(t2 - t1 - T
                            for t1, t2
-                           in zip(tn[0:], tn[1:]) if t2 - t1 > T))
+                           in zip(tn[0:], tn[1:])
+                           if t2 - t1 > T))
     return
 
 
 main()
-
