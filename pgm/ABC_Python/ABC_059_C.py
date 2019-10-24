16399b57c6eaff891477ab963f606d22e2507e06
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-24 23:35:14 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_059_C.py
+++ pgm/ABC_Python/ABC_059_C.py
@@ -3,28 +3,24 @@ def f(flg, an):
     ret = 0
     for i, ai in enumerate(an):
         accum += ai
-        if i % 2:
-            if flg and 0 <= accum:
-                ret += accum + 1
-                accum = -1
-            elif not flg and accum <= 0:
+        if flg:
+            flg = False
+            if accum <= 0:
                 ret += -accum + 1
                 accum = 1
         else:
-            if flg and accum <= 0:
-                ret += -accum + 1
-                accum = 1
-            elif not flg and 0 <= accum:
+            flg = True
+            if 0 <= accum:
                 ret += accum + 1
                 accum = -1
-
     return ret
 
 
 def main():
     n, *an = map(int, open(0).read().split())
-    print(min(f(0, an), f(1, an)))
+    print(min(f(False, an), f(True, an)))
     return
 
 
 main()
+
