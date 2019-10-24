39e4ba11b05628c327dee01b9ea401059b351c44
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-24 22:13:23 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_060_C.py
+++ pgm/ABC_Python/ABC_060_C.py
@@ -5,12 +5,11 @@ def main():
     lines = stdin.readlines()
     T = int(lines[0].split()[1])
     tn = [int(ti) for ti in lines[1].split()]
-    ans = 0
-    for t1, t2 in zip(tn[0:], tn[1:]):
-        ans += min(T, t2 - t1)
-    ans += T
-    print(ans)
+    print(tn[-1] + T - sum(t2 - t1 - T
+                           for t1, t2
+                           in zip(tn[0:], tn[1:]) if t2 - t1 > T))
     return
 
 
 main()
+
