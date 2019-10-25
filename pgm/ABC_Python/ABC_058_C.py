5cac63f03bd44054581c3530286c681a069c4315
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-25 20:34:17 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_058_C.py
+++ pgm/ABC_Python/ABC_058_C.py
@@ -1,17 +1,16 @@
-from collections import defaultdict
 INF = int(1e9+7)
 
 
 def main():
     Sn = list(open(0).read().split()[1:])
-    d = defaultdict(lambda: INF)
-    for Si in Sn:
-        for i in range(26):
-            d[i] = min(d[i], Si.count(chr(ord("a") + i)))
     for i in range(26):
-        if d[i] == INF:
+        cnt = INF
+        for Si in Sn:
+            cnt = min(cnt, Si.count(chr(ord("a") + i)))
+        if cnt == INF:
             continue
-        print(chr(ord("a") + i) * d[i], end="")
+        print(chr(ord("a") + i) * cnt, end="")
 
 
 main()
+
