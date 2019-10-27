67c95e4b3c24ac15d44d3e4abc029c59356b10e9
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-28 07:05:46 +0900

    Arrangement.

---
+++ pgm/ABC_Python/ABC_053_C.py
@@ -0,0 +1,14 @@
+def main():
+    x = int(input())
+    if x <= 6:
+        print(1)
+    elif x % 11 == 0:
+        print(2 * (x // 11))
+    elif x % 11 <= 6:
+        print(2 * (x // 11) + 1)
+    else:
+        print(2 * (x // 11) + 2)
+    return
+
+
+main()
