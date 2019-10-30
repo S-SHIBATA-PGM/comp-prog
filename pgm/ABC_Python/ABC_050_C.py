e31c82dd00babfcd2c242b47d0a81dbcc0a6c5f0
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-30 21:20:06 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_050_C.py
+++ pgm/ABC_Python/ABC_050_C.py
@@ -1,4 +1,3 @@
-from functools import reduce
 INF = int(1e9+7)
 
 
@@ -7,14 +6,15 @@ def main():
     An.sort()
     if N % 2:
         if [0] + [i for i in range(2, N, 2) for j in range(2)] == An:
-            print(reduce(lambda x, y: (x << 1) % INF, range(N // 2), 1))
+            print(2 ** (N // 2) % INF)
             return
     else:
         if [i for i in range(1, N, 2) for j in range(2)] == An:
-            print(reduce(lambda x, y: (x << 1) % INF, range(N // 2), 1))
+            print(2 ** (N // 2) % INF)
             return
     print(0)
     return
 
 
 main()
+
