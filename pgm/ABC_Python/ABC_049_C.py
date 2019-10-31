3ce040187bd4759aac5e2fefec78010aa972ee6c
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-10-31 23:04:36 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_049_C.py
+++ pgm/ABC_Python/ABC_049_C.py
@@ -1,14 +1,22 @@
-import re
+dreamer = "dreamer"
+eraser = "eraser"
+dream = "dream"
+erase = "erase"
 
 
 def main():
     S = input()
-    lens = len(S)
+
     while len(S):
-        S = re.sub("dreamer$", "", S)
-        S = re.sub("eraser$", "", S)
-        S = re.sub("dream$", "", S)
-        S = re.sub("erase$", "", S)
+        lens = len(S)
+        if S[lens - len(dreamer):] == dreamer:
+            S = S[: lens - len(dreamer)]
+        elif S[lens - len(eraser):] == eraser:
+            S = S[:lens - len(eraser)]
+        elif S[lens - len(dream):] == dream:
+            S = S[:lens - len(dream)]
+        elif S[lens - len(erase):] == erase:
+            S = S[:lens - len(erase)]
         if lens == len(S):
             print("NO")
             return
