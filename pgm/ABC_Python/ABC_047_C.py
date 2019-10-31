6deb73b1c3d4d319c4f23a73ddf9cc88c2bd89e5
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-11-01 07:06:23 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_047_C.py
+++ pgm/ABC_Python/ABC_047_C.py
@@ -1,12 +1,6 @@
 def main():
     S = input()
-    ans = -1
-    pre = ""
-    for s in S:
-        if pre != s:
-            ans += 1
-            pre = s
-    print(ans)
+    print(S.count("WB") + S.count("BW"))
     return
 
 
