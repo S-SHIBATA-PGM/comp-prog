33557c44c5fc6ce4eaa73cda9a25663a2b52cc32
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-11-03 14:41:54 +0900

    Arrangement.

---
+++ pgm/ABC_Python/ABC_039_C.py
@@ -0,0 +1,19 @@
+def main():
+    d = {"WBWBWWBWBWBWWBWBWWBW": "Do",
+         "BWBWWBWBWBWWBWBWWBWB": "Do",
+         "WBWWBWBWBWWBWBWWBWBW": "Re",
+         "BWWBWBWBWWBWBWWBWBWB": "Re",
+         "WWBWBWBWWBWBWWBWBWBW": "Mi",
+         "WBWBWBWWBWBWWBWBWBWW": "Fa",
+         "BWBWBWWBWBWWBWBWBWWB": "Fa",
+         "WBWBWWBWBWWBWBWBWWBW": "So",
+         "BWBWWBWBWWBWBWBWWBWB": "So",
+         "WBWWBWBWWBWBWBWWBWBW": "La",
+         "BWWBWBWWBWBWBWWBWBWW": "La",
+         "WWBWBWWBWBWBWWBWBWWB": "Si"}
+    print(d[input()])
+    return
+
+
+main()
+
