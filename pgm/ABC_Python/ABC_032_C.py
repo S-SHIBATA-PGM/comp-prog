17770d1b369829cb9884d714d80240ebfac07fb1
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-11-04 23:11:21 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_032_C.py
+++ pgm/ABC_Python/ABC_032_C.py
@@ -1,36 +1,44 @@
-from itertools import accumulate
-from operator import mul
-INF = float('inf')
-
-
 def main():
     N, K, *s = map(int, open(0).read().split())
-    s = list(accumulate(s, mul))
-    ans = N
-    if s[-1] <= K:
-        print(ans)
+    if 0 < s.count(0):
+        print(N)
         return
+    accum = s[0] if s[0] <= K else 1
     ans = 0
+    cur = 1 if s[0] <= K else 0
     i = 0
-    pre = 1
-    while i < N:
-        if K < s[i] // pre:
-            pre = s[i]
-            i += 1
-            continue
-        tmp = 1
-        for j in range(i + 1, N):
-            if s[j] // pre <= K:
-                tmp += 1
-            else:
-                ans = max(ans, tmp)
-                pre = s[j]
-                i = j
-                break
-        else:
-            ans = max(ans, tmp)
+    j = 1
+    while True:
+        if N <= j:
             break
-    print(ans)
+        if accum * s[j] <= K:
+            accum *= s[j]
+            cur += 1
+            j += 1
+        else:
+            ans = max(ans, cur)
+            while True:
+                flg = False
+                if 0 < cur:
+                    accum //= s[i]
+                    cur -= 1
+                    flg = True
+                i += 1
+                if N <= i:
+                    j = i
+                    break
+                elif flg and s[i] <= K:
+                    if cur == 0:
+                        accum *= s[i]
+                        cur += 1
+                        j = i + 1
+                    break
+                elif s[i] <= K:
+                    accum = s[i]
+                    cur = 1
+                    j = i + 1
+                    break
+    print(max(ans, cur))
     return
 
 
