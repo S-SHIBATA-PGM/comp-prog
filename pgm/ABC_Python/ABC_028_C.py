59964fdd7b75088bca0e7b961349852425061ae2
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-11-06 06:45:10 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_028_C.py
+++ pgm/ABC_Python/ABC_028_C.py
@@ -4,7 +4,7 @@ from itertools import combinations
 def main():
     A, B, C, D, E = map(int, input().split())
 
-    print(sorted(list(set(x + y + z for x, y, z
+    print(sorted(list(set(sum(x) for x
                           in combinations((A, B, C, D, E), 3))))[-3])
     return
 
