0d095c61bbeb70e9c1f33a07c76bf3801b525fc6
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-11-09 21:39:56 +0900

    Arrangement.

---
+++ pgm/ABC_Python/ABC_024_C.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+
+def main():
+    lines = stdin.readlines()
+    N, D, K = map(int, lines[0].split())
+    L = [0] * D
+    R = [0] * D
+    S = [0] * K
+    T = [0] * K
+    ans = [0] * K
+    for i, j in enumerate(range(1, D + 1)):
+        L[i], R[i] = map(int, lines[j].split())
+    for i, j in enumerate(range(D + 1, D + K + 1)):
+        S[i], T[i] = map(int, lines[j].split())
+    for d in range(D):
+        for k in range(K):
+            if L[d] <= S[k] <= R[d]:
+                if S[k] < T[k]:
+                    S[k] = min(T[k], R[d])
+                else:
+                    S[k] = max(T[k], L[d])
+            if not ans[k] and S[k] == T[k]:
+                ans[k] = d + 1
+    print("\n".join([str(a) for a in ans]))
+
+
+main()
+
