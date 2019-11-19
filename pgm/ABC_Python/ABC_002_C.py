22657fa8bdcf472f5fdd73100c38edf97d257677
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-11-19 22:30:23 +0900

    Arrangement.

---
+++ pgm/ABC_Python/ABC_002_C.py
@@ -0,0 +1,12 @@
+def main():
+    xa, ya, xb, yb, xc, yc = map(int, input().split())
+    xb -= xa
+    yb -= ya
+    xc -= xa
+    yc -= ya
+    print(abs(xb * yc - yb * xc) / 2)
+    return
+
+
+main()
+
