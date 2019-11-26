67e044295c76d8fbc31bc2c64d478c7fc995496e
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-11-26 21:08:22 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_137_C.py
+++ pgm/ABC_Python/ABC_137_C.py
@@ -1,13 +1,13 @@
-from collections import defaultdict
-
-
 def main():
     s = ["".join(sorted(i)) for i in open(0).read().split()[1:]]
-    d = defaultdict(lambda: 0)
     ans = 0
+    d = {}
     for i in s:
-        ans += d[i]
-        d[i] += 1
+        if i in d:
+            ans += d[i]
+            d[i] += 1
+        else:
+            d[i] = 1
     print(ans)
     return
 
