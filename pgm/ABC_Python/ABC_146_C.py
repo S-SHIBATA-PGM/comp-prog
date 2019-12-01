415a8cbefc204243677c5c87465481196b232b2a
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-12-01 13:47:08 +0900

    Performance challenge.

---
+++ pgm/ABC_Python/ABC_146_C.py
@@ -0,0 +1,31 @@
+A = 0
+B = 0
+X = 0
+
+
+def f(N):
+    return X < A * N + B * len(str(N))
+
+
+def bisectionMethod(left, right):
+    # Bisection Method
+    # [left, right]
+    # to narrow the range
+    while (1 < abs(right - left)):
+        mid = (left + right) // 2
+        rslt = f(mid)
+        if rslt:
+            right = mid
+        else:
+            left = mid
+    return left
+
+
+def main():
+    global A, B, X
+    A, B, X = map(int, input().split())
+    print(bisectionMethod(0, int(1e9 + 1)))
+    return
+
+
+main()
