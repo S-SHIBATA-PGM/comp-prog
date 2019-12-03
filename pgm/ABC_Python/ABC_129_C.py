268dbbcd8692a9b197d82b63433bfe48486ed821
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-12-03 21:45:35 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_129_C.py
+++ pgm/ABC_Python/ABC_129_C.py
@@ -1,25 +1,15 @@
-from sys import stdin
-INF = int(1e9+7)
-
-
-def modadd(a, b):
-    # aとbを足した値をmodする(a + b mod p)
-    return (a + b) % INF
-
-
 def main():
-    lines = stdin.readlines()
-    N = int(lines[0].split()[0])
-    a = [True] * (N + 1)
-    for i in map(int, lines[1:]):
-        a[i] = False
+    INF = int(1e9+7)
+    N, M, *a = map(int, open(0).read().split())
+    a = set(a)
     dp = [0] * (N + 1)
-    dp[:2] = 1, 1 if a[1] else 0
+    dp[:2] = 1, 1 if 1 not in a else 0
     for i in range(2, N + 1):
-        if a[i]:
-            dp[i] = modadd(dp[i - 1], dp[i - 2])
-    print(dp[N])
+        if i not in a:
+            dp[i] = (dp[i - 1] + dp[i - 2]) % INF
+    print(dp[-1])
     return
 
 
 main()
+
