92cb17ed7e0c5e82e839a4f4dfc5a21e34d1ab12
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-12-08 18:41:00 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_126_C.py
+++ pgm/ABC_Python/ABC_126_C.py
@@ -3,10 +3,12 @@ import math
 
 def main():
     N, K = map(int, input().split())
-    print((max(0, N - K + 1) + sum(
-        max(0, N - (math.ceil(K * 0.5 ** i)) + 1) * 0.5 ** i
-        - max(0, N - (math.ceil(K * 0.5 ** (i - 1))) + 1) * 0.5 ** i
-        for i in range(1, len(bin(K)) - 2 + 1))) / N)
+    accum = 0
+    for i in range(1, len(bin(K)) - 2 + 1):
+        cur = max(0, N - (math.ceil(K * 0.5 ** i)) + 1) * 0.5 ** i
+        pre = max(0, N - (math.ceil(K * 0.5 ** (i - 1))) + 1) * 0.5 ** i
+        accum += cur - pre
+    print((max(0, N - K + 1) + accum) / N)
     return
 
 
