227814739f96562bb67027ef7d58ba6c9cce6bb2
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-12-11 07:10:52 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_122_C.py
+++ pgm/ABC_Python/ABC_122_C.py
@@ -2,11 +2,9 @@ from sys import stdin
 
 
 def main():
-    readline = stdin.readline
-    read = stdin.read
-    N, Q = map(int, readline().split())
-    S = readline().strip()
-    *LR, = map(lambda x: int(x) - 1, read().split())
+    N, Q = map(int, input().split())
+    S = input()
+    LR = [int(x) - 1 for x in stdin.read().split()]
     AC = []
     pre = None
     cnt = 0
