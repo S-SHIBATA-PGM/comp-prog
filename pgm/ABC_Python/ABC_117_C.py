ee0487cb90f4dce86f0a50fc0721b4fdc6aa97f3
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-12-20 07:26:40 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_117_C.py
+++ pgm/ABC_Python/ABC_117_C.py
@@ -1,6 +1,6 @@
 def main():
     N, M, *X = map(int, open(0).read().split())
-    X.sort()
+    X = sorted(set(X))
     D = [j - i for i, j in zip(X, X[1:])]
     D.sort()
     print(0 if M <= N else sum(D[:M - N]))
