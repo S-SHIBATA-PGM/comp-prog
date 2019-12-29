658a532647287b461e53cba90990e20e940dac94
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-12-30 07:09:13 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_114_C.py
+++ pgm/ABC_Python/ABC_114_C.py
@@ -18,27 +18,25 @@ def f(d, used, next):
 
 def high(N, used, ret):
     if N:
-        if int(N[0]) > 3:
+        if N[0] > "3":
             ret += f(len(N) - 1, used, "3")
-        if int(N[0]) > 5:
+        if N[0] > "5":
             ret += f(len(N) - 1, used, "5")
-        if int(N[0]) > 7:
+        if N[0] > "7":
             ret += f(len(N) - 1, used, "7")
     else:
         if len(used) == 3:
             ret += 1
         return ret
-    if N[0] in ["3", "5", "7"]:
+    if N[0] in "357":
         ret = high(N[1:], used | set(N[0]), ret)
     return ret
 
 
 def low():
-    d = 0
     ret = 0
     for i in range(len(N) - 1):
-        ret += 3 * (3 ** d - 2 ** (d + 1) + 1)
-        d += 1
+        ret += 3 * (3 ** i - 2 ** (i + 1) + 1)
     return ret
 
 
