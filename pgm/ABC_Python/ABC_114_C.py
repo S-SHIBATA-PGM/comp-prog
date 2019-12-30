efa1f61f9acc656aa12055aa3beeb5c7fd929607
ShinjiSHIBATA <sshibata0843@gmail.com>
2019-12-30 12:02:58 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_114_C.py
+++ pgm/ABC_Python/ABC_114_C.py
@@ -1,47 +1,21 @@
-N = input()
+N = int(input())
 
 
-def f(d, used, cur):
-    num = len(used | set(cur))
-    if d == 0:
-        if num == 3:
-            return 1
-        else:
-            return 0
-    elif num == 3:
-        return 3 ** d
-    elif num == 2:
-        return 3 ** d - 2 ** d
-    else:
-        return 3 ** d - 2 * (2 ** d) + 1
-
-
-def high(N, used, ret):
-    if N:
-        if N[0] > "3":
-            ret += f(len(N) - 1, used, "3")
-        if N[0] > "5":
-            ret += f(len(N) - 1, used, "5")
-        if N[0] > "7":
-            ret += f(len(N) - 1, used, "7")
-    else:
-        if len(used) == 3:
-            ret += 1
+def dfs(ret, cur, f1, f2, f3):
+    if N < cur:
         return ret
-    if N[0] in "357":
-        ret = high(N[1:], used | set(N[0]), ret)
-    return ret
 
+    ret += 1 if f1 and f2 and f3 else 0
 
-def low():
-    ret = 0
-    for d in range(len(N) - 1):
-        ret += 3 * (3 ** d - 2 ** (d + 1) + 1)
+    ret = dfs(ret, cur * 10 + 7, True, f2, f3)
+    ret = dfs(ret, cur * 10 + 5, f1, True, f3)
+    ret = dfs(ret, cur * 10 + 3, f1, f2, True)
+
     return ret
 
 
 def main():
-    print(high(N, set(), low()))
+    print(dfs(0, 0, False, False, False))
     return
 
 
