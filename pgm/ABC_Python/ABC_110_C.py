f60b4a6918a89dad291348683b274c9c67d9f94a
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-02 15:57:20 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_110_C.py
+++ pgm/ABC_Python/ABC_110_C.py
@@ -1,25 +1,17 @@
-input = open(0).read
-S, T = input().split()
-
-
 def main():
-    C1 = [None] * (ord('z') - ord('a') + 1)
-    C2 = [None] * (ord('z') - ord('a') + 1)
-
+    S = input()
+    T = input()
+    d = dict()
     flg = True
-
-    for i in range(len(S)):
-        c1 = ord(S[i]) - ord('a')
-        c2 = ord(T[i]) - ord('a')
-
-        if C1[c1] or C2[c2]:
-            if c1 != C2[c2] or c2 != C1[c1]:
-                flg = False
-                break
-
-        C1[c1] = c2
-        C2[c2] = c1
-
+    for s, t in zip(S, T):
+        # 同アルファベット、違アルファベット変換チェック
+        if s in d and d[s] != t:
+            flg = False
+            break
+        d[s] = t
+    # 変換先の重複チェック
+    if len(d.values()) != len(set(d.values())):
+        flg = False
     print("Yes" if flg else "No")
     return
 
