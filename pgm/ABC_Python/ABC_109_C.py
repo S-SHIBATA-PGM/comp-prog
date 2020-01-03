0fc546afb77adab1d203df20d3ae4767cbc78794
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-03 09:52:49 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_109_C.py
+++ pgm/ABC_Python/ABC_109_C.py
@@ -8,11 +8,10 @@ def main():
     N, X, *xn = map(int, open(0).read().split())
     ans = None
     for x in xn:
-        if ans is not None:
-            if abs(X - x) % ans:
-                ans = gcd(ans, abs(X - x))
-        else:
+        if ans is None:
             ans = abs(X - xn[0])
+        elif abs(X - x) % ans:
+            ans = gcd(ans, abs(X - x))
     print(ans)
     return
 
