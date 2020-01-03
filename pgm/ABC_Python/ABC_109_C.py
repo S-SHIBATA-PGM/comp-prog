b9f53ef6a9131a98ca60b78a145b99a2c2c507e2
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-03 14:02:30 +0900

    Arrangement.

--- pgm/ABC_Python/ABC_109_C.py
+++ pgm/ABC_Python/ABC_109_C.py
@@ -8,9 +8,9 @@ def main():
     N, X, *xn = map(int, open(0).read().split())
     ans = abs(X - xn[0])
     for x in xn:
-        dif = abs(X - x)
-        if dif % ans:
-            ans = gcd(ans, dif)
+        df = abs(X - x)
+        if df % ans:
+            ans = gcd(ans, df)
     print(ans)
     return
 
