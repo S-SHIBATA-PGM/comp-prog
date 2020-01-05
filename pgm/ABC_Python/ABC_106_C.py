fc11eb2b2f6fc84b7be0f85cc9c30e3f671770f1
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-05 09:45:00 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_106_C.py
+++ pgm/ABC_Python/ABC_106_C.py
@@ -1,13 +1,10 @@
-S = input()
-K = int(input())
-
-
 def main():
+    S = input()
+    K = int(input())
     for i in range(min(len(S), K)):
         if S[i] != '1':
             print(S[i])
             return
-
     print(1)
     return
 
