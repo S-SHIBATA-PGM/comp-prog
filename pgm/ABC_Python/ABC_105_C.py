e86affa06cf095840bad943220b6e375164679c1
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-05 10:09:17 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_105_C.py
+++ pgm/ABC_Python/ABC_105_C.py
@@ -1,23 +1,14 @@
 def main():
     N = int(input())
     S = ""
-
-    if N == 0:
-        print("0")
-
-    else:
-        while N:
-            if N % 2:
-                N -= 1
-                S = "1" + S
-
-            else:
-                S = "0" + S
-
-            N //= -2
-
-        print(S)
-
+    while N:
+        if N % 2:
+            N -= 1
+            S = "1" + S
+        else:
+            S = "0" + S
+        N //= -2
+    print(S if S else "0")
     return
 
 
