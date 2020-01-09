d9b5211d65891954544b00006d77533b3ab651ca
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-10 08:15:12 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_104_C.py
+++ pgm/ABC_Python/ABC_104_C.py
@@ -2,31 +2,28 @@ def main():
     D, G, *pc = map(int, open(0).read().split())
     p = pc[::2]
     c = pc[1::2]
-    ans = 1e9 + 7
-    # 2 ** D - 1 まで
-    for b in range(2 ** D):
-        num = 0
-        pnt = 0
-        # コンプリートしない MAX
-        x = None
-        # 得点が高い方から
-        for i in range(D - 1, -1, -1):
-            # i 番目 の フラグ が 立っている
-            if b >> i & 1:
-                num += p[i]
-                # コンプリートする
-                pnt += c[i] + 100 * (i + 1) * p[i]
-            elif x is None:
-                x = i
-        # コンプリート オール
-        # または 既に 目標の 総合スコア に到達
-        if x is None or G <= pnt:
-            ans = min(ans, num)
-        # コンプリートしない MAX で 目標の 総合スコア に到達
-        elif -(-(G - pnt) // (100 * (x + 1))) < p[x]:
-            ans = min(ans, num - (-(G - pnt) // (100 * (x + 1))))
-    print(ans)
-    return
+    # コンプリート
+    comp = [c[i] + 100 * (i + 1) * p[i] for i in range(D)]
+
+    def dfs(G, i):
+        # G 総合スコア
+        # i 問題 1-based indexing
+        if i <= 0:
+            # 目標の 総合スコア に到達しなかった
+            return 1e9 + 7
+        score = 0
+        num = min(G // (100 * i), p[i - 1])
+        if num == p[i - 1]:
+            # コンプリートする
+            score = comp[i - 1]
+        else:
+            # コンプリートしない
+            score = 100 * i * num
+        if score < G:
+            num += dfs(G - score, i - 1)
+        # 問題を解いた、解かなかったの最小
+        return min(num, dfs(G, i - 1))
+    print(dfs(G, D))
 
 
 main()
