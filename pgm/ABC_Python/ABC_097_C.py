976b1681c2fc099a2e769c957ff23afb8cc348c1
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-21 22:23:17 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_097_C.py
+++ pgm/ABC_Python/ABC_097_C.py
@@ -1,9 +1,11 @@
 def main():
     s = input()
     K = int(input())
-    print(sorted({s[i:j]
-                  for i in range(len(s))
-                  for j in range(i + 1, min(i + K + 1, len(s) + 1))})[K - 1])
+    print(sorted(list({s[i:j]
+                       for i
+                       in range(len(s))
+                       for j
+                       in range(i + 1, min(i + K + 1, len(s) + 1))}))[K - 1])
     return
 
 
