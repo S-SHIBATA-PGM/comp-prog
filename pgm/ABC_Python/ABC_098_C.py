641b6234b4936bb4e002c59e633d7b68369b35b9
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-21 22:29:47 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_098_C.py
+++ pgm/ABC_Python/ABC_098_C.py
@@ -3,14 +3,15 @@ def main():
     pre = S.count("E")
     cur = pre
     for Si in S:
-        if pre < cur:
-            cur = pre
+        if cur < pre:
+            pre = cur
         if Si == "E":
-            pre -= 1
+            cur -= 1
         else:
-            pre += 1
+            cur += 1
     print(pre if pre < cur else cur)
     return
 
 
 main()
+
