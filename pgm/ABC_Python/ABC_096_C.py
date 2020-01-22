7b09a39026b3544fde5291f81c067bdd8a14a1dd
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-22 23:31:30 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_096_C.py
+++ pgm/ABC_Python/ABC_096_C.py
@@ -1,35 +1,20 @@
-# 下、右、上、左
-dy = [1, 0, -1, 0]
-dx = [0, 1, 0, -1]
-
-H, W = map(int, input().split())
-s = [input() for i in range(H)]
-
-
 def main():
-    for y in range(H):
-        for x in range(W):
-
-            if s[y][x] == '.':
+    # 下、右、上、左
+    dyx = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    H, W = map(int, input().split())
+    s = ["." * (W + 2)]
+    s += ["." + input() + "." for i in range(H)]
+    s.append("." * (W + 2))
+    for y in range(1, H + 1):
+        for x in range(1, W + 1):
+            if "." == s[y][x]:
                 continue
-
-            cnt = 0
-
-            for k in range(4):
-                ny = y + dy[k]
-                nx = x + dx[k]
-
-                if ny < 0 or H <= ny\
-                        or nx < 0 or W <= nx\
-                        or s[ny][nx] == '.':
-                    continue
-
-                cnt += 1
-
-            if cnt == 0:
+            for dy, dx in dyx:
+                if "#" == s[y + dy][x + dx]:
+                    break
+            else:
                 print("No")
                 return
-
     print("Yes")
     return
 
