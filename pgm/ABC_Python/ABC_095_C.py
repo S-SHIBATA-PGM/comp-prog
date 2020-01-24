6a53c6090a019a1f49ff96f0da8c94c36411202e
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-24 23:12:22 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_095_C.py
+++ pgm/ABC_Python/ABC_095_C.py
@@ -1,11 +1,12 @@
-A, B, C, X, Y = map(int, input().split())
-
-
 def main():
-    print(
-        min(A * X + B * Y,
-            2 * max(X, Y) * C,
-            2 * min(X, Y) * C + (B if X < Y else A) * abs(X - Y)))
+    A, B, C, X, Y = map(int, input().split())
+    if 2 * C < A + B:
+        print(min(
+            2 * C * max(X, Y),
+            2 * C * min(X, Y) + A * (X - min(X, Y)) + B * (Y - min(X, Y))
+        ))
+    else:
+        print(A * X + B * Y)
     return
 
 
