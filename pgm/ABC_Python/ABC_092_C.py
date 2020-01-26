f995a3a15b1c4441e19f346fec69e314a425af00
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-26 14:41:13 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_092_C.py
+++ pgm/ABC_Python/ABC_092_C.py
@@ -1,10 +1,10 @@
 def main():
     N = int(input())
     A = [0] + [int(i) for i in input().split()] + [0]
-    l = [abs(A[i + 1] - A[i]) for i in range(N + 1)]
-    num = sum(l)
+    a = [abs(A[i + 1] - A[i]) for i in range(N + 1)]
+    num = sum(a)
     print("\n".join(
-        [str(num - l[i] - l[i + 1] + abs(A[i] - A[i + 2]))
+        [str(num - a[i] - a[i + 1] + abs(A[i] - A[i + 2]))
          for i in range(N)]))
 
     return
