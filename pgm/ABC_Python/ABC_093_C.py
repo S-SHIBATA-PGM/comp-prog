90273c02c313ce5eaf5e786d7d51476f9889c5a8
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-26 10:17:25 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_093_C.py
+++ pgm/ABC_Python/ABC_093_C.py
@@ -1,7 +1,5 @@
-A, B, C = sorted(map(int, input().split()))
-
-
 def main():
+    A, B, C = sorted(map(int, input().split()))
     print((2 * C - A - B) // 2 + 2 if (2 * C - A - B) % 2 else
           (2 * C - A - B) // 2)
     return
