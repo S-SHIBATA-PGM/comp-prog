e935a392d8ca125b3ef8abe2735ff2415417c8bb
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-01-27 23:37:40 +0900

    Performance challenge.

--- pgm/ABC_Python/ABC_091_C.py
+++ pgm/ABC_Python/ABC_091_C.py
@@ -1,22 +1,19 @@
-from operator import itemgetter
-
-
 def main():
     N = int(input())
     ab = [tuple(int(j) for j in input().split()) for i in range(N)]
     cd = [tuple(int(j) for j in input().split()) for i in range(N)]
     # y 降順
-    ab.sort(key=itemgetter(1), reverse=True)
+    ab.sort(key=lambda x: x[1], reverse=True)
     # x 昇順
-    cd.sort(key=itemgetter(0))
-    # ペアになった赤
-    done = [False] * N
+    cd.sort()
+    ans = 0
     for bx, by in cd:
-        for k, ((rx, ry), d) in enumerate(zip(ab, done)):
-            if not d and rx < bx and ry < by:
-                done[k] = True
+        for rx, ry in ab:
+            if rx < bx and ry < by:
+                ab.remove((rx, ry))
+                ans += 1
                 break
-    print(sum(done))
+    print(ans)
     return
 
 
