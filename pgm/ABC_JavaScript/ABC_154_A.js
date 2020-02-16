aeafa868ff6fc225e354b45261d132834e36aaf3
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-16 12:42:27 +0900

    Arrangement.

---
+++ pgm/ABC_JavaScript/ABC_154_A.js
@@ -0,0 +1,15 @@
+function main(input) {
+    var line = input.split(/\r?\n/g);
+    var arr = line[0].split(" ");
+    const S = arr[0], T = arr[1];
+    arr = line[1].split(" ")
+    var A = parseInt(arr[0]), B = parseInt(arr[1]);
+    const U = line[2];
+    if (S === U) {
+        A--;
+    } else {
+        B--;
+    }
+    console.log(A + " " + B);
+}
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
