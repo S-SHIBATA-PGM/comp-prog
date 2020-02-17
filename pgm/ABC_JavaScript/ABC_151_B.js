449508233fdf92046a410ccb5a0db67f8a6dddc3
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-17 23:20:39 +0900

    Arrangement.

---
+++ pgm/ABC_JavaScript/ABC_151_B.js
@@ -0,0 +1,13 @@
+// (x + ans) / N >= M
+// x + ans >= N * M
+// ans >= N * M - x
+
+function main(input) {
+    var line = input.split(/\r?\n/g);
+    var arr = line[0].split(" ");
+    const N = arr[0], K = arr[1], M = arr[2];
+    arr = line[1].split(" ").map(Number);
+    ans = N * M - arr.reduce((A, x) => A + x);
+    console.log(ans < 0 ? 0 : K < ans ? -1 : ans);
+}
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
