7d81f6984a340858d6853c691a0741e0e8ea80f5
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-19 21:16:46 +0900

    Arrangement.

--- pgm/ABC_Visual_Basic/ABC_151_B.vb
+++ pgm/ABC_Visual_Basic/ABC_151_B.vb
@@ -1,16 +1,16 @@
 Public Class Main
     Public Shared Sub Main(ByVal args() As String)
         Dim N, K, M As Integer
-        Dim arr() as String = Split(Console.ReadLine())
-        Dim NKM() As Integer = Array.ConvertAll(Of String, Integer)(arr, AddressOf Integer.Parse)
+        Dim NKM(3) As Integer
+        NKM = Array.ConvertAll(Of String, Integer)(Split(Console.ReadLine()), AddressOf Integer.Parse)
         N = NKM(0) : K = NKM(1) : M = NKM(2)
-        arr = Split(Console.Readline())
-        Dim A() As Integer = Array.ConvertAll(Of String, Integer)(arr, AddressOf Integer.Parse)
+        Dim A() As Integer
+        A = Array.ConvertAll(Of String, Integer)(Split(Console.ReadLine()), AddressOf Integer.Parse)
         Dim accum As Integer = 0
-        For Each Ai As Integer in A
+        For Each Ai As Integer In A
             accum += Ai
         Next
         Dim ans As Integer = N * M - accum
-        Console.WriteLine(If(ans < 0, 0, If(K < ans, -1, ans)))
+        Console.WriteLine(if(ans < 0, 0, if(K < ans, -1, ans)))
     End Sub
 End Class
