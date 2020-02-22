9197318ee7351ce1ada1e49f85fe9b07a6fbf174
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-22 19:28:02 +0900

    Arrangement.

---
+++ pgm/ABC_Visual_Basic/ABC_155_B.vb
@@ -0,0 +1,16 @@
+Public Class Main
+    Public Shared Sub Main(ByVal args() As String)
+        Dim N As Integer
+        N = console.ReadLine()
+        Dim A() As Integer
+        A = Array.ConvertAll(Of String, Integer)(Split(Console.ReadLine()), AddressOf Integer.Parse)
+        Dim flg As Boolean = True
+        For Each Ai As Integer In A
+            If (Ai Mod 2 = 0) AndAlso (Ai Mod 3 <> 0) AndAlso (Ai Mod 5 <> 0) Then
+                flg = False
+                Exit For
+            End If
+        Next
+        console.WriteLine(If(flg, "APPROVED", "DENIED"))
+    End Sub
+End Class
