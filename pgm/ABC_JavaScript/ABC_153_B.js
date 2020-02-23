cd696dc19555f92c56465952dcb603e8985ce031
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-23 22:18:13 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_153_B.js
+++ pgm/ABC_JavaScript/ABC_153_B.js
@@ -2,11 +2,10 @@ function main(input) {
     var line = input.split(/\r?\n/g);
     var arr = line[0].split(" ");
     const H = arr[0], N = arr[1];
-    var A = line[1].split(" ").map(Number);
-    var accum = 0
-    A.forEach(Ai => {
-        accum += Ai
-    });
+    var A = line[1].split(" ");
+    var accum = 0;
+    for (var i = 0; i < A.length; i++)
+        accum += parseInt(A[i]);
     console.log(1 < Math.ceil(H / accum) ? "No" : "Yes");
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
