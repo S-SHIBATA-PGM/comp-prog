f8279cb71a2ea0abcb45ad6b0569e502a24c32d8
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-23 21:43:36 +0900

    Arrangement.

---
+++ pgm/ABC_Visual_Basic/ABC_154_B.vb
@@ -0,0 +1,6 @@
+Public Class Main
+    Public Shared Sub Main(ByVal args() As String)
+        Dim line As String = Console.ReadLine()
+        Console.WriteLine(New String("x"c, Len(line)))
+    End Sub
+End Class
