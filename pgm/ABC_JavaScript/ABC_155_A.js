48954768da235def4392eb24175eed49d6f98c42
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-23 20:06:33 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_155_A.js
+++ pgm/ABC_JavaScript/ABC_155_A.js
@@ -1,7 +1,11 @@
 function main(input) {
     var arr = input.replace(/\r?\n/g, "").split(" ");
-    const A = parseInt(arr[0]), B = parseInt(arr[1]), C = parseInt(arr[2]);
-    if (A == B && B == C) console.log("No");
-    else console.log(arr.some(v => v == (A ^ B ^ C)) ? "Yes" : "No");
+    const A = arr[0], B = arr[1], C = arr[2];
+    var flg = false;
+    if (A == B && B != C) flg = true;
+    if (B == C && C != A) flg = true;
+    if (C == A && A != B) flg = true;
+    if (flg) console.log("Yes");
+    else console.log("No");
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
