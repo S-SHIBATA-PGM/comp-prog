5cf46717513796809e816beac62c3ebed75abe1a
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-24 20:16:42 +0900

    Performance challenge.

--- pgm/ABC_Visual_Basic/ABC_153_B.vb
+++ pgm/ABC_Visual_Basic/ABC_153_B.vb
@@ -1,17 +1,21 @@
-Option Strict On
-Imports System.Collections.Generic
 Public Class Main
     Public Shared Sub Main(ByVal args() As String)
         Dim H, N As Integer
         Dim arr(2) As String
         arr = Split(Console.ReadLine())
-        H = Integer.Parse(arr(0)) : N = Integer.Parse(arr(1))
-        Dim A As New List(Of Integer)(Array.ConvertAll(Of String, Integer) _
-            (Split(Console.ReadLine()), AddressOf Integer.Parse))
+        H = arr(0) : N = arr(1)
+        Dim A() As Integer = Array.ConvertAll(Of String, Integer) _
+            (Split(Console.ReadLine()), AddressOf Integer.Parse)
+        Array.Sort(A)
         Dim accum As Integer = 0
-        For Each Ai As Integer In A
-            accum += Ai
+        Dim flg As Boolean = False
+        For i As Integer = N - 1 To 0 Step -1
+            accum += A(i)
+            If H <= accum Then
+                flg = True
+                Exit For
+            End If
         Next
-        Console.WriteLine(If(accum < H, "No", "Yes"))
+        Console.WriteLine(If(flg, "Yes", "No"))
     End Sub
 End Class
