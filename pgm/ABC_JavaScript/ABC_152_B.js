fd9b99b8d007a7ae7ca3b224d1708f1f819d9468
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-25 08:16:46 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_152_B.js
+++ pgm/ABC_JavaScript/ABC_152_B.js
@@ -1,13 +1,8 @@
+"use strict";
 function main(input) {
-    var line = input.split(/\r?\n/g);
-    var arr = line[0].split(" ");
-    const a = arr[0], b = arr[1];
-    var an = "";
-    var bn = "";
-    for (var i = 0; i < b; i++)
-        an += a;
-    for (var i = 0; i < a; i++)
-        bn += b
-    console.log(an < bn ? an : bn)
+    let line = input.split(/\r?\n/g);
+    let arr = line[0].split(" ");
+    const a = parseInt(arr[0]), b = parseInt(arr[1]);
+    console.log(a < b ? String(a).repeat(b) : String(b).repeat(a))
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
