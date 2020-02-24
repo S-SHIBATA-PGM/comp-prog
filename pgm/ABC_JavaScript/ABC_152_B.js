d563d4dcdd397bfd65b5f8cb74bacb4a66c88b1f
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-25 07:40:11 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_152_B.js
+++ pgm/ABC_JavaScript/ABC_152_B.js
@@ -2,6 +2,12 @@ function main(input) {
     var line = input.split(/\r?\n/g);
     var arr = line[0].split(" ");
     const a = arr[0], b = arr[1];
-    console.log(a.repeat(b) < b.repeat(a) ? a.repeat(b) : b.repeat(a));
+    var an = "";
+    var bn = "";
+    for (var i = 0; i < b; i++)
+        an += a;
+    for (var i = 0; i < a; i++)
+        bn += b
+    console.log(an < bn ? an : bn)
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
