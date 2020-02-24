23ca0169908811030f5f56f67770a959106f131d
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-24 11:27:54 +0900

    Performance challenge.

--- pgm/ABC_Java/ABC_151_B.java
+++ pgm/ABC_Java/ABC_151_B.java
@@ -1,10 +1,8 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         FastScanner sc = new FastScanner();
-        PrintWriter out = new PrintWriter(System.out);
         final int N = sc.nextInt();
         final int K = sc.nextInt();
         final int M = sc.nextInt();
@@ -12,99 +10,144 @@ public class Main {
         for (int i = 0; i < N - 1; i++)
             accum += sc.nextInt();
         int ans = N * M - accum;
-        out.println(ans < 0 ? 0 : K < ans ? -1 : ans);
-        out.flush();
+        System.out.println(ans < 0 ? 0 : K < ans ? -1 : ans);
     }
-}
 
-class FastScanner {
     // FastScanner start
-    private final InputStream in = System.in;
-    private final byte[] buffer = new byte[1024];
-    private int ptr = 0;
-    private int buflen = 0;
+    static class FastScanner {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
 
-    private boolean hasNextByte() {
-        if (ptr < buflen) {
-            return true;
-        } else {
-            ptr = 0;
-            try {
-                buflen = in.read(buffer);
-            } catch (IOException e) {
-                e.printStackTrace();
+        public FastScanner() {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public FastScanner(String file_name) throws IOException {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        public char nextChar() throws IOException {
+            byte c = read();
+            while (Character.isWhitespace(c)) {
+                c = read();
             }
-            if (buflen <= 0) {
-                return false;
+            return (char) c;
+        }
+
+        public String nextString() throws IOException {
+            byte c = read();
+            while (Character.isWhitespace(c)) {
+                c = read();
             }
-        }
-        return true;
-    }
-
-    private int readByte() {
-        if (hasNextByte())
-            return buffer[ptr++];
-        else
-            return -1;
-    }
-
-    private static boolean isPrintableChar(int c) {
-        return 33 <= c && c <= 126;
-    }
-
-    public boolean hasNext() {
-        while (hasNextByte() && !isPrintableChar(buffer[ptr]))
-            ptr++;
-        return hasNextByte();
-    }
-
-    public String next() {
-        if (!hasNext())
-            throw new NoSuchElementException();
-        StringBuilder sb = new StringBuilder();
-        int b = readByte();
-        while (isPrintableChar(b)) {
-            sb.appendCodePoint(b);
-            b = readByte();
-        }
-        return sb.toString();
-    }
-
-    public long nextLong() {
-        if (!hasNext())
-            throw new NoSuchElementException();
-        long n = 0;
-        boolean minus = false;
-        int b = readByte();
-        if (b == '-') {
-            minus = true;
-            b = readByte();
-        }
-        if (b < '0' || '9' < b) {
-            throw new NumberFormatException();
-        }
-        while (true) {
-            if ('0' <= b && b <= '9') {
-                n *= 10;
-                n += b - '0';
-            } else if (b == -1 || !isPrintableChar(b)) {
-                return minus ? -n : n;
-            } else {
-                throw new NumberFormatException();
+            StringBuilder builder = new StringBuilder();
+            builder.append((char) c);
+            c = read();
+            while (!Character.isWhitespace(c)) {
+                builder.append((char) c);
+                c = read();
             }
-            b = readByte();
+            return builder.toString();
+        }
+
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public int[] nextIntArray(int n) throws IOException {
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        public long[] nextLongArray(int n) throws IOException {
+            long arr[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        public double[] nextDoubleArray(int n) throws IOException {
+            double arr[] = new double[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = nextDouble();
+            return arr;
         }
     }
-
-    public int nextInt() {
-        long nl = nextLong();
-        if (nl < Integer.MIN_VALUE || nl > Integer.MAX_VALUE)
-            throw new NumberFormatException();
-        return (int) nl;
-    }
-
-    public double nextDouble() {
-        return Double.parseDouble(next());
-    }
+    // FastScanner end
 }
-// FastScanner end
