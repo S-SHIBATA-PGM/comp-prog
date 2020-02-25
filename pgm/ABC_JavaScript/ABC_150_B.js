5aa27b01fcbcf1b8441ba613e2f953e6aec92728
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-02-26 08:47:26 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_150_B.js
+++ pgm/ABC_JavaScript/ABC_150_B.js
@@ -1,6 +1,7 @@
 "use strict";
 function main(input) {
     let line = input.split(/\r?\n/g);
-    console.log(line[1].match("abc"))
+    let arr = line[1].match(/ABC/g);
+    console.log(arr == null ? 0 : arr.length);
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
