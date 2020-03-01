336c743273415a2df07fafebc75a2f07b300f102
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-01 19:08:41 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_156_A.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String[] arr = Console.ReadLine().Split();
+        int N = int.Parse(arr[0]), R = int.Parse(arr[1]);
+        Console.WriteLine(10 <= N ? R : R + 100 * (10 - N));
+    }
+}
