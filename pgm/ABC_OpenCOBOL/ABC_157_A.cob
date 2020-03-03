a077611de895dc9e5e28fa648533ccb4faf16687
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-04 08:33:59 +0900

    Performance challenge.

---
+++ pgm/ABC_OpenCOBOL/ABC_157_A.cob
@@ -0,0 +1,11 @@
+PROGRAM-ID.                      PROGRAM_ID.
+DATA                             DIVISION.
+WORKING-STORAGE                  SECTION.
+    01 N            PIC 9(3).
+    01 ans          PIC Z9.
+ 
+PROCEDURE                        DIVISION.
+    ACCEPT N.
+    COMPUTE ans = (N + 1) / 2.
+    DISPLAY ans.
+    STOP RUN.
