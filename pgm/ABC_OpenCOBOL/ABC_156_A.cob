ecd892594a40d6bde4ce51d93b65ece858032c16
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-05 08:00:09 +0900

    Performance challenge.

--- pgm/ABC_OpenCOBOL/ABC_156_A.cob
+++ pgm/ABC_OpenCOBOL/ABC_156_A.cob
@@ -1,29 +1,19 @@
-PROGRAM-ID.                      PROGRAM_ID.
-DATA                             DIVISION.
-WORKING-STORAGE                  SECTION.
-    01 LN          PIC X(8).
-    01 N           PIC 9(3).
-    01 R           PIC 9(4).
-    01 ans         PIC X(4).
-    01 ZS          PIC Z(4)9.
-    01 DUMMY       PIC X(1).
+IDENTIFICATION                    DIVISION.
+PROGRAM-ID.                       PROGRAM_ID.
+DATA                              DIVISION.
+WORKING-STORAGE                   SECTION.
+01 LN           PIC X(8).
+01 N            PIC 9(3).
+01 R            PIC 9(4).
+01 ans          PIC Z(3)9.
 
-PROCEDURE                        DIVISION.
+PROCEDURE                         DIVISION.
     ACCEPT LN.
-    UNSTRING LN
-    DELIMITED BY SPACE
-    INTO N R.
+    UNSTRING LN DELIMITED BY SPACE INTO N R.
     IF 10 <= N
-        THEN
-            MOVE R TO ZS;
-        ELSE
-            COMPUTE ZS = R + 100 * (10 - N);
+        MOVE R TO ans
+    ELSE
+        COMPUTE ans = R + 100 * (10 - N)
     END-IF.
-    PERFORM UNANS.
-    DISPLAY ans(1:FUNCTION STORED-CHAR-LENGTH(ans)).
+    DISPLAY ans.
     STOP RUN.
-
-UNANS                            SECTION.
-    UNSTRING
-    ZS DELIMITED BY ALL SPACE
-    INTO DUMMY ans.
