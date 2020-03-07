4b6b6165b7da39daf42a6ff8d5ca8aa3a9430af2
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-07 19:38:24 +0900

    Performance challenge.

--- pgm/ABC_OpenCOBOL/ABC_156_A.cob
+++ pgm/ABC_OpenCOBOL/ABC_156_A.cob
@@ -1,19 +1,20 @@
-IDENTIFICATION                    DIVISION.
-PROGRAM-ID.                       PROGRAM_ID.
-DATA                              DIVISION.
-WORKING-STORAGE                   SECTION.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
 01 LN           PIC X(8).
 01 N            PIC 9(3).
 01 R            PIC 9(4).
 01 ans          PIC Z(3)9.
 
-PROCEDURE                         DIVISION.
-    ACCEPT LN.
-    UNSTRING LN DELIMITED BY SPACE INTO N R.
-    IF 10 <= N
-        MOVE R TO ans
-    ELSE
-        COMPUTE ans = R + 100 * (10 - N)
-    END-IF.
-    DISPLAY ans.
-    STOP RUN.
+PROCEDURE DIVISION.
+ACCEPT LN.
+UNSTRING LN DELIMITED BY SPACE INTO N R.
+IF 10 <= N
+    MOVE R TO ans
+ELSE
+    COMPUTE ans = R + 100 * (10 - N)
+END-IF.
+DISPLAY ans.
+STOP RUN.
