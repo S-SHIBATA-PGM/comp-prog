cbc00431a7e38e1b803990e9f2857ac9b9f14f8f
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-14 19:49:06 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_152_A.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String[] arr = Console.ReadLine().Split();
+        int N = int.Parse(arr[0]), M = int.Parse(arr[1]);
+        Console.WriteLine(N == M ? "Yes" : "No");
+    }
+}
---
+++ pgm/ABC_C#/ABC_152_B.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String[] arr = Console.ReadLine().Split();
+        string a = arr[0], b = arr[1];
+        Console.WriteLine(
+            int.Parse(a) < int.Parse(b) ?
+            new string(a[0], int.Parse(b)) : new string(b[0], int.Parse(a)));
+    }
+}
