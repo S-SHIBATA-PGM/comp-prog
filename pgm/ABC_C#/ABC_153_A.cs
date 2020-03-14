8b0cb134c010af625d2977732f075c8745240c2d
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-14 12:11:34 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_153_A.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String[] arr = Console.ReadLine().Split();
+        int H = int.Parse(arr[0]), A = int.Parse(arr[1]);
+        Console.WriteLine(Math.Ceiling((double)H / A));
+    }
+}
