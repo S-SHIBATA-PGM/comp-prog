b433da6416e934cc8bdeb4b5cbdb51c256ba358c
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-14 20:37:15 +0900

    Performance challenge.

--- pgm/ABC_OpenCOBOL/ABC_152_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_152_B.cob
@@ -6,20 +6,20 @@ WORKING-STORAGE SECTION.
 01 LN             PIC X(3).
 01 a              PIC 9(1).
 01 b              PIC 9(1).
+01 x              PIC 9(1).
 01 i              PIC 9(2).
 01 ans            PIC X(9).
 
 PROCEDURE DIVISION.
   ACCEPT LN.
   UNSTRING LN DELIMITED BY SPACE INTO a b.
-  IF a < b
-      PERFORM VARYING i FROM 1 BY 1 UNTIL b < i
-          MOVE a TO ans(i:1)
-      END-PERFORM
-  ELSE
-      PERFORM VARYING i FROM 1 BY 1 UNTIL a < i
-          MOVE b TO ans(i:1)
-      END-PERFORM
+  IF b < a
+      MOVE a TO x
+      MOVE b TO a
+      MOVE x TO b
   END-IF.
+  PERFORM VARYING i FROM 1 BY 1 UNTIL b < i
+      MOVE a TO ans(i:1)
+  END-PERFORM
   DISPLAY ans.
   STOP RUN.
