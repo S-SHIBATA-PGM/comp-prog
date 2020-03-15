b6e306d7e2eb86367e8c14015cf5744c15bb2adb
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-15 14:15:04 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_150_A.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String[] arr = Console.ReadLine().Split();
+        int K = int.Parse(arr[0]), X = int.Parse(arr[1]);
+        Console.WriteLine(X <= 500 * K ? "Yes" : "No");
+    }
+}
