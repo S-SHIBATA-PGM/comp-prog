e9b5cf7a82fbf119f1d812bb4417c46b0ebfb259
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-15 14:49:36 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_150_B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f) (begin (ALL), end (ALL), ##__VA_ARGS__);                    \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int N;
+    cin >> N;
+    string S;
+    cin >> S;
+    int ans = 0;
+    REP (i, N - 2) if (S.substr (i, 3) == "ABC") ans += 1;
+    cout << ans << endl;
+    return 0;
+}
