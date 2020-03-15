9a240cb3f510ca5b612bd4fddce05a8263d8cfdd
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-15 17:52:06 +0900

    Performance challenge.

--- pgm/ABC_Visual_Basic/ABC_156_A.vb
+++ pgm/ABC_Visual_Basic/ABC_156_A.vb
@@ -1,7 +1,7 @@
 Class Main
     Public Shared Sub Main()
         Dim N, R As Integer
-        Dim arr(2) As String
+        Dim arr() As String
         arr = Split(Console.ReadLine())
         N = arr(0) : R = arr(1)
         Console.WriteLine(if(10 <= N, R, R + 100 * (10 - N)))
