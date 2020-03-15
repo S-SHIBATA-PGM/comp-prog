cd1e23ae4ea69994d80edca54ec3b1efe9679517
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-15 15:19:50 +0900

    Performance challenge.

---
+++ pgm/ABC_C#/ABC_150_B.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        string S = Console.ReadLine();
+        Regex reg = new Regex("ABC");
+        Console.WriteLine(reg.Matches(S).Count);
+    }
+}
