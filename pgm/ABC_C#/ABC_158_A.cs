5c7349e2a53c5c29a9a68c8710c0826bd98e0463
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-19 08:38:12 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_158_A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        String arr = Console.ReadLine();
+        HashSet<char> S = new HashSet<char>(arr);
+        Console.WriteLine(1 < S.Count ? "Yes" : "No");
+    }
+}
