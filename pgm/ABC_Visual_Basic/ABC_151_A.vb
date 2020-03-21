39256dbb3e952ae9cadd6672405d6407503cc9d5
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 12:33:22 +0900

    Performance challenge.

--- pgm/ABC_Visual_Basic/ABC_151_A.vb
+++ pgm/ABC_Visual_Basic/ABC_151_A.vb
@@ -1,6 +1,6 @@
 Public Class Main
     Public Shared Sub Main(ByVal args() As String)
-        Dim C as String
+        Dim C as Char
         C = Console.ReadLine()
         Console.WriteLine(Chr(Asc(C) + 1))
     End Sub
