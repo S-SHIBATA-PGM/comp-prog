ae9b43f96b0dac2c6ebb56ade9730e937ede1086
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 14:22:00 +0900

    Performance challenge.

--- pgm/ABC_Visual_Basic/ABC_152_B.vb
+++ pgm/ABC_Visual_Basic/ABC_152_B.vb
@@ -1,13 +1,14 @@
+
 Public Class Main
     Public Shared Sub Main(ByVal args() As String)
-        Dim a, b As Char
+        Dim a, b As Integer
         Dim arr(2) As String
         arr = Split(Console.ReadLine())
         a = arr(0) : b = arr(1)
         If a < b Then
-            Console.WriteLine(New String(a, Integer.Parse(b)))
+            Console.WriteLine(New String(arr(0)(0), b))
         Else
-            Console.WriteLine(New String(b, Integer.Parse(a)))
+            Console.WriteLine(New String(arr(1)(0), a))
         End If
     End Sub
 End Class
