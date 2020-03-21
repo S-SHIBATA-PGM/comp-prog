28204b98bee988a9df996ba0eaff2a8ded1f2f90
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 11:42:48 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_154_A.js
+++ pgm/ABC_JavaScript/ABC_154_A.js
@@ -1,3 +1,4 @@
+"use strict";
 function main(input) {
     var line = input.split(/\r?\n/g);
     var arr = line[0].split(" ");
@@ -5,11 +6,8 @@ function main(input) {
     arr = line[1].split(" ")
     var A = parseInt(arr[0]), B = parseInt(arr[1]);
     const U = line[2];
-    if (S === U) {
-        A--;
-    } else {
-        B--;
-    }
+    if (S == U) A--;
+    else B--;
     console.log(A + " " + B);
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
