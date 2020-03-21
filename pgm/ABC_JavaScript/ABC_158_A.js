98195a2c26b91e2a3bf5e6335fbd17402ff58736
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 10:36:13 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_158_A.js
+++ pgm/ABC_JavaScript/ABC_158_A.js
@@ -1,9 +1,7 @@
 "use strict";
 function main(input) {
     let line = input.split(/\r?\n/g);
-    let S = new Set(line[0].slice(0, line.length));
-    console.log(S);
-    console.log(S.size);
+    let S = new Set(line[0].slice(0, line[0].length));
     console.log(1 < S.size ? "Yes" : "No");
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
