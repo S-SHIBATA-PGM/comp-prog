e46c78cbf501f06132eccd8680e3d1b87a45d41c
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 10:08:46 +0900

    Performance challenge.

--- pgm/ABC_Java/ABC_152_B.java
+++ pgm/ABC_Java/ABC_152_B.java
@@ -1,16 +1,19 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static String repeat(String str, int n) {
-        return String.join("", Collections.nCopies(n, str));
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = reader.readLine().split(" ");
         int a = Integer.parseInt(arr[0]);
         int b = Integer.parseInt(arr[1]);
-        System.out.println(a < b ? repeat(arr[0], b) : repeat(arr[1], a));
+        int ans = 0;
+        if (a < b) {
+            for (int i = 0; i < b; i++)
+                ans = 10 * ans + a;
+        } else {
+            for (int i = 0; i < a; i++)
+                ans = 10 * ans + b;
+        }
+        System.out.println(ans);
     }
 }
