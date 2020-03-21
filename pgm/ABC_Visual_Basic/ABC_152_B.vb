82112a097923f84f2af5506c1481c0ccb02778c5
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 12:19:44 +0900

    Performance challenge.

--- pgm/ABC_Visual_Basic/ABC_152_B.vb
+++ pgm/ABC_Visual_Basic/ABC_152_B.vb
@@ -4,7 +4,7 @@ Public Class Main
         Dim arr(2) As String
         arr = Split(Console.ReadLine())
         a = arr(0) : b = arr(1)
-        If Integer.Parse(a) < Integer.Parse(b) Then
+        If a < b Then
             Console.WriteLine(New String(a, Integer.Parse(b)))
         Else
             Console.WriteLine(New String(b, Integer.Parse(a)))
