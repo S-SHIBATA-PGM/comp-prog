afb214ef67476a9ec2578a7e000bbd896df513f0
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 10:54:02 +0900

    Performance challenge.

--- pgm/ABC_Java/ABC_154_B.java
+++ pgm/ABC_Java/ABC_154_B.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line = reader.readLine();
-        for (int i = 0; i < line.length(); i++) {
-            System.out.print("x");
-        }
-        System.out.println();
+        String ans = "";
+        for (int i = 0; i < line.length(); i++)
+            ans = ans.concat("x");
+        System.out.println(ans);
     }
 }
