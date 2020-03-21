67e653e8d0c0d596ed5f3db5cf1dd6d0c972e751
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 14:02:34 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_153_A.js
+++ pgm/ABC_JavaScript/ABC_153_A.js
@@ -1,6 +1,6 @@
 function main(input) {
     var arr = input.split(" ");
     const H = arr[0], A = arr[1];
-    console.log(H % A ? (H / A | 0) + 1 : H / A);
+    console.log(Math.ceil(H / A));
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
