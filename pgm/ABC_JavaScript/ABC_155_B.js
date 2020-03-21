5ee79c2d683256317e4e6400f40b80ce09a05058
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 19:20:44 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_155_B.js
+++ pgm/ABC_JavaScript/ABC_155_B.js
@@ -1,9 +1,10 @@
+"use strict";
 function main(input) {
-    var line = input.split(/\r?\n/g);
+    let line = input.split(/\r?\n/g);
     const A = line[1].split(" ").map(Number);
-    var flg = true;
-    for (var i in A)
-        if (!(A[i] % 2) && (A[i] % 3) && (A[i] % 5)) {
+    let flg = true;
+    for (let i in A)
+        if (!(A[i] % 2) && A[i] % 3 && A[i] % 5) {
             flg = false;
             break;
         }
