2eb63b8454ea47ac64a48538713b5383dad8a97b
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-21 10:03:57 +0900

    Arrangement.

--- pgm/ABC_Java/ABC_152_B.java
+++ pgm/ABC_Java/ABC_152_B.java
@@ -1,21 +1,16 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static String repeat(String str, int n) {
+        return String.join("", Collections.nCopies(n, str));
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] arr = reader.readLine().split(" ");
         int a = Integer.parseInt(arr[0]);
         int b = Integer.parseInt(arr[1]);
-        int ans = 0;
-        if (a < b) {
-            ans = a;
-            for (int i = 0; i < b - 1; i++)
-                ans = 10 * ans + a;
-        } else {
-            ans = b;
-            for (int i = 0; i < a - 1; i++)
-                ans = 10 * ans + b;
-        }
-        System.out.println(ans);
+        System.out.println(a < b ? repeat(arr[0], b) : repeat(arr[1], a));
     }
 }
