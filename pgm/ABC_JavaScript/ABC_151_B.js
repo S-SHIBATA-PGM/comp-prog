6d7b19c3691146e14e3f4a9a77840a01bf6052a8
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-03-31 17:54:18 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_151_B.js
+++ pgm/ABC_JavaScript/ABC_151_B.js
@@ -3,7 +3,7 @@ function main(input) {
     let line = input.split("\n");
     let arr = line[0].split(" ").map(Number);
     const N = arr[0], K = arr[1], M = arr[2];
-    arr = line[1].split(" ").map(Number)
+    arr = line[1].split(" ").map(Number);
     let accum = arr.reduce((A, x) => A + x);
     let ans = N * M - accum;
     console.log(ans < 0 ? 0 : K < ans ? -1 : ans);
