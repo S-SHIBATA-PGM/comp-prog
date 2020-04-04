f018a817eb39987e8e2ee69c002e6650447f7115
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-04-04 16:54:06 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_160_A.js
+++ pgm/ABC_JavaScript/ABC_160_A.js
@@ -1,7 +1,7 @@
 "use strict";
 function main(input) {
-    let S = input.replace("\n", "");
-    let flg = S.charAt(2) === S.charAt(3) && S.charAt(4) === S.charAt(5)
+    let S = input.replace("\n", "").split("");
+    let flg = S[2] === S[3] && S[4] === S[5];
     console.log(flg ? "Yes" : "No");
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
