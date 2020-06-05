9f576169c496f53f9ebca0c7d5239afe9aed754d
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-06-06 00:14:48 +0900

    Arrangement.

--- pgm/ABC_C#/ABC_168_A.cs
+++ pgm/ABC_C#/ABC_168_A.cs
@@ -5,9 +5,7 @@ class Program
 {
     static void Main()
     {
-        String[] arr = Console.ReadLine().Split();
-        int N;
-        N = int.Parse(arr[0]);
+        int N = int.Parse(Console.ReadLine());
         if (new int[] { 2, 4, 5, 7, 9 }.Contains(N % 10))
             Console.WriteLine("hon");
         else if (new int[] { 0, 1, 4, 6, 8 }.Contains(N % 10))
