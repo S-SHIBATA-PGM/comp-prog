d5129dcecab7ada66733a0b626a1e433c2415e94
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-06-26 09:48:16 +0900

    Arrangement.

---
+++ pgm/ABC_JavaScript/ABC_145_B.js
@@ -0,0 +1,9 @@
+"use strict";
+function main(input) {
+    const line = input.split("\n");
+    const N = parseInt(line[0]);
+    const S = line[1];
+    console.log(!(N % 2) ? S.slice(0, N / 2) == S.slice(N / 2) ? "Yes" : "No" : "No");
+    return;
+}
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
