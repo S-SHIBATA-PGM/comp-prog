76c651a15dc06de289f2f5b56f15e005d690004d
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-06-26 13:20:45 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_171_A.cs
@@ -0,0 +1,14 @@
+using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        char a = Console.ReadLine()[0];
+        Console.WriteLine(a <= 'Z' ? "A" : "a");
+        return;
+    }
+}
