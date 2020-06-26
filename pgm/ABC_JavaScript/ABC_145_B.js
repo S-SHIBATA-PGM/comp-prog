c395fa2fed70ce6f47f2adebd67900c558db0127
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-06-26 09:57:57 +0900

    Performance challenge.

--- pgm/ABC_JavaScript/ABC_145_B.js
+++ pgm/ABC_JavaScript/ABC_145_B.js
@@ -3,7 +3,7 @@ function main(input) {
     const line = input.split("\n");
     const N = parseInt(line[0]);
     const S = line[1];
-    console.log(!(N % 2) ? S.slice(0, N / 2) == S.slice(N / 2) ? "Yes" : "No" : "No");
+    console.log(!(N % 2) ? S.slice(0, N / 2) === S.slice(N / 2) ? "Yes" : "No" : "No");
     return;
 }
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
