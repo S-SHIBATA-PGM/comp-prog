cc26cc5df200f3614b4695c288fc0fea84c2da4a
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-06-26 11:09:17 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_145_A.cs
@@ -0,0 +1,14 @@
+using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int r = int.Parse(Console.ReadLine());
+        Console.WriteLine(Math.Pow(r, 2));
+        return;
+    }
+}
