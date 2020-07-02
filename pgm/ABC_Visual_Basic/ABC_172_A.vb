095ff5eeb6ceea3b3bf170a45a96b0396c7d3b52
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-07-02 22:37:03 +0900

    Arrangement.

---
+++ pgm/ABC_Visual_Basic/ABC_172_A.vb
@@ -0,0 +1,12 @@
+' Imports System.Collections.Generic
+' Imports System.Linq
+' Imports System.Text
+' Imports System.Text.RegularExpressions
+
+Class Main
+    Public Shared Sub Main()
+        Dim a As Integer = Console.ReadLine()
+        Console.WriteLine(a + a ^ 2 + a ^ 3)
+        Return
+    End Sub
+End Class
