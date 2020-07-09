07049a0b0610d611f9cf5616bd0fc2bc6558116c
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-07-09 23:26:25 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_140_B.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+// using System.Globalization;
+using System.IO;
+using System.Linq;
+// using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        FastScanner sc = new FastScanner(Console.OpenStandardInput());
+        int N = sc.nextInt();
+        int[] A = sc.intArray(N);
+        int[] B = sc.intArray(N);
+        int[] C = sc.intArray(N - 1);
+        int ans = B.Sum();
+        for (int i = 1; i < N; i++) ans += A[i - 1] + 1 == A[i] ? C[A[i - 1] - 1] : 0;
+        Console.WriteLine(ans);
+        return;
+    }
+}
+
+// FastScanner start
+public class FastScanner
+{
+    public readonly Stream str;
+    private readonly byte[] buf = new byte[1024];
+    private int len, ptr;
+    public bool isEof = false;
+    public FastScanner(Stream stream)
+    {
+        str = stream;
+    }
+    private T[] enumerate<T>(int n, Func<T> f)
+    {
+        var a = new T[n];
+        for (int i = 0; i < n; ++i) a[i] = f();
+        return a;
+    }
+    public char nextChar()
+    {
+        byte b = 0;
+        do
+        {
+            b = read();
+        }
+        while ((b < 33 || 126 < b) && !isEof);
+        return (char)b;
+    }
+    public int nextInt()
+    {
+        return (isEof) ? int.MinValue : (int)nextLong();
+    }
+    public long nextLong()
+    {
+        if (isEof)
+        {
+            return long.MinValue;
+        }
+        long ret = 0;
+        byte b = 0;
+        var ng = false;
+        do
+        {
+            b = read();
+        }
+        while (b != '-' && (b < '0' || '9' < b));
+        if (b == '-')
+        {
+            ng = true;
+            b = read();
+        }
+        for (; true; b = read())
+        {
+            if (b < '0' || '9' < b)
+            {
+                return ng ? -ret : ret;
+            }
+            else ret = ret * 10 + b - '0';
+        }
+    }
+    public int[] intArray(int n)
+    {
+        return enumerate(n, nextInt);
+    }
+    public long[] longArray(int n)
+    {
+        return enumerate(n, nextLong);
+    }
+    public char[] charArray(int n)
+    {
+        return enumerate(n, nextChar);
+    }
+    private byte read()
+    {
+        if (isEof)
+        {
+            return 0;
+        }
+        if (ptr >= len)
+        {
+            ptr = 0;
+            if ((len = str.Read(buf, 0, 1024)) <= 0)
+            {
+                isEof = true;
+                return 0;
+            }
+        }
+        return buf[ptr++];
+    }
+}
+// FastScanner end
