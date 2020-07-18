65f8e32c779508ef26b1d552e8c8226f38a9492b
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-07-18 19:49:02 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_138_A.cs
@@ -0,0 +1,15 @@
+using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        string s = Console.ReadLine();
+        Console.WriteLine(3200 <= a ? s : "red");
+        return;
+    }
+}
