267e40f530e0bd1a0fbbd5de135b6a68b10d92b9
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-07-21 23:40:35 +0900

    Arrangement.

---
+++ pgm/ABC_Visual_Basic/ABC_137_A.vb
@@ -0,0 +1,15 @@
+' Imports System.Collections.Generic
+' Imports System.Linq
+' Imports System.Text
+' Imports System.Text.RegularExpressions
+
+Class Main
+    Public Shared Sub Main()
+        Dim A, B As Integer
+        Dim arr() As String
+        arr = Split(Console.ReadLine())
+        A = arr(0) : B = arr(1)
+        Console.WriteLine(Math.Max(A + B, Math.Max(A - B, A * B)))
+        Return
+    End Sub
+End Class
