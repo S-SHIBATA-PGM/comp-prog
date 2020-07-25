46dd58149d7d723d90a37247c235376c55a5365a
ShinjiSHIBATA <sshibata0843@gmail.com>
2020-07-25 20:12:39 +0900

    Arrangement.

---
+++ pgm/ABC_OpenCOBOL/ABC_147_A.cob
@@ -0,0 +1,36 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 ln         PIC X(400).
+01 maxlen     PIC 9(10) VALUE 100 COMP.
+01 cur        PIC 9(10) VALUE 1 COMP.
+01 i          PIC 9(10) VALUE 1 COMP.
+01 j          PIC 9(10) COMP.
+01 len        PIC 9(10) COMP.
+01 accum      PIC 9(10) VALUE ZERO COMP.
+01 N          PIC 9(10).
+01 A1.
+  03 A11 OCCURS 100 DEPENDING ON N.
+      05 A PIC 9(10) COMP.
+
+PROCEDURE DIVISION.
+  MOVE 3 TO N.
+  ACCEPT ln.
+  MOVE N TO maxlen.
+  PERFORM maxlen TIMES
+    PERFORM VARYING j FROM cur BY 1 UNTIL ln(j:1) = SPACE
+    END-PERFORM
+    COMPUTE len = j - cur
+    MOVE ln(cur:len) TO A(i)
+    ADD A(i) TO accum
+    COMPUTE cur = j + 1
+    ADD 1 TO i
+  END-PERFORM.
+  IF 22 <= accum THEN
+    DISPLAY "bust"
+  ELSE
+    DISPLAY "win"
+  END-IF.
+  STOP RUN.
