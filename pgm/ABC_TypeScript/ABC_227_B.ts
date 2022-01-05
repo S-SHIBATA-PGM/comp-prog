72a98e561ceb89ba7b900dc0f519b12fd0893c92
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-01-05 21:01:43 +0900

    Arrangement.

---
+++ pgm/ABC_TypeScript/ABC_227_B.ts
@@ -0,0 +1,47 @@
+import fs = require("fs");
+
+class Solver
+{
+    S: number[];
+    line: string[];
+
+    N: number;
+    input: string;
+    ub: number = 1000;
+
+    constructor()
+    {
+        this.input = fs.readFileSync("/dev/stdin", "utf8");
+        this.line = this.input.split("\n");
+        this.N = parseInt(this.line[0], 10);
+        this.S = this.line[1].split(" ").map((str) => parseInt(str, 10));
+    }
+
+    solve()
+    {
+        let ans = 0;
+        for (let i = 0; i < this.N; i++)
+        {
+            let flg = false;
+            for (let a = 1; a <= this.ub; a++)
+            {
+                for (let b = 1; b <= this.ub; b++)
+                {
+                    const area = 4 * a * b + 3 * a + 3 * b;
+                    if (area === this.S[i])
+                    {
+                        flg = true;
+                        break;
+                    }
+                }
+                if (flg) break;
+            }
+            if (!flg) ans++;
+        }
+        console.log(ans);
+        return 0;
+    }
+}
+
+const s = new Solver();
+s.solve();
