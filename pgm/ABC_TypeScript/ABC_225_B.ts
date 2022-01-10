13b69e884d7df8f5f1dcd5b93d4ae1c2a730c9b5
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-01-10 09:26:44 +0900

    Performance challenge.

--- pgm/ABC_TypeScript/ABC_225_B.ts
+++ pgm/ABC_TypeScript/ABC_225_B.ts
@@ -3,38 +3,44 @@ import fs = require("fs");
 const NO: string = "No";
 const YES: string = "Yes";
 
-function arrays(arr: string[], start: number, end: number): string[][]
+class FastScanner
 {
-    return arr.slice(start, end).map((str) => str.split(" "));
-}
-
-function integers(x: string[][] | string[] | string): any
-{
-    return x instanceof (Array) ? x.map((x) => integers(x)) : retInt(x);
-}
-
-function retInt(str: string)
-{
-    return parseInt(str, 10);
+    inputs: string[];
+    index = 0;
+    constructor()
+    {
+        this.inputs = fs.readFileSync("/dev/stdin", "utf8").split(/ |\n/);
+    }
+    bigint(): BigInt
+    {
+        return BigInt(this.inputs[this.index++]);
+    }
+    integer(): number
+    {
+        return parseInt(this.inputs[this.index++], 10);
+    }
+    number(): number
+    {
+        return Number(this.inputs[this.index++]);
+    }
+    string()
+    {
+        return this.inputs[this.index++];
+    }
 }
 
 class Solver
 {
-    a: number[][];
-    arr: string[][];
-    grp: Array<number>;
-    line: string[];
+    fs: FastScanner;
+
+    grp: number[];
 
     N: number;
-    input: string;
 
     constructor()
     {
-        this.input = fs.readFileSync("/dev/stdin", "utf8");
-        this.line = this.input.split("\n");
-        this.N = parseInt(this.line[0]);
-        this.arr = arrays(this.line, 1, this.N);
-        this.a = integers(this.arr);
+        this.fs = new FastScanner();
+        this.N = this.fs.integer();
         this.grp = new Array(this.N).fill(0);
     }
 
@@ -42,8 +48,8 @@ class Solver
     {
         for (let i = 0; i < this.N - 1; i++)
         {
-            this.grp[--this.a[i][0]]++;
-            this.grp[--this.a[i][1]]++;
+            this.grp[this.fs.integer() - 1]++;
+            this.grp[this.fs.integer() - 1]++;
         }
 
         console.log(
