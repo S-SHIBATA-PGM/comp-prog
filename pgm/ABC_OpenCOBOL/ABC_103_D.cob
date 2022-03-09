9e8de8da68e0c5c523a801ed96af4d3d42faaee8
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-03-10 07:50:03 +0900

    Arrangement.

---
+++ pgm/ABC_OpenCOBOL/ABC_103_D.cob
@@ -0,0 +1,38 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 N          PIC 9(6).
+01 M          PIC 9(6).
+01 ans        PIC X(6).
+01 ab1.
+   03 ab OCCURS 100000 DEPENDING M.
+      05 a    PIC 9(6).
+      05 b    PIC 9(6).
+01 cnt        PIC 9(6).
+01 done       PIC 9(6) VALUE ZERO.
+01 i          PIC 9(6) BINARY.
+01 ln         PIC X(13).
+01 zs         PIC Z(7).
+
+PROCEDURE DIVISION.
+  ACCEPT ln.
+  UNSTRING ln DELIMITED BY SPACE INTO N M.
+  PERFORM VARYING i FROM 1 BY 1 UNTIL M < i
+    ACCEPT ln
+    UNSTRING ln DELIMITED BY SPACE INTO a(i) b(i)
+  END-PERFORM.
+  SORT ab ASCENDING b.
+  MOVE ZERO TO done.
+  PERFORM VARYING i FROM 1 BY 1 UNTIL M < i
+    IF done < a(i) THEN
+      ADD 1 TO cnt
+      MOVE b(i) TO done
+      SUBTRACT 1 FROM done
+    END-IF
+  END-PERFORM.
+  MOVE cnt TO zs.
+  DISPLAY FUNCTION TRIM(zs).
+  STOP RUN.
+
