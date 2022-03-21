c5ffc3ec0252a467f32ddc4b73a0eccc5fd0542e
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-03-22 07:11:15 +0900

    Arrangement.

---
+++ pgm/ABC_OpenCOBOL/ABC_084_B.cob
@@ -0,0 +1,32 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 A          PIC 9(1).
+01 B          PIC 9(1).
+01 S          PIC X(10).
+01 flg        PIC 9(1) VALUE 1.
+01 i          PIC 9(2).
+01 ln         PIC X(11).
+
+PROCEDURE DIVISION.
+  ACCEPT ln.
+  ACCEPT S.
+  UNSTRING ln DELIMITED BY SPACE INTO A B.
+  PERFORM VARYING i FROM 1 BY 1 UNTIL A + B + 1 < i
+    IF A + 1 = i AND S(i:1) NOT = '-' THEN
+      MOVE 0 TO flg
+    ELSE
+      IF A + 1 NOT = i AND S(i:1) = '-' THEN
+        MOVE 0 TO flg
+      END-IF
+    END-IF
+  END-PERFORM.
+  IF flg = 1 THEN
+    DISPLAY "Yes"
+  ELSE
+    DISPLAY "No"
+  END-IF.
+  STOP RUN.
+
