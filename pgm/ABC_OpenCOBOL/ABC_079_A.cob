a26d4761a2aaa0a4d29c4b2d1b0ac3c90cdb26eb
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-03-24 07:33:10 +0900

    Arrangement.

---
+++ pgm/ABC_OpenCOBOL/ABC_079_A.cob
@@ -0,0 +1,35 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 D          PIC 9(4).
+01 N          PIC 9(4).
+01 R          PIC 9(1).
+01 bk         PIC 9(1).
+01 cnt        PIC 9(1).
+01 mx         PIC 9(1).
+
+PROCEDURE DIVISION.
+  ACCEPT N.
+  MOVE N TO D.
+  DIVIDE 10 INTO D GIVING D REMAINDER bk.
+  MOVE 0 TO mx.
+  MOVE 1 TO cnt.
+  PERFORM UNTIL D = 0
+    DIVIDE 10 INTO D GIVING D REMAINDER R
+    IF bk = R THEN
+      ADD 1 TO cnt
+    ELSE
+      MOVE 1 TO cnt
+    END-IF
+    MOVE R TO bk
+    COMPUTE mx = FUNCTION MAX(mx, cnt)
+  END-PERFORM.
+  IF 3 <= mx THEN
+    DISPLAY "Yes"
+  ELSE
+    DISPLAY "No"
+  END-IF.
+  STOP RUN.
+
---
+++ pgm/ABC_OpenCOBOL/ABC_079_B.cob
@@ -0,0 +1,32 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 N          PIC 9(2).
+01 i          PIC 9(2).
+01 lucas      PIC 9(18).
+01 ppr        PIC 9(18).
+01 pr         PIC 9(18).
+01 tmp        PIC 9(18).
+01 zs         PIC Z(18)9.
+
+PROCEDURE DIVISION.
+  ACCEPT N.
+  PERFORM LUCASNUMBER.
+  MOVE lucas TO zs.
+  DISPLAY FUNCTION TRIM(zs).
+  STOP RUN.
+
+LUCASNUMBER SECTION.
+  MOVE 2 TO ppr.
+  MOVE 1 TO pr.
+  PERFORM VARYING i FROM 2 BY 1 UNTIL N < i
+    ADD ppr pr GIVING lucas
+    MOVE pr TO ppr
+    MOVE lucas TO pr
+  END-PERFORM.
+  IF N = 1 THEN
+    MOVE 1 TO lucas
+  END-IF.
+
