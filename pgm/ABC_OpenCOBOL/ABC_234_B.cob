25367b0848d891950c329cbeb7797255699c8931
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-02 08:37:31 +0900

    Arrangement.

---
+++ pgm/ABC_OpenCOBOL/ABC_234_B.cob
@@ -0,0 +1,35 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 N          PIC 9(10).
+01 ans        FLOAT-LONG VALUE -1.
+01 cur        FLOAT-LONG.
+01 i          PIC 9(10) COMP.
+01 j          PIC 9(10) COMP.
+01 ln         PIC X(30).
+01 xy1.
+   03 xy      OCCURS 100 DEPENDING N.
+      05 x    PIC S9(10).
+      05 y    PIC S9(10).
+01 zs         PIC Z(9)9.9(10).
+
+PROCEDURE DIVISION.
+  ACCEPT N.
+  PERFORM VARYING i FROM 1 BY 1 UNTIL N < i
+    ACCEPT ln
+    UNSTRING ln DELIMITED SPACE INTO x(i) y(i)
+  END-PERFORM.
+  PERFORM VARYING i FROM 1 BY 1 UNTIL N < i
+    ADD 1 TO i GIVING j
+    PERFORM VARYING j FROM j BY 1 UNTIL N < j
+      COMPUTE cur =
+      FUNCTION SQRT((X(j) - x(i)) ** 2 + (y(j) - y(i)) ** 2)
+      IF ans < cur MOVE cur TO ans
+    END-PERFORM
+  END-PERFORM.
+  MOVE ans TO zs.
+  DISPLAY FUNCTION TRIM(zs).
+  STOP RUN.
+
