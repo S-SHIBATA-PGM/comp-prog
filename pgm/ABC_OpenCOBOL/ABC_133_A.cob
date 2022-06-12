3a39752dd243c59c295586ba5d12b20f7199879e
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-12 10:42:51 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_133_A.cob
+++ pgm/ABC_OpenCOBOL/ABC_133_A.cob
@@ -3,11 +3,11 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 ln         PIC X(30).
-01 N          PIC 9(10).
 01 A          PIC 9(10).
 01 B          PIC 9(10).
+01 N          PIC 9(10).
 01 ans        PIC 9(10).
+01 ln         PIC X(30).
 01 zs         PIC Z(9)9.
 
 PROCEDURE DIVISION.
@@ -17,3 +17,4 @@ PROCEDURE DIVISION.
   MOVE ans TO zs.
   DISPLAY FUNCTION TRIM(zs).
   STOP RUN.
+
