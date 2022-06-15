cc18b4d8c0c92431e7acfe4e5f786e80e5fd2f8f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-16 07:27:57 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_149_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_149_B.cob
@@ -3,13 +3,13 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 ln         PIC X(42).
 01 A          PIC 9(19).
 01 B          PIC 9(19).
 01 K          PIC 9(19).
+01 ans        PIC X(40).
+01 ln         PIC X(42).
 01 mna        PIC 9(19).
 01 mnb        PIC 9(19).
-01 ans        PIC X(40).
 01 zsa        PIC Z(18)9.
 01 zsb        PIC Z(18)9.
 
@@ -26,3 +26,4 @@ PROCEDURE DIVISION.
   STRING FUNCTION TRIM(zsa) SPACE FUNCTION TRIM(zsb) INTO ans.
   DISPLAY FUNCTION TRIM(ans).
   STOP RUN.
+
