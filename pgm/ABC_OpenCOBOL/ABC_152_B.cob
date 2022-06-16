60dfaa90f626ba87f781d46f575321b0e02930ab
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-17 06:08:49 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_152_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_152_B.cob
@@ -1,14 +1,16 @@
 IDENTIFICATION DIVISION.
 PROGRAM-ID. PROGRAM_ID.
+
 DATA DIVISION.
 WORKING-STORAGE SECTION.
 01 LN             PIC X(10).
 01 a              PIC 9(1).
-01 ca             PIC X(1).
-01 b              PIC 9(1).
-01 n              PIC 9(10).
-01 i              PIC 9(10).
 01 ans            PIC X(10).
+01 b              PIC 9(1).
+01 ca             PIC X(1).
+01 i              PIC 9(10).
+01 n              PIC 9(10).
+
 PROCEDURE DIVISION.
   ACCEPT LN.
   UNSTRING LN DELIMITED BY SPACE INTO a b.
@@ -24,3 +26,4 @@ PROCEDURE DIVISION.
   END-PERFORM.
   DISPLAY ans(1:n).
   STOP RUN.
+
