347b6ecf000f42578b4de597802a8a83c2e5a31e
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-18 10:26:02 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_157_A.cob
+++ pgm/ABC_OpenCOBOL/ABC_157_A.cob
@@ -1,12 +1,14 @@
-IDENTIFICATION                   DIVISION.
-PROGRAM-ID.                      PROGRAM_ID.
-DATA                             DIVISION.
-WORKING-STORAGE                  SECTION.
-01 N            PIC 9(3).
-01 ans          PIC Z9.
- 
-PROCEDURE                        DIVISION.
-    ACCEPT N.
-    COMPUTE ans = (N + 1) / 2.
-    DISPLAY ans.
-    STOP RUN.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 N          PIC 9(3).
+01 ans        PIC Z9.
+
+PROCEDURE DIVISION.
+  ACCEPT N.
+  COMPUTE ans = (N + 1) / 2.
+  DISPLAY FUNCTION TRIM(ans).
+  STOP RUN.
+
