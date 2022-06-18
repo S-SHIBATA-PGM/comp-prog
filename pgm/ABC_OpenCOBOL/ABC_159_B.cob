615d77279b546c9428b4374093bf79a7e9a87889
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-19 08:43:29 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_159_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_159_B.cob
@@ -3,13 +3,13 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 S              PIC X(100).
-01 X              PIC X(100).
-01 cur            PIC X(100).
-01 pre            PIC X(100).
-01 n              PIC 9(10).
-01 m              PIC 9(10).
-01 i              PIC 9(10).
+01 S          PIC X(100).
+01 X          PIC X(100).
+01 cur        PIC X(100).
+01 i          PIC 9(10).
+01 m          PIC 9(10).
+01 n          PIC 9(10).
+01 pre        PIC X(100).
 
 
 PROCEDURE DIVISION.
@@ -18,21 +18,22 @@ PROCEDURE DIVISION.
   MOVE n TO m.
   PERFORM REVERSESTR.
   IF S = cur
-      MOVE ALL SPACE TO cur
-      DIVIDE n BY 2 GIVING m
-      MOVE S(1 : m) TO X
-      PERFORM REVERSESTR
-      IF X = cur
-          DISPLAY "Yes"
-      ELSE
-          DISPLAY "No"
-      END-IF
-  ELSE
+    MOVE ALL SPACE TO cur
+    DIVIDE n BY 2 GIVING m
+    MOVE S(1 : m) TO X
+    PERFORM REVERSESTR
+    IF X = cur
+      DISPLAY "Yes"
+    ELSE
       DISPLAY "No"
+    END-IF
+  ELSE
+    DISPLAY "No"
   END-IF.
   STOP RUN.
 
 REVERSESTR SECTION.
   PERFORM VARYING i FROM 1 BY 1 UNTIL m < i
-      MOVE S(m - i + 1 : 1) TO cur(i : 1)
+    MOVE S(m - i + 1 : 1) TO cur(i : 1)
   END-PERFORM.
+
