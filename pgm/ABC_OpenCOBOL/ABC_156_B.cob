37b7a3bbb3c4d47baa3fd0509852914aa4804982
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-18 10:22:01 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_156_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_156_B.cob
@@ -3,19 +3,20 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 LN           PIC X(13).
-01 N            PIC 9(10).
 01 K            PIC 9(2).
-01 cnt          PIC 9(2).
+01 N            PIC 9(10).
 01 ans          PIC Z9.
+01 cnt          PIC 9(2).
+01 ln           PIC X(13).
 
 PROCEDURE DIVISION.
-  ACCEPT LN.
-  UNSTRING LN DELIMITED BY SPACE INTO N K.
+  ACCEPT ln.
+  UNSTRING ln DELIMITED SPACE INTO N K.
   MOVE 1 TO cnt.
   PERFORM UNTIL N < K ** cnt
-      ADD 1 TO cnt
+    ADD 1 TO cnt
   END-PERFORM.
   MOVE cnt TO ans.
-  DISPLAY ans.
+  DISPLAY FUNCTION TRIM(ans).
   STOP RUN.
+
