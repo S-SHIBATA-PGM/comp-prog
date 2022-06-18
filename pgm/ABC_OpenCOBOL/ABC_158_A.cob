87ce482d8618757cc8a2de66b1bf572439ecda54
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-18 10:38:04 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_158_A.cob
+++ pgm/ABC_OpenCOBOL/ABC_158_A.cob
@@ -3,15 +3,16 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 S              PIC X(3).
-01 cnt            PIC 9(10).
+01 S          PIC X(3).
+01 cnt        PIC 9(10).
 
 PROCEDURE DIVISION.
   ACCEPT S.
   INSPECT S TALLYING cnt FOR ALL "A".
   IF cnt = 1 OR 2
-      DISPLAY "Yes"
+    DISPLAY "Yes"
   ELSE
-      DISPLAY "No"
+    DISPLAY "No"
   END-IF.
   STOP RUN.
+
