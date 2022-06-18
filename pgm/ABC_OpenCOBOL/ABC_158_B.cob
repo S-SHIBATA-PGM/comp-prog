e8993077e7eb0546de2904d061999dc4507668dd
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-18 10:55:54 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_158_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_158_B.cob
@@ -3,25 +3,26 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 LN             PIC X(60).
-01 N              PIC 9(19).
-01 A              PIC 9(19).
-01 B              PIC 9(19).
-01 dv             PIC 9(19).
-01 rm             PIC 9(19).
-01 ans            PIC 9(19).
-01 ZS             PIC Z(18)9.
+01 A          PIC 9(19).
+01 B          PIC 9(19).
+01 N          PIC 9(19).
+01 ans        PIC 9(19).
+01 dv         PIC 9(19).
+01 ln         PIC X(60).
+01 rm         PIC 9(19).
+01 zs         PIC Z(18)9.
 
 PROCEDURE DIVISION.
-  ACCEPT LN.
-  UNSTRING LN DELIMITED BY SPACE INTO N A B.
+  ACCEPT ln.
+  UNSTRING ln DELIMITED SPACE INTO N A B.
   COMPUTE dv = N / (A + B).
   COMPUTE rm = N - dv * (A + B).
   IF A < rm
-      COMPUTE ans = A * dv + A
+    COMPUTE ans = A * dv + A
   ELSE
-      COMPUTE ans = A * dv + rm
+    COMPUTE ans = A * dv + rm
   END-IF.
-  MOVE ans TO ZS.
-  DISPLAY ZS.
+  MOVE ans TO zs.
+  DISPLAY FUNCTION TRIM(zs).
   STOP RUN.
+
