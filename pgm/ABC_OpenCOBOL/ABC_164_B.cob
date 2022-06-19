d50f3979bb05107f08627b08f21b63ff720ec9aa
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-20 08:14:32 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_164_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_164_B.cob
@@ -7,13 +7,13 @@ WORKING-STORAGE SECTION.
 01 B          PIC 9(10).
 01 C          PIC 9(10).
 01 D          PIC 9(10).
-01 ln         PIC X(15).
 01 ak         PIC 9(10).
+01 ln         PIC X(15).
 01 tk         PIC 9(10).
 
 PROCEDURE DIVISION.
   ACCEPT ln.
-  UNSTRING ln DELIMITED BY SPACE INTO A B C D.
+  UNSTRING ln DELIMITED SPACE INTO A B C D.
   COMPUTE tk = (C + B - 1) / B.
   COMPUTE ak = (A + D - 1) / D.
   IF tk <= ak
