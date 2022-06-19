8fcbf48ffeb09e1d11aecdaa3cca2e436c5e7a35
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-19 09:01:50 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_161_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_161_B.cob
@@ -3,24 +3,24 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 ln         PIC X(500).
-01 maxlen     PIC 9(10) VALUE 100 COMP.
+01 A1.
+   03 A11 OCCURS 100 DEPENDING ON N.
+      05 A    PIC 9(10) COMP.
+01 M          PIC 9(10).
+01 N          PIC 9(10).
+01 accum      PIC 9(10) VALUE ZERO COMP.
+01 ans        PIC 9(10).
 01 cur        PIC 9(10) VALUE 1 COMP.
 01 i          PIC 9(10) VALUE 1 COMP.
 01 j          PIC 9(10) COMP.
 01 len        PIC 9(10) COMP.
-01 accum      PIC 9(10) VALUE ZERO COMP.
-01 ans        PIC 9(10).
+01 ln         PIC X(500).
+01 maxlen     PIC 9(10) VALUE 100 COMP.
 01 zs         PIC Z(9)9.
-01 N          PIC 9(10).
-01 M          PIC 9(10).
-01 A1.
-   03 A11 OCCURS 100 DEPENDING ON N.
-      05 A    PIC 9(10) COMP.
 
 PROCEDURE DIVISION.
   ACCEPT ln.
-  UNSTRING ln DELIMITED BY SPACE INTO N M.
+  UNSTRING ln DELIMITED SPACE INTO N M.
   MOVE N TO maxlen.
   ACCEPT ln.
   PERFORM maxlen TIMES
@@ -32,10 +32,11 @@ PROCEDURE DIVISION.
     COMPUTE cur = j + 1
     ADD 1 TO i
   END-PERFORM.
-  SORT A11 ON DESCENDING A.
+  SORT A11 DESCENDING A.
   IF accum / (4 * M) <= A(M) THEN
-     DISPLAY "Yes"
+    DISPLAY "Yes"
   ELSE
-     DISPLAY "No"
+    DISPLAY "No"
   END-IF.
   STOP RUN.
+
