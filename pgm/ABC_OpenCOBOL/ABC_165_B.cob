cb355560a8db9665770ca117a7fc7e9a9cd080e9
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-20 07:52:01 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_165_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_165_B.cob
@@ -4,8 +4,8 @@ PROGRAM-ID. PROGRAM_ID.
 DATA DIVISION.
 WORKING-STORAGE SECTION.
 01 X          PIC 9(19).
-01 cur        PIC 9(19).
 01 ans        PIC 9(10).
+01 cur        PIC 9(19).
 01 zs         PIC Z(9)9.
 
 PROCEDURE DIVISION.
@@ -18,3 +18,4 @@ PROCEDURE DIVISION.
   MOVE ans TO zs.
   DISPLAY FUNCTION TRIM(zs).
   STOP RUN.
+
