1e02a8a2b48e1bb845c8f51e8b5c110d8e5a5e6d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-20 07:58:59 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_166_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_166_B.cob
@@ -3,20 +3,20 @@ PROGRAM-ID. PROGRAM_ID.
 
 DATA DIVISION.
 WORKING-STORAGE SECTION.
-01 N          PIC 9(10).
-01 K          PIC 9(10).
-01 idx        PIC 9(10).
-01 ln         PIC X(599).
-01 maxlen     PIC 9(10) VALUE 100 COMP.
-01 cur        PIC 9(10) VALUE 1 COMP.
-01 i          PIC 9(10) VALUE 1 COMP.
-01 j          PIC 9(10) COMP.
-01 len        PIC 9(10) COMP.
-01 ans        PIC 9(10).
-01 zs         PIC Z(9)9.
 01 A1.
    03 A11 OCCURS 100 DEPENDING ON N.
       05 A    PIC S9(10) VALUE 1.
+01 K          PIC 9(10).
+01 N          PIC 9(10).
+01 ans        PIC 9(10).
+01 cur        PIC 9(10) VALUE 1 COMP.
+01 i          PIC 9(10) VALUE 1 COMP.
+01 idx        PIC 9(10).
+01 j          PIC 9(10) COMP.
+01 len        PIC 9(10) COMP.
+01 ln         PIC X(599).
+01 maxlen     PIC 9(10) VALUE 100 COMP.
+01 zs         PIC Z(9)9.
 
 PROCEDURE DIVISION.
   ACCEPT ln.
@@ -43,3 +43,4 @@ PROCEDURE DIVISION.
   MOVE ans TO zs.
   DISPLAY FUNCTION TRIM(zs).
   STOP RUN.
+
