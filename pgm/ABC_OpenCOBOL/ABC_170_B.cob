44a40760c2525b90f21e051d3ea9102ed3b91297
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2022-06-21 07:38:05 +0900

    Arrangement.

--- pgm/ABC_OpenCOBOL/ABC_170_B.cob
+++ pgm/ABC_OpenCOBOL/ABC_170_B.cob
@@ -1,3 +1,17 @@
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 C          PIC S9(10).
+01 T          PIC S9(10).
+01 X          PIC 9(10).
+01 Y          PIC 9(10).
+01 ans        PIC X(30).
+01 ln         PIC X(30).
+01 zs         PIC Z(9)9.
+
+PROCEDURE DIVISION.
 *> C + T = X
 *> 2 * C + 4 * T = Y
 *> T = X - C
@@ -5,27 +19,14 @@
 *> - 2 * C = Y - 4 * X
 *> C = 2 * X - Y / 2
 *> T = -X + Y / 2
-IDENTIFICATION DIVISION.
-PROGRAM-ID. PROGRAM_ID.
-
-DATA DIVISION.
-WORKING-STORAGE SECTION.
-01 ln         PIC X(30).
-01 X          PIC 9(10).
-01 Y          PIC 9(10).
-01 C          PIC S9(10).
-01 T          PIC S9(10).
-01 ans        PIC X(30).
-01 zs         PIC Z(9)9.
-
-PROCEDURE DIVISION.
   ACCEPT ln.
-  UNSTRING ln DELIMITED BY SPACE INTO X Y.
+  UNSTRING ln DELIMITED SPACE INTO X Y.
   COMPUTE C = 2 * X - Y / 2.
   COMPUTE T = -X + Y / 2.
-  IF ZERO <= C AND ZERO <= T AND C + T = X AND 2 * C + 4 * T = Y THEN
+  IF ZERO <= C AND ZERO <= T AND C + T = X AND 2 * C + 4 * T = Y
     DISPLAY "Yes"
   ELSE
     DISPLAY "No"
   END-IF.
   STOP RUN.
+
