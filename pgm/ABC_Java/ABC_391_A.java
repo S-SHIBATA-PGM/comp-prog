ce7dded0d0f980e2c7e3b427e23307bede677d81
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-01 17:26:55 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_391_A.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        Map<String, String> NEWS = new HashMap<>();
+        NEWS.put ("N", "S");
+        NEWS.put ("E", "W");
+        NEWS.put ("W", "E");
+        NEWS.put ("S", "N");
+        String[] D = reader.readLine().split ("");
+        StringBuilder sb = new StringBuilder();
+        for (String c : D) {
+            sb.append (NEWS.get (c));
+        }
+        System.out.println (sb.toString());
+        return;
+    }
+}
