5b5992129cc3c0943dac31d447be041f480d223f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-06 22:00:02 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_395_A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f) (begin (ALL), end (ALL), ##__VA_ARGS__);                    \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int N;
+    cin >> N;
+    int A[N];
+    FOR (i, 0, N) cin >> A[i];
+    const string Yes = "Yes";
+    const string No = "No";
+    int pre = 0;
+    FOR (i, 0, N) {
+        if (A[i] <= pre) {
+            cout << No << endl;
+            return 0;
+        }
+        pre = A[i];
+    }
+    cout << Yes << endl;
+    return 0;
+}
