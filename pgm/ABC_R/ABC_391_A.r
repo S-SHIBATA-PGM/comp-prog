afe7f0045d9bb6944cdb65682e64c819b2933b9e
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-15 09:59:31 +0900

    Performance challenge.

--- pgm/ABC_R/ABC_391_A.r
+++ pgm/ABC_R/ABC_391_A.r
@@ -1,10 +1,14 @@
 con <- file("stdin", "r")
-D <- scan(con, what = character()) |> strsplit(split = "") |> unlist()
-NEWS <- c("N", "E", "W", "S")
-SWEN <- c("S", "W", "E", "N")
-v <- c(SWEN[NEWS == D[1]])
+D <- scan(file = con, what = character()) |> strsplit(split = "") |> unlist()
+close(con)
+NEWS <- new.env()
+NEWS[["N"]] <- "S"
+NEWS[["E"]] <- "W"
+NEWS[["W"]] <- "E"
+NEWS[["S"]] <- "N"
+v <- c(NEWS[[D[1]]])
 if (length(D) == 2) {
-    v <- c(v, SWEN[NEWS == D[2]])
+    v <- c(v, NEWS[[D[2]]])
 }
 v |> paste(collapse = "") |> cat()
 cat("\n")
