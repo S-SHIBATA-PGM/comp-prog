9445126292eb461197e3865ca4348f3ecb57114c
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-17 21:58:47 +0900

    Arrangement.

--- pgm/ABC_R/ABC_383_A.r
+++ pgm/ABC_R/ABC_383_A.r
@@ -6,8 +6,12 @@ T <- TV[rep(c(TRUE, FALSE), N)]
 V <- TV[rep(c(FALSE, TRUE), N)]
 time <- c(0, diff(T))
 water <- 0
-for (i in seq_along(V)) {
-    water = max(0, water - time[i]) + V[i];
+for (i in seq_len(N)) {
+    water <- water - time[i]
+    if (water < 0) {
+        water  <- 0
+    }
+    water <- water + V[i]
 }
 cat(water)
 cat("\n")
