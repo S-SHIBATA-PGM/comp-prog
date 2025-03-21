b1773bf4c79d65014a79bf122a2195389b334d83
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-21 20:25:37 +0900

    Arrangement.

--- pgm/ABC_Java/ABC_354_A.java
+++ pgm/ABC_Java/ABC_354_A.java
@@ -12,8 +12,8 @@ public class Main {
         int ans = 1;
         int base = 2;
         int e = 1;
-        int h = 0;
-        while (h < H) {
+        int h = 1;
+        while (h <= H) {
             e *= base;
             h += e;
             ans++;
@@ -21,22 +21,4 @@ public class Main {
         System.out.println (ans);
         return;
     }
-    // static double log2 (double x) {
-    //     if (Double.isNaN (x) || x < 0.0)
-    //         return Double.NaN;
-    //     if (x == Double.POSITIVE_INFINITY)
-    //         return Double.POSITIVE_INFINITY;
-    //     if (x == 0.0)
-    //         return Double.NEGATIVE_INFINITY;
-    //     int k = Math.getExponent (x);
-    //     if (k < Double.MIN_EXPONENT) {
-    //         k = Math.getExponent (x * 0x1.0p52) - 52;
-    //     }
-    //     if (k < 0) {
-    //         k++;
-    //     }
-    //     double s = Math.scalb (x, -k);
-    //     final double LOG2_E = 1.4426950408889634;
-    //     return k + LOG2_E * Math.log (s);
-    // }
 }
