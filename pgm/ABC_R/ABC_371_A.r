845c80fe6f7d9aac615c2385b740ee09947088ef
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-22 10:04:07 +0900

    Arrangement.

--- pgm/ABC_R/ABC_371_A.r
+++ pgm/ABC_R/ABC_371_A.r
@@ -7,39 +7,23 @@ gt <- ">"
 A <- "A"
 B <- "B"
 C <- "C"
+rank <- function(s, a, b, ABC) {
+    if (gt == s) {
+        if (match(a, ABC) < match(b, ABC)) {
+            ABC <- ABC[-match(b, ABC)]
+            ABC <- append(ABC, b, after = match(a, ABC) - 1)
+        }
+    } else {
+        if (match(b, ABC) < match(a, ABC)) {
+            ABC <- ABC[-match(a, ABC)]
+            ABC <- append(ABC, a, after = match(b, ABC) - 1)
+        }
+    }
+    return(ABC)
+}
 ABC <- c(A, B, C)
-if (gt == S[1]) {
-    if (match(A, ABC) < match(B, ABC)) {
-        ABC <- ABC[-match(B, ABC)]
-        ABC <- append(ABC, B, after = match(A, ABC) - 1)
-    }
-} else {
-    if (match(B, ABC) < match(A, ABC)) {
-        ABC <- ABC[-match(A, ABC)]
-        ABC <- append(ABC, A, after = match(B, ABC) - 1)
-    }
-}
-if (gt == S[2]) {
-    if (match(A, ABC) < match(C, ABC)) {
-        ABC <- ABC[-match(C, ABC)]
-        ABC <- append(ABC, C, after = match(A, ABC) - 1)
-    }
-} else {
-    if (match(C, ABC) < match(A, ABC)) {
-        ABC <- ABC[-match(A, ABC)]
-        ABC <- append(ABC, A, after = match(C, ABC) - 1)
-    }
-}
-if (gt == S[3]) {
-    if (match(B, ABC) < match(C, ABC)) {
-        ABC <- ABC[-match(C, ABC)]
-        ABC <- append(ABC, C, after = match(B, ABC) - 1)
-    }
-} else {
-    if (match(C, ABC) < match(B, ABC)) {
-        ABC <- ABC[-match(B, ABC)]
-        ABC <- append(ABC, B, after = match(C, ABC) - 1)
-    }
-}
+ABC <- rank(S[1], A, B, ABC)
+ABC <- rank(S[2], A, C, ABC)
+ABC <- rank(S[3], B, C, ABC)
 ABC[2] |>
     cat("\n", sep = "")
