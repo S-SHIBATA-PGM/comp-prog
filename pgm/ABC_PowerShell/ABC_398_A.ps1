3f30c8177a642b5fb03e5db5732a047a42b01b9c
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-24 06:45:08 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_398_A.ps1
+++ pgm/ABC_PowerShell/ABC_398_A.ps1
@@ -3,13 +3,7 @@ Set-Variable -name hyphen -value "-" -option constant
 Set-Variable -name equal -value "=" -option constant
 $num_hyphen = ($N - (2 - $N % 2)) / 2
 $num_equal = 2 - $N % 2
-for ($i = 1; $i -le $num_hyphen; $i++) {
-    Write-Host -NoNewLine $hyphen
-}
-for ($i = 1; $i -le $num_equal; $i++) {
-    Write-Host -NoNewLine $equal
-}
-for ($i = 1; $i -le $num_hyphen; $i++) {
-    Write-Host -NoNewLine $hyphen
-}
+Write-Host -NoNewLine ("$hyphen" * $num_hyphen)
+Write-Host -NoNewLine ("$equal" * $num_equal)
+Write-Host -NoNewLine ("$hyphen" * $num_hyphen)
 Write-Host
