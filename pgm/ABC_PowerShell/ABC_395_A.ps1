ee9f735dfffbe38d72724dd262f2b895135060de
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-03-24 19:55:15 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_395_A.ps1
+++ pgm/ABC_PowerShell/ABC_395_A.ps1
@@ -1,14 +1,12 @@
-[int]$N = [Console]::ReadLine().Trim()
-[int[]]$A = [Console]::ReadLine().Trim() -split " " -ne ""
+$N = [int][Console]::ReadLine()
+$A = [int[]]([Console]::ReadLine()).Split(" ")
 Set-Variable -name Yes -value "Yes" -option constant
 Set-Variable -name No -value "No" -option constant
-$pre = 0
-for ($i = 0; $i -lt $N; $i++) {
-    if ($A[$i] -le $pre) {
+for ($i = 1; $i -lt $N; $i++) {
+    if ($A[$i] -le $A[$i - 1]) {
         Write-Host ($No)
         exit
     }
-    $pre = $A[$i];
 }
 Write-Host($Yes)
 exit
