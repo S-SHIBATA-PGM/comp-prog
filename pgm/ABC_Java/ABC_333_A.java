88c07fee36bd5684903e9122126ebdb389f38706
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-03 20:38:35 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_333_A.java
@@ -0,0 +1,16 @@
+import java.io.*;
+// import java.math.*;
+import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final int N = Integer.parseInt (reader.readLine());
+        System.out.println (
+            String.join ("", Collections.nCopies (N, String.valueOf (N))));
+        return;
+    }
+}
