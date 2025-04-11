7cf0a9fbbc11002525203c46cf4f08758505f9a2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-11 20:14:59 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_383_A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int N;
+    cin >> N;
+    int T[N];
+    int V[N];
+    REP (i, N) {
+        cin >> T[i] >> V[i];
+    }
+    int time = 0;
+    int water = 0;
+    REP (i, N) {
+        water = max (0, water - T[i] + time);
+        water += V[i];
+        time = T[i];
+    }
+    cout << water << endl;
+    return 0;
+}
