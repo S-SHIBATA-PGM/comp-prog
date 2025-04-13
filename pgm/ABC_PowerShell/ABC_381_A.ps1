c49994e3723945341c2df7aac72f746bd84e8816
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-13 19:06:03 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_381_A.ps1
+++ pgm/ABC_PowerShell/ABC_381_A.ps1
@@ -1,4 +1,4 @@
-[int] $N = [Console]::ReadLine()
+[Console]::ReadLine() > $null
 [string[]] $S = [Console]::ReadLine().split("/")
 Set-Variable -Name Yes -Value "Yes" -Option Constant
 Set-Variable -Name No -Value "No" -Option Constant
@@ -6,13 +6,38 @@ Set-Variable -Name one -Value '1' -Option Constant
 Set-Variable -Name two -Value '2' -Option Constant
 if ($S.Length -ne 2) {
     Write-Host -NoNewLine $No
-} elseif ($S[0].Length -eq $S[1].Length) {
-    if(($S[0].ToCharArray() |% {$ret = $true} {$ret = $ret -and $_ -eq $one} {$ret}) -and ($S[1].ToCharArray() |% {$ret = $true} {$ret = $ret -and $_ -eq $two} {$ret})) {
+}
+elseif ($S[0].Length -eq $S[1].Length) {
+    if (
+        (
+            $S[0].ToCharArray() |
+            ForEach-Object -Begin {
+                $ret = $true
+                $ret > $null
+            } -Process {
+                $ret = $ret -and $_ -eq $one
+            } -End {
+                $ret
+            }
+        ) -and (
+            $S[1].ToCharArray() |
+            ForEach-Object -Begin {
+                $ret = $true
+                $ret > $null
+            } -Process {
+                $ret = $ret -and $_ -eq $two
+            } -End {
+                $ret
+            }
+        )
+    ) {
         Write-Host -NoNewLine $Yes
-    } else {
+    }
+    else {
         Write-Host -NoNewLine $No
     }
-} else {
+}
+else {
     Write-Host -NoNewLine $No
 }
 Write-Host
