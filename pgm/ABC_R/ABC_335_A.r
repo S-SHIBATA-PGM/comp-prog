b9cf1f4bf2f3ff5e1bcb4f640e9c01f4530f6eb2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-20 06:10:53 +0900

    Arrangement.

--- pgm/ABC_R/ABC_335_A.r
+++ pgm/ABC_R/ABC_335_A.r
@@ -2,5 +2,5 @@ con <- file(description = "stdin", open = "r")
 S <- readLines(con = con)
 close(con)
 four <- "4"
-paste(substr(S, 1, nchar(S) - 1), four, sep = "") |>
+paste0(substr(S, 1, nchar(S) - 1), four) |>
     cat("\n", sep = "")
