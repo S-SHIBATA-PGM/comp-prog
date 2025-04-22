7d8d6436f66c82a0c9a4e21cf34d9360d3f6326a
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-23 05:40:13 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_369_A.ps1
+++ pgm/ABC_PowerShell/ABC_369_A.ps1
@@ -1,11 +1,11 @@
-[int] $A, $B = [Console]::ReadLine().Split(" ")
+[int] $A, [int] $B = [Console]::ReadLine().Split(" ")
 if ($A -eq $B) {
-Write-Host -NoNewline 1
+    Write-Host -NoNewline 1
 }
 elseif ([Math]::Abs($A - $B) % 2 -eq 1) {
-Write-Host -NoNewLine 2
+    Write-Host -NoNewLine 2
 }
 else {
-Write-Host -NoNewLine 3
+    Write-Host -NoNewLine 3
 }
 Write-Host
