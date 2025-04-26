e5817c6b9c9cb55d7677596e941d65cc28c235fa
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-27 06:46:08 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_359_A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<string> S;
+    REP (i, N) {
+        cin >> S.emplace_back();
+    }
+    const string Takahashi = "Takahashi";
+    cout << count (S.begin(), S.end(), Takahashi) << endl;
+    return 0;
+}
---
+++ pgm/ABC_PowerShell/ABC_359_A.ps1
@@ -0,0 +1,9 @@
+[int] $N = [Console]::ReadLine()
+[string[]] $S = New-Object string[] $N
+[string] $line > $null
+for ([int] $i = 0; $null -ne ($line = [Console]::ReadLine()); $i++) {
+    $S[$i] = $line
+}
+Set-Variable -Name Takahashi -Value "Takahashi" -Option Constant
+Write-Host ($S | Where-Object { $_ -eq $Takahashi }).Count
+exit 0
