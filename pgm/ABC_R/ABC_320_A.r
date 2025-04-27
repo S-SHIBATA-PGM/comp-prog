5dc65b6cc391dd1cdc534f96151951f3db279005
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-28 04:38:25 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_320_A.r
@@ -0,0 +1,10 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+A <- strsplit(arr[1], split = " ")[[1]][1] |>
+    as.integer()
+B <- strsplit(arr[1], split = " ")[[1]][2] |>
+    as.integer()
+cat(A^B + B^A) |>
+    cat("\n", sep = "")
+q("no")
