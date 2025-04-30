3223a3ee0261bcdd9f1df5588cbecb7c32257f54
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-30 12:08:40 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_349_A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A;
+    REP (i, N) {
+        cin >> A.emplace_back();
+    }
+    cout << -ALL (reduce, A) << endl;
+    return 0;
+}
