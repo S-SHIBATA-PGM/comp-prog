5a046f5b4e53f0b419ce9a5938b09649db7e2aad
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-04-30 13:40:01 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_348_A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int N;
+    cin >> N;
+    const string fail = "x";
+    const string success = "o";
+    const int failure_time = 3;
+    FOR (i, 1, N + 1) {
+        if (i % failure_time == 0) {
+            cout << fail;
+        } else {
+            cout << success;
+        }
+    }
+    cout << endl;
+    return 0;
+}
