1b4a45298928dff136219ca6ebe7a03391e5e9e1
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-05-03 21:42:07 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_334_A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int B, G;
+    cin >> B >> G;
+    const string Bat = "Bat";
+    const string Glove = "Glove";
+    cout << (B > G ? Bat : Glove) << endl;
+    return 0;
+}
