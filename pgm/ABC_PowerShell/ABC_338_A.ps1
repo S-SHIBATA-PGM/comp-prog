def66f8504ce64184975858972a9fb7dda0ccb34
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-05-03 21:17:34 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_338_A.ps1
@@ -0,0 +1,17 @@
+Set-Variable -Name Yes -Value "Yes" -Option Constant
+Set-Variable -Name No -Value "No" -Option Constant
+[string][Console]::ReadLine() | ForEach-Object {
+    $flg = [char]::IsUpper($_[0])
+    if ($flg -and $_.Length -gt 1) {
+        $_[1..($_.length - 1)] | ForEach-Object {
+            $flg = $flg -and [char]::isLower($_)
+        }
+    }
+}
+if ($flg) {
+    Write-Host $Yes
+}
+else {
+    Write-Host $No
+}
+exit 0
