d8ffd3758c8b3434bcc459815f7d0bc21a0d67fb
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-05-05 16:59:15 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_326_A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    const int two = 2;
+    const int three = 3;
+    const string Yes = "Yes";
+    const string No = "No";
+    cout << (X < Y && Y - X <= two     ? Yes
+             : X > Y && X - Y <= three ? Yes
+                                       : No)
+         << endl;
+    return 0;
+}
