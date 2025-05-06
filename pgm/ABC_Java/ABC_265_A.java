3be7e1254972e4cd7fa7a2c1e85ebd25abe5172e
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-05-07 03:21:48 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_265_A.java
@@ -0,0 +1,23 @@
+import java.io.*;
+// import java.math.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final String[] arr = reader.readLine().split (" ");
+        final int three = 3;
+        final int X = Integer.parseInt (arr[0]);
+        final int Y = Integer.parseInt (arr[1]);
+        final int N = Integer.parseInt (arr[2]);
+        if (X * three < Y) {
+            System.out.println (X * N);
+        } else {
+            System.out.println (X * (N % three) + Y * (N / three));
+        }
+        return;
+    }
+}
