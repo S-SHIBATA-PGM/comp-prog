8218fb4927007a248ed6c3ab69a0fd6e095294a1
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-05-12 21:38:07 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_308_A.ps1
+++ pgm/ABC_PowerShell/ABC_308_A.ps1
@@ -3,18 +3,16 @@ Set-Variable -Name No -Value "No" -Option Constant
 Set-Variable -Name lower -Value 100 -Option Constant
 Set-Variable -Name upper -Value 675 -Option Constant
 Set-Variable -Name multiple -Value 25 -Option Constant
-[int[]][Console]::ReadLine().Split(" ") | ForEach-Object -Begin {
-    $allFlg = $true
-    $allFlg > $null
-    $pre = -1
-    $pre > $null
-} -Process {
+$allFlg = $true
+$pre = -1
+$S = [int[]][Console]::ReadLine().Split(" ") | Where-Object {
     $allFlg = $allFlg -and
     $pre -le $_ -and
     $lower -le $_ -and
     $_ -le $upper -and
     $_ % $multiple -eq 0
     $pre = $_
-} -End {
-    Write-Host ($allFlg ? $Yes : $No)
+    return $allFlg
 }
+Write-Host ($S.Count -eq 8 ? $Yes : $No)
+exit 0
