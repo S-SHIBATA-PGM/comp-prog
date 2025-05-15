3a058573f8c48f38d171dcdd7f224d814e23c172
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-05-16 07:16:14 +0900

    Arrangement.

--- pgm/ABC_R/ABC_267_A.r
+++ pgm/ABC_R/ABC_267_A.r
@@ -6,23 +6,23 @@
 # dow[["Saturday"]] == 6
 # dow[["Sunday"]] == 0
 get_day_of_week_by_code <- function(dow, upper_case_flg = FALSE) {
-    continue_flg = TRUE
+    # %A 曜日名
+    # %a 曜日名 短縮形
+    ##  実行環境 ロケール設定
+    # original_locale <- Sys.getlocale("LC_TIME")
+    ##  日本語 ロケール設定
+    # Sys.setlocale("LC_TIME", "ja_JP.UTF-8")
+    ##  ロケール設定 元に戻す
+    # Sys.setlocale("LC_TIME", original_locale)
+    original_locale <- Sys.getlocale("LC_TIME")
+    Sys.setlocale("LC_TIME", "C.UTF-8")
+    continue_flg <- TRUE
     base_date <- Sys.Date()
     idx <- 0
     while (continue_flg) {
         current_date <- base_date + idx
         # str(current_date) |>
         #    cat("\n", sep = "")
-        # %A 曜日名
-        # %a 曜日名 短縮形
-        ##  実行環境 ロケール設定
-        # original_locale <- Sys.getlocale("LC_TIME")
-        ##  日本語 ロケール設定
-        # Sys.setlocale("LC_TIME", "ja_JP.UTF-8")
-        ##  ロケール設定 元に戻す
-        # Sys.setlocale("LC_TIME", original_locale)
-        original_locale <- Sys.getlocale("LC_TIME")
-        Sys.setlocale("LC_TIME", "C.UTF-8")
         if (!exists(format(current_date, "%A"), envir = dow)) {
             dow[[format(current_date, "%A")]] <- format(current_date, "%w") |>
                 as.integer()
@@ -30,8 +30,8 @@ get_day_of_week_by_code <- function(dow, upper_case_flg = FALSE) {
         } else {
             continue_flg <- FALSE
         }
-        Sys.setlocale("LC_TIME", original_locale)
     }
+    Sys.setlocale("LC_TIME", original_locale)
     return(dow)
 }
 con <- file(description = "stdin", open = "r")
