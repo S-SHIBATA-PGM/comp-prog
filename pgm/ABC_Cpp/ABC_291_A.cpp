a0100c03884b36a1f55f32df4012406cd2ac426a
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-05-26 22:39:27 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_291_A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    string S;
+    cin >> S;
+    smatch m;
+    regex_search (S, m, regex ("[A-Z]"));
+    cout << m.position() + 1 << endl;
+    return 0;
+}
