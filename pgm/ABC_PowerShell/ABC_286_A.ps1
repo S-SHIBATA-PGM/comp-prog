a98ff0fa9a1c0736db46cf117f3abbdcaaad6ab3
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-06-01 16:26:34 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_286_A.ps1
@@ -0,0 +1,8 @@
+$N, $P, $Q, $R, $S = [int[]][Console]::ReadLine().Split()
+$A = [int[]][Console]::ReadLine().Split()
+$i = $R - 1
+@(($P - 1)..($Q - 1)) | ForEach-Object {
+    $A[$_], $A[$i] = $A[$i], $A[$_]
+    $i++
+}
+Write-Host $A
