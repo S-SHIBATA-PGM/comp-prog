0bcf73ab371936ca2ca45fcd148d3b302027095f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-06-04 20:41:47 +0900

    Arrangement.

---
+++ pgm/ABC_Visual_Basic/ABC_394_A.vb
@@ -0,0 +1,16 @@
+' Option Strict On
+' Imports System.Collections.Generic
+' Imports System.Globalization
+' Imports System.IO
+' Imports System.Linq
+' Imports System.Text
+Imports System.Text.RegularExpressions
+
+Class Main
+    Public Shared Sub Main()
+        Dim N As String = Console.readLine()
+        Console.WriteLine(
+            Regex.Replace(N, "[^2]+", ""))
+        Return
+    End Sub
+End Class
