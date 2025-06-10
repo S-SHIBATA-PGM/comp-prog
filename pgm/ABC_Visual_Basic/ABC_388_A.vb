7c4233b9d77bacdea3c652b7558893a13742daa7
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-06-10 21:55:55 +0900

    Arrangement.

---
+++ pgm/ABC_Visual_Basic/ABC_388_A.vb
@@ -0,0 +1,16 @@
+' Option Strict On
+' Imports System.Collections.Generic
+' Imports System.Globalization
+' Imports System.IO
+' Imports System.Linq
+' Imports System.Text
+' Imports System.Text.RegularExpressions
+
+Class Main
+    Public Shared Sub Main()
+        Dim S As String = Console.ReadLine()
+        Const UPC As String = "UPC"
+        Console.WriteLine(S.Chars(0) & UPC)
+        Return
+    End Sub
+End Class
