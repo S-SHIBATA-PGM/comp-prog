2243436ea6945c37e213238290806951423bfb65
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-06-19 20:31:02 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_213_A.java
@@ -0,0 +1,17 @@
+import java.io.*;
+// import java.math.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final String[] arr = reader.readLine().split (" ");
+        final int A = Integer.parseInt (arr[0]);
+        final int B = Integer.parseInt (arr[1]);
+        System.out.println (A ^ B);
+        return;
+    }
+}
