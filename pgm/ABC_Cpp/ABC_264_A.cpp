237b88cdc9fb05acc672ab65f5b42caaf6167136
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-06-24 21:50:10 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_264_A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int L, R;
+    cin >> L >> R;
+    const string atcoder = "atcoder";
+    cout << atcoder.substr (L - 1, R - L + 1) << endl;
+    return 0;
+}
