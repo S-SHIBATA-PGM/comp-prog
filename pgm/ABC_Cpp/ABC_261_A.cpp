99a710c1c33cbc0313b18ce939e125463b1a3fdc
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-06-30 23:26:09 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_261_A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int L1, R1, L2, R2;
+    cin >> L1 >> R1 >> L2 >> R2;
+    cout << max (min (R1, R2) - max (L1, L2), 0) << endl;
+    return 0;
+}
