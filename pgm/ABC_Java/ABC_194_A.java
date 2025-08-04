c4597683eae4b5f7374a6c633bbe73325aa5759a
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-08-05 07:09:19 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_194_A.java
@@ -0,0 +1,35 @@
+import java.io.*;
+// import java.math.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final String[] arr = reader.readLine().split (" ");
+        final int solid15 = 15;
+        final int solid10 = 10;
+        final int fat8 = 8;
+        final int fat3 = 3;
+        final int one = 1;
+        final int two = 2;
+        final int three = 3;
+        final int four = 4;
+        final int zero = 0;
+        final int A = Integer.parseInt (arr[zero]);
+        final int B = Integer.parseInt (arr[one]);
+        final int AB = A + B;
+        if (solid15 <= AB && fat8 <= B) {
+            System.out.println (one);
+        } else if (solid10 <= AB && fat3 <= B) {
+            System.out.println (two);
+        } else if (fat3 <= AB) {
+            System.out.println (three);
+        } else {
+            System.out.println (four);
+        }
+        return;
+    }
+}
