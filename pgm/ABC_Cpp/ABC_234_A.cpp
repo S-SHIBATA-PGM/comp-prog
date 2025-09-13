6a44067324abfa46e5c840f68519add137d28c92
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-09-13 19:12:19 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_234_A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+double f (double x) {
+    return (pow (x, 2) + 2 * x + 3);
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cout << fixed << setprecision (0) << f (f (f (t) + t) + f (f (t))) << endl;
+    return 0;
+}
