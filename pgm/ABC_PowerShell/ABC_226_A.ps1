4ae19fbcb5d07d53f19fce81096d8433c3e57906
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-09-25 20:26:14 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_226_A.ps1
+++ pgm/ABC_PowerShell/ABC_226_A.ps1
@@ -1,4 +1,4 @@
 $X = [decimal][Console]::ReadLine()
 Set-Variable -Name zero -Value 0 -Option Constant
-Write-Host([Math]::Round($X, $zero, [MidpointRounding]::AwayFromZero))
+Write-Host ([Math]::Round($X, $zero, [MidpointRounding]::AwayFromZero))
 exit 0
