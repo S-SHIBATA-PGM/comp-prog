600fec0f04dbe01b918a5dd4c0de8e48433c4f48
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-10-16 22:40:50 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_120_A.java
@@ -0,0 +1,21 @@
+import java.io.*;
+// import java.math.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final String[] arr = reader.readLine().split (" ");
+        final int one = 1;
+        final int two = 2;
+        final int zero = 0;
+        final int A = Integer.parseInt (arr[zero]);
+        final int B = Integer.parseInt (arr[one]);
+        final int C = Integer.parseInt (arr[two]);
+        System.out.println (Math.min (B / A, C));
+        return;
+    }
+}
