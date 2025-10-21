c3ec5f41eca6f705bf753dac9e26b9b10fb097cf
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-10-21 23:10:10 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_215_A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    string S;
+    cin >> S;
+    const string hello = "Hello,World!";
+    const string AC = "AC";
+    const string WA = "WA";
+    const int zero = 0;
+    if (S.compare (hello) == zero) {
+        cout << AC << endl;
+    } else {
+        cout << WA << endl;
+    }
+    return 0;
+}
