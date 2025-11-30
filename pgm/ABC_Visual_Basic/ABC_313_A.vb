46d9279aff4dc453b33d95be7f9a53216a1d06a2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-11-30 19:19:26 +0900

    Arrangement.

---
+++ pgm/ABC_Visual_Basic/ABC_313_A.vb
@@ -0,0 +1,25 @@
+' Option Strict On
+' Imports System.Collections.Generic
+' Imports System.Globalization
+' Imports System.IO
+' Imports System.Linq
+' Imports System.Text
+' Imports System.Text.RegularExpressions
+
+Class Main
+    Public Shared Sub Main()
+        Dim N As Integer = Integer.Parse(Console.ReadLine())
+        Dim P() As Integer = Array.ConvertAll(
+        Console.ReadLine().Split(), Function(x) Integer.Parse(x))
+        Const one As Integer = 1
+        Const zero As Integer = 0
+        Dim max As Integer = P.Max()
+        Dim cnt As Integer = P.Count(Function(a) a = max)
+        If P(zero) <> max OrElse (P(zero) = max AndAlso cnt > one)
+          Console.WriteLine(max - P(zero) + one)
+        Else
+          Console.WriteLine(zero)
+        End If
+        Return
+    End Sub
+End Class
