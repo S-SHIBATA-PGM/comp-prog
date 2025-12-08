23755e602b692b292d622fa60ee767927ce21a51
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-12-08 22:29:57 +0900

    Arrangement.

--- pgm/ABC_R/ABC_168_A.r
+++ pgm/ABC_R/ABC_168_A.r
@@ -1,8 +1,8 @@
 con <- file(description = "stdin", open = "r")
 arr <- readLines(con = con)
 close(con)
-vecHon = c(2, 4, 5, 7, 9)
-vecPon = c(0, 1, 6, 8)
+vec_hon <- c(2, 4, 5, 7, 9)
+vec_pon <- c(0, 1, 6, 8)
 hon <- "hon"
 pon <- "pon"
 bon <- "bon"
@@ -10,10 +10,10 @@ one <- 1
 blank <- ""
 N <- arr[one] |>
     as.integer()
-if (any(vecHon == N %% 10)) {
+if (any(vec_hon == N %% 10)) {
     hon |>
         cat("\n", sep = blank)
-} else if (any(vecPon == N %% 10)) {
+} else if (any(vec_pon == N %% 10)) {
     pon |>
         cat("\n", sep = blank)
 } else {
