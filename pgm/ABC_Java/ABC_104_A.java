3643db783a71ef266aed34f93945d7890867521f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-12-10 23:14:52 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_104_A.java
@@ -0,0 +1,28 @@
+import java.io.*;
+// import java.math.*;
+import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final Integer R = Integer.valueOf (reader.readLine());
+        final Map<Integer, String> Rating = new LinkedHashMap<>();
+        final Integer lower = 1200;
+        final String AGC = "AGC";
+        final int zero = 0;
+        Rating.put (lower, "ABC");
+        Rating.put (2800, "ARC");
+        String Contest = "";
+        for (Map.Entry<Integer, String> entry : Rating.entrySet()) {
+            if (entry.getKey().compareTo (R) > zero) {
+                Contest = entry.getValue();
+                break;
+            }
+        }
+        System.out.println (Contest.isEmpty() ? AGC : Contest);
+        return;
+    }
+}
