9447ea4c8ac9c6b9a729a53f27a816bad25b9dc6
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-12-28 18:06:33 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_433_A.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  var [int X, int Y, int Z] = stdin
+      .readLineSync()!
+      .split(" ")
+      .map(int.parse)
+      .toList();
+  const String Yes = "Yes";
+  const String No = "No";
+  const int zero = 0;
+  print((X - Y * Z) >= zero && (X - Y * Z) % (Z - 1) == zero ? Yes : No);
+  exit(0);
+}
