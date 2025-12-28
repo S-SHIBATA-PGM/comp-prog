a8ec4ae677541fb0954b0d282fa7ba996947a81a
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-12-29 07:54:08 +0900

    Arrangement.

--- pgm/ABC_Kotlin/ABC_436_A.kt
+++ pgm/ABC_Kotlin/ABC_436_A.kt
@@ -2,7 +2,7 @@ const val circle: Char = 'o'
 
 fun main() {
     val N: Int = readln().toInt()
-    var S: String = readln()
+    val S: String = readln()
     println(S.padStart(N, circle))
     kotlin.system.exitProcess(0)
 }
