c57e548687570bcbd9b5f42ac31818d2eb8ee2ed
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-12-29 08:06:16 +0900

    Arrangement.

--- pgm/ABC_Dart/ABC_434_A.dart
+++ pgm/ABC_Dart/ABC_434_A.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
 void main() {
-  var [int W, int B] = stdin.readLineSync()!.split(" ").map(int.parse).toList();
+  final [int W, int B] = stdin
+      .readLineSync()!
+      .split(" ")
+      .map(int.parse)
+      .toList();
   const int one = 1;
   const int thousand = 1000;
   print((W * thousand / B + one).toInt());
