d6926bceacd119bd05b014a9da2a8fdeada2c278
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2025-12-30 16:32:05 +0900

    Arrangement.

--- pgm/ABC_Java/ABC_162_A.java
+++ pgm/ABC_Java/ABC_162_A.java
@@ -1,10 +1,18 @@
 import java.io.*;
+// import java.math.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
 
 public class Main {
     public static void main (String[] args) throws Exception {
         BufferedReader reader =
             new BufferedReader (new InputStreamReader (System.in));
-        String line = reader.readLine();
-        System.out.println (line.indexOf ("7") == -1 ? "No" : "Yes");
+        final String N = reader.readLine();
+        final String seven = "7";
+        final String Yes = "Yes";
+        final String No = "No";
+        System.out.println (N.contains (seven) ? Yes : No);
+        return;
     }
 }
