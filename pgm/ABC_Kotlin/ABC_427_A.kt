fd2e9a575694dcd1abe74013563c69b89370601b
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-01 17:03:46 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_427_A.kt
@@ -0,0 +1,11 @@
+import kotlin.math.*
+
+const val one: Int = 1
+const val two: Int = 2
+
+fun main() {
+    val S: String = readln()
+    val idx: Int = (S.length + one) / two - one
+    println(S.removeRange(idx, idx + one))
+    kotlin.system.exitProcess(0)
+}
