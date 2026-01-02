e085c95eba094bee14b7748bc3e2d91883638cb2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-02 17:27:00 +0900

    Arrangement.

--- pgm/ABC_Dart/ABC_425_A.dart
+++ pgm/ABC_Dart/ABC_425_A.dart
@@ -3,10 +3,10 @@ import 'dart:math';
 
 void main() {
   final int N = int.parse(stdin.readLineSync()!);
-  const int zero = 0;
   const int one = 1;
   const int three = 3;
   const int oneMinus = -1;
+  const int zero = 0;
   int cubes = zero;
   for (int i = one; i <= N; i++) {
     cubes += (pow(oneMinus, i) * pow(i, three)).round();
