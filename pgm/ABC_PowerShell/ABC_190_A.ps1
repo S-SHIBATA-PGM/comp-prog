b48440ca56535d8a36e9065e5f1f6230c1b7e01c
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-02 12:23:12 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_190_A.ps1
@@ -0,0 +1,21 @@
+$A, $B, $C = [int[]][Console]::ReadLine().Split()
+Set-Variable -Name zero -Value 0U -Option Constant
+Set-Variable -Name Takahashi -Value "Takahashi" -Option Constant
+Set-Variable -Name Aoki -Value "Aoki" -Option Constant
+if ($C -eq $zero) {
+    if ($A -le $B) {
+        Write-Host $Aoki
+    }
+    else {
+        Write-Host $Takahashi
+    }
+}
+else {
+    if ($B -le $A) {
+        Write-Host $Takahashi
+    }
+    else {
+        Write-Host $Aoki
+    }
+}
+exit 0
