0044bad26fd21653f24ca912f2cc6ffab0ff4f44
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-03 07:30:56 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_091_A.java
@@ -0,0 +1,23 @@
+import java.io.*;
+// import java.math.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final String[] arr = reader.readLine().split (" ");
+        final int one = 1;
+        final int two = 2;
+        final int zero = 0;
+        final int A = Integer.parseInt (arr[zero]);
+        final int B = Integer.parseInt (arr[one]);
+        final int C = Integer.parseInt (arr[two]);
+        final String Yes = "Yes";
+        final String No = "No";
+        System.out.println (C <= A + B ? Yes : No);
+        System.exit (0);
+    }
+}
