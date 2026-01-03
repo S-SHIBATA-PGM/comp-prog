78a39442ee30feee5bc889c0a21c72b76a480195
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-03 17:06:16 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_188_A.ps1
@@ -0,0 +1,11 @@
+$X, $Y = [int[]][Console]::ReadLine().Split()
+Set-Variable -Name "Yes" -Value "Yes" -Option Constant
+Set-Variable -Name "No" -Value "No" -Option Constant
+Set-Variable -Name "three" -Value 3U -Option Constant
+if ([Math]::Max($X, $Y) -lt [Math]::min($X, $Y) + $three) {
+    Write-Host $Yes
+}
+else {
+    Write-Host $No
+}
+exit 0
