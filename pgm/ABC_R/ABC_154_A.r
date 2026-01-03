8b1e3662b7ba719584a9676ecf0cc459212dc0a2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-03 16:45:24 +0900

    Arrangement.

--- pgm/ABC_R/ABC_154_A.r
+++ pgm/ABC_R/ABC_154_A.r
@@ -6,7 +6,7 @@ two <- 2L
 three <- 3L
 space <- " "
 S <- strsplit(x = arr[one], split = space)[[one]][one]
-T <- strsplit(x = arr[one], split = space)[[one]][two]
+t <- strsplit(x = arr[one], split = space)[[one]][two]
 A <- strsplit(x = arr[two], split = space)[[one]][one] |>
     as.integer()
 B <- strsplit(x = arr[two], split = space)[[one]][two] |>
@@ -14,7 +14,7 @@ B <- strsplit(x = arr[two], split = space)[[one]][two] |>
 U <- arr[three]
 if (S == U) {
     A <- A - one
-} else if (T == U) {
+} else if (t == U) {
     B <- B - one
 }
 c(A, B) |>
