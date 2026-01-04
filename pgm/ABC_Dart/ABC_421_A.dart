eacbfd2d05eef25e230c459f9b385bd944bf4668
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-04 10:35:32 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_421_A.dart
@@ -0,0 +1,22 @@
+// import 'dart:collection';
+import 'dart:io';
+// import 'dart:math';
+
+void main() {
+  final int N = int.parse(stdin.readLineSync()!);
+  List<String> S = [];
+  for (int i = 0; i < N; i++) {
+    S.add(stdin.readLineSync()!);
+  }
+  final (int X, String Y) = ((arr) =>
+      (int.parse(arr[0]), arr[1]))(stdin.readLineSync()!.split(' '));
+  const String Yes = "Yes";
+  const String No = "No";
+  const int one = 1;
+  if (S[X - one] == Y) {
+    print(Yes);
+  } else {
+    print(No);
+  }
+  exit(0);
+}
