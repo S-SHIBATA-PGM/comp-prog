2b96cc0589c346bf0334a9ac9d314926fc50ef07
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-06 06:12:11 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_419_A.dart
@@ -0,0 +1,21 @@
+// import 'dart:collection';
+import 'dart:io';
+// import 'dart:math';
+
+void main() {
+  final String S = stdin.readLineSync()!;
+  final String red = 'red';
+  final String blue = 'blue';
+  final String green = 'green';
+  final String SSS = 'SSS';
+  final String FFF = 'FFF';
+  final String MMM = 'MMM';
+  final String Unknown = 'Unknown';
+  final Map<String, String> Language = Map.unmodifiable({
+    red: SSS,
+    blue: FFF,
+    green: MMM,
+  });
+  print(Language.containsKey(S) ? Language[S] : Unknown);
+  exit(0);
+}
