798a52aba2e319cd2dc775fc9bfbc64114d5a232
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-11 08:54:17 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_179_A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    string S;
+    cin >> S;
+    const string es = "es";
+    const string s = "s";
+    cout << (S.ends_with (s) ? S.append (es) : S.append (s)) << endl;
+    return 0;
+}
