d1c7637b4171a804b1158a33a149f773a6a62e71
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-11 10:05:58 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_413_A.kt
@@ -0,0 +1,18 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val Yes: String = "Yes"
+const val No: String = "No"
+const val space: String = " "
+
+fun main() {
+    val (N: Int, M: Int) = readln().split(space).map { it.toInt() }
+    var A: List<Int> = readln().split(space).map { it.toInt() }
+    if (A.sum() <= M) {
+        println(Yes)
+    } else {
+        println(No)
+    }
+    kotlin.system.exitProcess(0)
+}
