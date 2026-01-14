018f618e9a9377ca4f0e135a366163c87b570ed9
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-14 22:23:58 +0900

    Arrangement.

--- pgm/ABC_Dart/ABC_413_A.dart
+++ pgm/ABC_Dart/ABC_413_A.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 // import 'dart:math';
 
 void main() {
-  final [int N, int M] = stdin
+  final [int _, int M] = stdin
       .readLineSync()!
       .split(' ')
       .map(int.parse)
