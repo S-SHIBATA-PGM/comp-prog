9e2188523b6f77c7ecc1b2b871045cbfdbcf1bc9
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-15 23:36:19 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_175_A.ps1
@@ -0,0 +1,16 @@
+Set-Variable -Name "R" -Value "R" -Option Constant
+Set-Variable -Name "reg" -Value "\B" -Option Constant
+Set-Variable -Name "zero" -Value 0U -Option Constant
+$num = $zero
+$cnt = $zero
+[string][Console]::ReadLine() -split $reg | ForEach-Object {
+    if ($_ -eq $R) {
+        $num++
+    }
+    else {
+        $cnt = [Math]::Max($cnt, $num)
+        $num = $zero
+    }
+}
+Write-Host ([Math]::Max($cnt, $num))
+exit 0
