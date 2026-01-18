4440770d10636a09b47ddd78c834c6cd251c21b8
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-18 11:59:03 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_175_A.ps1
+++ pgm/ABC_PowerShell/ABC_175_A.ps1
@@ -1,6 +1,6 @@
 Set-Variable -Name "R" -Value "R" -Option Constant
 Set-Variable -Name "reg" -Value "\B" -Option Constant
-Set-Variable -Name "zero" -Value 0U -Option Constant
+Set-Variable -Name "zero" -Value ([uint32]0) -Option Constant
 $num = $zero
 $cnt = $zero
 [string][Console]::ReadLine() -split $reg | ForEach-Object {
--- pgm/ABC_PowerShell/ABC_178_A.ps1
+++ pgm/ABC_PowerShell/ABC_178_A.ps1
@@ -1,4 +1,4 @@
 $x = [int][Console]::ReadLine()
-Set-Variable -Name "one" -Value 1U -Option Constant
+Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
 Write-Host ($x -bxor $one)
 exit 0
