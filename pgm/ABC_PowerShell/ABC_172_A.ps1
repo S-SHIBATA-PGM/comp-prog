f12ea0bde569a99a7af22f7d5c3b70c5e08c9b07
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-18 10:28:08 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_172_A.ps1
+++ pgm/ABC_PowerShell/ABC_172_A.ps1
@@ -1,5 +1,5 @@
 $a = [int][Console]::ReadLine()
-Set-Variable -Name "two" -Value 2U -Option Constant
-Set-Variable -Name "three" -Value 3U -Option Constant
+Set-Variable -Name "two" -Value ([uint32]2) -Option Constant
+Set-Variable -Name "three" -Value ([uint32]3) -Option Constant
 Write-Host ($a + [Math]::pow($a, $two) + [Math]::pow($a, $three))
 exit 0
