d620bdf660133fe2ef9f0c1e2bc57f44ebd655bc
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-18 11:54:18 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_174_A.ps1
+++ pgm/ABC_PowerShell/ABC_174_A.ps1
@@ -1,7 +1,7 @@
 $X = [int][Console]::ReadLine()
 Set-Variable -Name "Yes" -Value "Yes" -Option Constant
 Set-Variable -Name "No" -Value "No" -Option Constant
-Set-Variable -Name "thirty" -Value 30U -Option Constant
+Set-Variable -Name "thirty" -Value ([uint32]30) -Option Constant
 if ($X -ge $thirty) {
     Write-Host $Yes
 }
