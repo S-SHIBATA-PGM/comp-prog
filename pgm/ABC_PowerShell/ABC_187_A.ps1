cac0c7dbca871913a918538cdd447d1765a5ab85
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-18 12:06:35 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_187_A.ps1
+++ pgm/ABC_PowerShell/ABC_187_A.ps1
@@ -1,7 +1,7 @@
 $A, $B = [string[]][Console]::ReadLine().Split()
-Set-Variable -Name "one" -Value 1U -Option Constant
-Set-Variable -Name "two" -Value 2U -Option Constant
-Set-Variable -Name "zero" -Value 0U -Option Constant
+Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
+Set-Variable -Name "two" -Value ([uint32]2) -Option Constant
+Set-Variable -Name "zero" -Value ([uint32]0) -Option Constant
 $SA = [int][string]$A[$zero] + [int][string]$A[$one] + [int][string]$A[$two]
 $SB = [int][string]$B[$zero] + [int][string]$B[$one] + [int][string]$B[$two]
 Write-Host ([Math]::Max($SA, $SB))
