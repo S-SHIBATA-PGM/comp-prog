d096632c16a9f35220b17bd2c7e3e90dd34b1d4a
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-19 06:10:33 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_171_A.ps1
+++ pgm/ABC_PowerShell/ABC_171_A.ps1
@@ -1,6 +1,6 @@
 $alpha = [Console]::ReadLine()
-Set-Variable -Name "upperA" -Value ([char]'A') -Option Constant
-Set-Variable -Name "lowerA" -Value ([char]'a') -Option Constant
+Set-Variable -Name "upperA" -Value [char]'A' -Option Constant
+Set-Variable -Name "lowerA" -Value [char]'a' -Option Constant
 if ([char]::IsUpper($alpha)) {
     Write-Host A
 }
