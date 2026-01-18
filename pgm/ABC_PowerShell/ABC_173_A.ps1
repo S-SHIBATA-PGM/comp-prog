ac6118d0b0fc951f3cb89ba93a43bd77290e171f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-18 11:51:46 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_173_A.ps1
+++ pgm/ABC_PowerShell/ABC_173_A.ps1
@@ -1,4 +1,4 @@
 $N = [int][Console]::ReadLine()
-Set-Variable -Name "thousand" -Value 1000U -Option Constant
+Set-Variable -Name "thousand" -Value ([uint32]1000) -Option Constant
 Write-Host (($thousand - $N % $thousand) % $thousand)
 exit 0
