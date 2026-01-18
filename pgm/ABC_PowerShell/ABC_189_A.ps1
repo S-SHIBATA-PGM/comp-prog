9c4af1e09ed52a544496e8a5f978f13b7625223d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-18 12:13:35 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_189_A.ps1
+++ pgm/ABC_PowerShell/ABC_189_A.ps1
@@ -1,9 +1,9 @@
 $C = [string][Console]::ReadLine()
 Set-Variable -Name "Won" -Value "Won" -Option Constant
 Set-Variable -Name "Lost" -Value "Lost" -Option Constant
-Set-Variable -Name "one" -Value 1U -Option Constant
-Set-Variable -Name "two" -Value 2U -Option Constant
-Set-Variable -Name "zero" -Value 0U -Option Constant
+Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
+Set-Variable -Name "two" -Value ([uint32]2) -Option Constant
+Set-Variable -Name "zero" -Value ([uint32]0) -Option Constant
 if ($C[$zero] -eq $C[$one] -and $C[$one] -eq $C[$two]) {
     Write-Host $Won
 }
