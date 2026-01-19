284e53b475934b64b471a9ed6bb6ec67ad882838
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-19 21:35:51 +0900

    Arrangement.

--- pgm/ABC_Kotlin/ABC_406_A.kt
+++ pgm/ABC_Kotlin/ABC_406_A.kt
@@ -2,12 +2,12 @@ import java.time.*
 
 // import kotlin.math.*
 
+const val Yes: String = "Yes"
+const val No: String = "No"
 const val space: String = " "
 
 fun main() {
     val (A: Int, B: Int, C: Int, D: Int) = readln().split(space).map { it.toInt() }
-    val Yes: String = "Yes"
-    val No: String = "No"
     if (!LocalTime.of(C, D).isAfter(LocalTime.of(A, B))) {
         println(Yes)
     } else {
