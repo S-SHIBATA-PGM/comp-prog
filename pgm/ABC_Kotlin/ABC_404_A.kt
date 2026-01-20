c79b3c63964639e6bcfadb047527117b08ad789c
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-20 20:23:38 +0900

    Arrangement.

--- pgm/ABC_Kotlin/ABC_404_A.kt
+++ pgm/ABC_Kotlin/ABC_404_A.kt
@@ -2,22 +2,15 @@
 
 // import kotlin.math.*
 
-const val a: Int = 'a'.code
-const val z: Int = 'z'.code
+const val a: Char = 'a'
+const val z: Char = 'z'
 const val space: String = " "
 
 fun main() {
-    val S = readln().toCharArray()
-    for (i in a..z) {
-        var breakFlg = false
-        for (c in S) {
-            if (c.code == i) {
-                breakFlg = true
-                break
-            }
-        }
-        if (!breakFlg) {
-            println(i.toChar())
+    val S = readln()
+    for (c in a..z) {
+        if (c !in S) {
+            println(c)
             return
         }
     }
