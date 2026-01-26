03998749241d3c4fc4d03716ac39bd459e63402f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-26 20:42:55 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_398_A.kt
@@ -0,0 +1,14 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val hyphen = "-"
+const val equal = "="
+
+fun main() {
+    val N: Int = readln().toInt()
+    val lr: String = hyphen.repeat((N - 2 + N % 2) / 2)
+    val c: String = equal.repeat(2 - N % 2)
+    println("$lr$c$lr")
+    kotlin.system.exitProcess(0)
+}
