8df35ecddb0db18da5877bff9eeca82b67f78ba5
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-28 06:02:40 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_119_A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    string S;
+    cin >> S;
+    const string boundary_date = "2019/04/30";
+    const string Heisei = "Heisei";
+    const string TBD = "TBD";
+    cout << (S <= boundary_date ? Heisei : TBD) << endl;
+    return 0;
+}
