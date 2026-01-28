cb677a6587631d03d2f4c6abb1fc6ccd2ee6de05
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-28 22:25:29 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_396_A.kt
@@ -0,0 +1,18 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val Yes:String = "Yes"
+const val No:String = "No"
+const val times:Int = 3
+
+fun main() {
+    readln()
+    val A:List<Int> = readln().split(" ").map { it.toInt() }
+    if (A.windowed(times).any { w -> w.all { it == w[0] }}) {
+        println(Yes)
+    } else {
+        println(No)
+    }
+    kotlin.system.exitProcess(0)
+}
