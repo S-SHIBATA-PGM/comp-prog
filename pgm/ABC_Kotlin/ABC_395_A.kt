6cf1459de48c06a2c76aa8f207fac650803343ed
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-29 06:17:31 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_395_A.kt
@@ -0,0 +1,18 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val Yes:String = "Yes"
+const val No:String = "No"
+const val two:Int = 2
+
+fun main() {
+    readln()
+    val A:List<Int> = readln().split(" ").map { it.toInt() }
+    if (A.windowed(two).all { (a, b) -> a < b }) {
+        println(Yes)
+    } else {
+        println(No)
+    }
+    kotlin.system.exitProcess(0)
+}
