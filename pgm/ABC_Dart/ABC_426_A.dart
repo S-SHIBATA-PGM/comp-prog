ad98e0ad2ce42f2c12f5778588acf65fca7fc0e1
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-30 20:14:00 +0900

    Arrangement.

--- pgm/ABC_Dart/ABC_426_A.dart
+++ pgm/ABC_Dart/ABC_426_A.dart
@@ -3,7 +3,10 @@ import 'dart:io';
 
 void main() {
   final [String X, String Y] = stdin.readLineSync()!.split(' ');
-  final List<String> OS = ['Ocelot', 'Serval', 'Lynx'];
+  const Ocelot = 'Ocelot';
+  const String Serval = 'Serval';
+  const String Lynx = 'Lynx';
+  const List<String> OS = [Ocelot, Serval, Lynx];
   const String Yes = 'Yes';
   const String No = 'No';
   print(OS.indexOf(Y) <= OS.indexOf(X) ? Yes : No);
