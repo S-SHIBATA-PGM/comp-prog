b50b02cca432df1cf3ef6ef2e3af48d69960af40
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-31 19:22:47 +0900

    Arrangement.

--- pgm/ABC_Kotlin/ABC_396_A.kt
+++ pgm/ABC_Kotlin/ABC_396_A.kt
@@ -2,14 +2,14 @@
 
 // import kotlin.math.*
 
-const val Yes:String = "Yes"
-const val No:String = "No"
-const val times:Int = 3
+const val Yes: String = "Yes"
+const val No: String = "No"
+const val times: Int = 3
 
 fun main() {
     readln()
-    val A:List<Int> = readln().split(" ").map { it.toInt() }
-    if (A.windowed(times).any { w -> w.all { it == w[0] }}) {
+    val A: List<Int> = readln().split(" ").map { it.toInt() }
+    if (A.windowed(times).any { w -> w.all { it == w[0] } }) {
         println(Yes)
     } else {
         println(No)
