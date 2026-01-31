ed8f460e9631bc0454ff4838444f09d39b56e500
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-31 19:40:59 +0900

    Arrangement.

--- pgm/ABC_Dart/ABC_394_A.dart
+++ pgm/ABC_Dart/ABC_394_A.dart
@@ -6,6 +6,6 @@ void main() {
   final String S = stdin.readLineSync()!;
   const String two = '2';
   const String blank = '';
-  print(S.split(blank).where((c) => c == two).join(blank));
+  print(S.split(blank).where((s) => s == two).join(blank));
   exit(0);
 }
