f4377ab6e23f1c5403435b9eaa140f8c312a8c21
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-01-31 19:32:42 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_394_A.kt
@@ -0,0 +1,11 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val two: Char = '2'
+
+fun main() {
+    var S: String = readln()
+    println(S.filter { it == two })
+    kotlin.system.exitProcess(0)
+}
