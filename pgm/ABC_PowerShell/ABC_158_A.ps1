feeab140323ff8a835cfa4e7cb21a78d1b97bd09
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-02 06:35:53 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_158_A.ps1
@@ -0,0 +1,10 @@
+$S = [Console]::ReadLine()
+Set-Variable -Name "Yes" -Value "Yes" -Option Constant
+Set-Variable -Name "No" -Value "No" -Option Constant
+Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
+if (($S.ToCharArray() | Select-Object -Unique).Count -eq $one) {
+    Write-Host $No
+}
+else {
+    Write-Host $Yes
+}
