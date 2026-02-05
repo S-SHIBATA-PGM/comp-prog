54f98d2c2c51a308439da5e6a147eddc626623c3
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-05 22:25:05 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_389_A.kt
@@ -0,0 +1,12 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val two: Int = 2
+const val zero: Int = 0
+
+fun main() {
+    val S: String = readln()
+    println(S[zero].digitToInt() * S[two].digitToInt())
+    kotlin.system.exitProcess(0)
+}
