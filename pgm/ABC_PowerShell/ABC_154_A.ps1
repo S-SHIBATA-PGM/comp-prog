81625f4e1c6ec0b3240fef7a33141ca2efccdeb7
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-05 22:22:36 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_154_A.ps1
@@ -0,0 +1,11 @@
+$S, $T = [string[]][Console]::ReadLine().Split()
+$A, $B = [int[]][Console]::ReadLine().Split()
+$U = [Console]::ReadLine()
+Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
+if ($S -ceq $U) {
+    Write-Host (($A - $one), $B)
+}
+else {
+    Write-Host ($A, ($B - $one))
+}
+exit 0
