631ead38364e1fdd649551778a482bae9dae7834
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-05 22:29:42 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_154_A.ps1
+++ pgm/ABC_PowerShell/ABC_154_A.ps1
@@ -3,9 +3,9 @@ $A, $B = [int[]][Console]::ReadLine().Split()
 $U = [Console]::ReadLine()
 Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
 if ($S -ceq $U) {
-    Write-Host (($A - $one), $B)
+    Write-Host ($A - $one), $B
 }
 else {
-    Write-Host ($A, ($B - $one))
+    Write-Host $A, ($B - $one)
 }
 exit 0
