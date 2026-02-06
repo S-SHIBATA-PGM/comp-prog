f372a970dda575d53666897c1dd7f57ed0889a9a
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-06 22:34:31 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_111_A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    string n;
+    cin >> n;
+    const char one = '1';
+    const char nine = '9';
+    cout << (ranges::transform (
+                 n, n.begin(),
+                 [] (char c) { return c == one    ? nine
+                                      : c == nine ? one
+                                                  : c; }),
+             n)
+         << endl;
+    return 0;
+}
