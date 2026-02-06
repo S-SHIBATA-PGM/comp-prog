84afce2e79b8b97e0583c99057adad3622ac0a81
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-06 22:38:26 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_388_A.kt
@@ -0,0 +1,12 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val UPC: String = "UPC"
+const val zero: Int = 0
+
+fun main() {
+    val S: String = readln()
+    println("${S[zero]}$UPC")
+    kotlin.system.exitProcess(0)
+}
