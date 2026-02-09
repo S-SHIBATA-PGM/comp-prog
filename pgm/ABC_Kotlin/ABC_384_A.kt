8de826620f3329c8ec938bc88b34b0c0af3ea840
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-10 06:17:24 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_384_A.kt
@@ -0,0 +1,16 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val one: Int = 1
+const val two: Int = 2
+const val zero: Int = 0
+const val space: String = " "
+
+fun main() {
+    val arr = readln().split(space)
+    val (_: Int, c1: String, c2: String) = Triple(arr[zero].toInt(), arr[one], arr[two])
+    val S: String = readln()
+    println(S.replace(Regex("[^${Regex.escape(c1.toString())}]"), c2))
+    kotlin.system.exitProcess(0)
+}
