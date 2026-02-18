2af734e0b678ca6197927299eb35c5e97e0ece21
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-02-19 00:56:24 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_379_A.dart
@@ -0,0 +1,12 @@
+// import 'dart:collection';
+import 'dart:io';
+// import 'dart:math';
+
+void main() {
+  final S = stdin.readLineSync()!;
+  const String space = ' ';
+  const int one = 1;
+  const int two = 2;
+  const int zero = 0;
+  print("${S[one]}${S[two]}${S[zero]}${space}${S[two]}${S[zero]}${S[one]}");
+}
