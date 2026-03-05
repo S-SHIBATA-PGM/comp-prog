d8e2e01256e2740905a1ac9098dc8e8c4a95387c
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-05 23:14:35 +0900

    Arrangement.

--- pgm/ABC_R/ABC_114_A.r
+++ pgm/ABC_R/ABC_114_A.r
@@ -1,13 +1,14 @@
+
 con <- file(description = "stdin", open = "r")
 arr <- readLines(con = con)
 close(con)
 YES <- "YES"
 NO <- "NO"
 blank <- ""
-shichi <- 7
-go <- 5
-san <- 3
-one <- 1
+shichi <- 7L
+go <- 5L
+san <- 3L
+one <- 1L
 shichigosan <- c(shichi, go, san)
 X <- arr[one] |>
     as.integer()
