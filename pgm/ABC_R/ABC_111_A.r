a106ed11263220c8b369f28a61962d42faf87211
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-05 23:17:03 +0900

    Arrangement.

--- pgm/ABC_R/ABC_111_A.r
+++ pgm/ABC_R/ABC_111_A.r
@@ -1,8 +1,8 @@
 con <- file(description = "stdin", open = "r")
 arr <- readLines(con = con)
 close(con)
-one <- 1
-nine <- 9
+one <- 1L
+nine <- 9L
 blank <- ""
 n <- arr[one]
 chartr(paste0(one, nine), paste0(nine, one), n) |>
