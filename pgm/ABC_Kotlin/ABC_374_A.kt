b8e9832c59c4f67e69da6c2ed6f39621e5410ce2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-06 21:27:30 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_374_A.kt
@@ -0,0 +1,16 @@
+// import java.time.*
+
+// import kotlin.math.*
+
+const val san: String = "san"
+const val Yes: String = "Yes"
+const val No: String = "No"
+
+fun main() {
+    if (readln().endsWith(san)) {
+        println(Yes)
+    } else {
+       println(No)
+    }
+    kotlin.system.exitProcess(0)
+}
