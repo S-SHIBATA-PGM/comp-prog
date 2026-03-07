93411230ed8bcd98b33ee8bf62715a4df0a90422
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-07 17:40:28 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_139_A.ps1
@@ -0,0 +1,10 @@
+$S = [char[]]([Console]::ReadLine())
+$T = [char[]]([Console]::ReadLine())
+Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
+Set-Variable -Name "zero" -Value ([uint32]0) -Option Constant
+Write-Host (
+    (
+        $zero..($S.Count - $one) |
+        Where-Object { $S[$_] -ceq $T[$_] }
+    ).Count
+)
