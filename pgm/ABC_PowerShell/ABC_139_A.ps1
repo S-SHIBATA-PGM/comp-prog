27c841f5d96dc6d3e98a7b2997286dc3f93ddcff
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-08 17:23:18 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_139_A.ps1
+++ pgm/ABC_PowerShell/ABC_139_A.ps1
@@ -1,5 +1,5 @@
-$S = [char[]]([Console]::ReadLine())
-$T = [char[]]([Console]::ReadLine())
+$S = [char[]][Console]::ReadLine()
+$T = [char[]][Console]::ReadLine()
 Set-Variable -Name "one" -Value ([uint32]1) -Option Constant
 Set-Variable -Name "zero" -Value ([uint32]0) -Option Constant
 Write-Host (
