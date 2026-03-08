e7ec21e5a0d392afdc016e85ab2b043b6575a66b
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-08 09:38:58 +0900

    Arrangement.

--- pgm/ABC_R/ABC_104_A.r
+++ pgm/ABC_R/ABC_104_A.r
@@ -4,9 +4,9 @@ close(con)
 ABC <- "ABC"
 ARC <- "ARC"
 AGC <- "AGC"
+blank <- ""
 one_thousand_two_hundred <- 1200L
 two_thousand_eight_hundred <- 2800L
-blank <- ""
 one <- 1L
 zero <- 0L
 R <- arr[one] |>
