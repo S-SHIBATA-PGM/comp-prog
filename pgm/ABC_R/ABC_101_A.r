ec31b42b1698680deb1e37a50a0bc7b9e9ae7cb0
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-11 23:45:49 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_101_A.r
@@ -0,0 +1,19 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+plus <- "+"
+blank <- ""
+one <- 1L
+zero <- 0L
+S <- arr[one]
+cnt <- zero
+for (i in one:nchar(S)) {
+    if (substr(S, start = i, stop = i) == plus) {
+        cnt = cnt + one
+    } else {
+        cnt = cnt - one
+    }
+}
+cnt |>
+    cat("\n", sep = blank)
+q("no")
