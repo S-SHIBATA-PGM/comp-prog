c9752a7d10107987f99e75d57710535d91fb5d8d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-21 16:37:41 +0900

    Arrangement.

--- pgm/ABC_Kotlin/ABC_382_A.kt
+++ pgm/ABC_Kotlin/ABC_382_A.kt
@@ -8,7 +8,6 @@ const val space: String = " "
 fun main() {
     val (_: Int, D: Int) = readln().split(space).map { it.toInt() }
     val S: String = readln()
-    S.count { it == dot }
     println(S.count { it == dot } + D)
     kotlin.system.exitProcess(0)
 }
