8628da7e766c9acf11026b3ec74ed772e93fb9ec
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-22 12:46:39 +0900

    Arrangement.

--- pgm/ABC_Kotlin/ABC_364_A.kt
+++ pgm/ABC_Kotlin/ABC_364_A.kt
@@ -7,6 +7,7 @@ const val No: String = "No"
 const val sweet: String = "sweet"
 const val blank: String = ""
 const val one: Int = 1
+const val two: Int = 2
 const val zero: Int = 0
 
 fun main() {
@@ -15,17 +16,10 @@ fun main() {
     for (i in zero until N) {
         S[i] = readln()
     }
-    var cnt: Int = zero
-    for (i in zero until S.size) {
-        if (i != N - 1 && cnt == one && S[i] == sweet) {
-            println(No)
-            kotlin.system.exitProcess(0)
-        } else if (S[i] == sweet) {
-            cnt++
-        } else {
-            cnt = zero
-        }
+    if ((zero until N - two).any { i -> S[i] == sweet && S[i + one] == sweet }) {
+        println(No)
+    } else {
+        println(Yes)
     }
-    println(Yes)
     kotlin.system.exitProcess(0)
 }
