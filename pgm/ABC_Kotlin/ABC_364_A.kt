7502c7c448d9bb40c1e2898544cb9420dafd51e5
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-22 09:03:26 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_364_A.kt
@@ -0,0 +1,31 @@
+// import java.time.*
+
+import kotlin.math.*
+
+const val Yes: String = "Yes"
+const val No: String = "No"
+const val sweet: String = "sweet"
+const val blank: String = ""
+const val one: Int = 1
+const val zero: Int = 0
+
+fun main() {
+    val N: Int = readln().toInt()
+    val S: Array<String> = Array(N) { blank }
+    for (i in zero until N) {
+        S[i] = readln()
+    }
+    var cnt: Int = zero
+    for (i in zero until S.size) {
+        if (i != N - 1 && cnt == one && S[i] == sweet) {
+            println(No)
+            return
+        } else if (S[i] == sweet) {
+            cnt++
+        } else {
+            cnt = zero
+        }
+    }
+    println(Yes)
+    kotlin.system.exitProcess(0)
+}
