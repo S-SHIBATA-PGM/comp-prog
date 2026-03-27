b425f8466b4a3cbb4f943277e3f54b417a5112fc
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-27 20:23:09 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_126_A.ps1
+++ pgm/ABC_PowerShell/ABC_126_A.ps1
@@ -1,6 +1,6 @@
 $N, $K = [int[]][Console]::ReadLine().Split()
 $S = [System.Text.StringBuilder]::new([Console]::ReadLine())
 Set-Variable -Name "one" -value ([uint32]1) -Option Constant
-$S[$K - 1] = [Char]::ToLower($S[$K - 1])
+$S[$K - $one] = [Char]::ToLower($S[$K - $one])
 Write-Host $S.ToString()
 exit 0
