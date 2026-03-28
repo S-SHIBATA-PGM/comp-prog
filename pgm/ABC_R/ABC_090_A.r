d8b65541a957d114878898f60502204d135a7435
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-29 08:00:04 +0900

    Arrangement.

--- pgm/ABC_R/ABC_090_A.r
+++ pgm/ABC_R/ABC_090_A.r
@@ -1,9 +1,11 @@
-line <- readLines(con = "stdin")
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
 blank <- ""
 one <- 1L
 two <- 2L
 three <- 3L
-char <- substring(line, one:three, one:three)
+char <- substring(arr, one:three, one:three)
 char |>
     cat("\n", sep = blank)
 q("no")
