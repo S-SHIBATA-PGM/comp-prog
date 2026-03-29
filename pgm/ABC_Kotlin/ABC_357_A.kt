654f5468e9710d3061531cd4d773b427ccf7bc0c
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-29 16:49:12 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_357_A.kt
@@ -0,0 +1,27 @@
+// import java.time.*
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val space: String = " "
+const val zero: Int = 0
+
+fun main() {
+    val (N: Int, M: Int) = readln().split(space).map { it.toInt() }
+    val H: IntArray = readln().split(space).map { it.toInt() }.toIntArray()
+    var cnt: Int = zero
+    var sum: Int = zero
+    for (h in H) {
+        sum += h
+        if (sum > M) {
+            break;
+        }
+        cnt++
+    }
+    println(cnt)
+    kotlin.system.exitProcess(0)
+}
