0e528040a52b29abbd6cd661a35e79d619aeac7f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-03-30 21:11:27 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_356_A.kt
@@ -0,0 +1,18 @@
+// import java.time.*
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val space: String = " "
+
+fun main() {
+    val (N: Int, L: Int, R: Int) = readln().split(space).map { it.toInt() }
+    val A: IntArray = IntArray(N) { i: Int -> i + 1 }
+    A.reverse(L - 1, R)
+    println(A.joinToString(space))
+    kotlin.system.exitProcess(0)
+}
