593368cd76b7ee1f883207a440432fde8292044f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-04 17:05:08 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_119_A.ps1
+++ pgm/ABC_PowerShell/ABC_119_A.ps1
@@ -1,4 +1,4 @@
-$S = [Console]::ReadLine().Split()
+$S = [Console]::ReadLine()
 Set-Variable -Name "boundaryDate" -value ([string]"2019/04/30") -Option Constant
 Set-Variable -Name "Heisei" -value ([string]"Heisei") -Option Constant
 Set-Variable -Name "TBD" -value ([string]"TBD") -Option Constant
