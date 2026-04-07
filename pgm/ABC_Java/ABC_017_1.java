87de3310c90fb7b29451202a425dc162697107d5
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-07 20:32:18 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_017_1.java
@@ -0,0 +1,23 @@
+// import java.io.*;
+// import java.math.*;
+// import java.time.*;
+import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        Scanner sc = new Scanner (System.in);
+        final double tenthsBase = 10.0D;
+        final double zero = 0D;
+        double sum = zero;
+        while (sc.hasNextInt()) {
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            sum += s * (e / tenthsBase);
+        }
+        sc.close();
+        System.out.println ((int)sum);
+        System.exit (0);
+    }
+}
