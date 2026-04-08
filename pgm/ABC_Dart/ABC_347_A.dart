e5c7bf2aed1430ca33f0e104c857cda38481eb00
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-08 21:38:11 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_347_A.dart
@@ -0,0 +1,21 @@
+// import 'dart:collection';
+import 'dart:io';
+// import 'dart:math';
+import 'dart:typed_data';
+
+void main() {
+  final [int _, int K] = stdin
+      .readLineSync()!
+      .split(' ')
+      .map(int.parse)
+      .toList();
+  final Int32List A = Int32List.fromList(
+    stdin.readLineSync()!.split(' ').map(int.parse).toList(),
+  );
+  const String space = ' ';
+  const int zero = 0;
+  print(
+    A.where((a) => a % K == zero).map((a) => (a ~/ K).toString()).join(space),
+  );
+  exit(0);
+}
