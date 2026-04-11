251d1a3e8fa58edaf942c2a851c899f5d4fda2b6
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-11 19:16:40 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_111_A.ps1
@@ -0,0 +1,11 @@
+$n = [Console]::ReadLine()
+Set-Variable -Name "sOne" -value ([string]"9")
+Set-Variable -Name "sNine" -value ([string]"1")
+Set-Variable -Name "zero" -value ([int]0)
+$hash = @{
+    $sOne  = $sNine
+    $sNine = $sOne
+}
+Write-Host (
+    [regex]::Replace($n, "[$sOne$sNine]", { $hash[$args[$zero].Value] })
+)
