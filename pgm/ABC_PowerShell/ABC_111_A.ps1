5909b8350eb87d2672186cf948bb4bf5637a1a1a
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-15 20:28:45 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_111_A.ps1
+++ pgm/ABC_PowerShell/ABC_111_A.ps1
@@ -1,7 +1,7 @@
 $n = [Console]::ReadLine()
 Set-Variable -Name "sOne" -value ([string]"9")
 Set-Variable -Name "sNine" -value ([string]"1")
-Set-Variable -Name "zero" -value ([int]0)
+Set-Variable -Name "zero" -value ([uint32]0)
 $hash = @{
     $sOne  = $sNine
     $sNine = $sOne
