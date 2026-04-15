a539dbee81c9821c5762ff1fe28276e6d9e8e002
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-15 20:30:30 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_110_A.ps1
+++ pgm/ABC_PowerShell/ABC_110_A.ps1
@@ -1,8 +1,8 @@
-$abc = [int[]][Console]::ReadLine().Split()
-Set-Variable -Name "one" -value ([int]1)
-Set-Variable -Name "two" -value ([int]2)
-Set-Variable -Name "ten" -value ([int]10)
-Set-Variable -Name "zero" -value ([int]0)
+$abc = [uint32[]][Console]::ReadLine().Split()
+Set-Variable -Name "one" -value ([uint32]1)
+Set-Variable -Name "two" -value ([uint32]2)
+Set-Variable -Name "ten" -value ([uint32]10)
+Set-Variable -Name "zero" -value ([uint32]0)
 $abc = $abc | Sort-Object -Descending
 Write-Host ($ten * $abc[$zero] + $abc[$one] + $abc[$two])
 exit 0
