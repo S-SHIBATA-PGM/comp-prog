747511d38518f297c6ebacb6270fb76848715472
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-16 21:08:28 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_339_A.kt
@@ -0,0 +1,20 @@
+// import java.time.*
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val dot: String = "."
+
+fun main() {
+    val S: List<String> = readln().split(dot)
+    println(S.last())
+    kotlin.system.exitProcess(0)
+}
