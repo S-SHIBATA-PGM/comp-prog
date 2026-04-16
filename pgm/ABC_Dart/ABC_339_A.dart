506f8da72f340f9da5161c26de60995805b52a20
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-16 21:09:19 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_339_A.dart
@@ -0,0 +1,11 @@
+//  'dart:collection';
+import 'dart:io';
+// import 'dart:math';
+// import 'dart:typed_data';
+
+void main() {
+  const String dot = '.';
+  final List<String> S = stdin.readLineSync()!.split(dot);
+  print(S.last);
+  exit(0);
+}
