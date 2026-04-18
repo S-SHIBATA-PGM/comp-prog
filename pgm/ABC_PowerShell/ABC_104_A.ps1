be57e88cdbac83fd7285dce3609f754bcaf495d2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-18 09:40:59 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_104_A.ps1
@@ -0,0 +1,22 @@
+$R = [uint32][Console]::ReadLine()
+Set-Variable -Name "ABC" -value ([string]"ABC")
+Set-Variable -Name "ARC" -value ([string]"ARC")
+Set-Variable -Name "AGC" -value ([string]"AGC")
+Set-Variable -Name "oneThousandTwoHundred" -value ([uint32]1200)
+Set-Variable -Name "twoThousandEightHundred" -value ([uint32]2800)
+Set-Variable -Name "one" -value ([uint32]1)
+$Rating = @(
+    [PSCustomObject]@{ threshold = $oneThousandTwoHundred; contest = $ABC }
+    [PSCustomObject]@{ threshold = $twoThousandEightHundred; contest = $ARC }
+)
+if ($Rating |
+    Where-Object { $_.threshold -gt $R } |
+    Select-Object -First $one) {
+    Write-Host ($Rating |
+        Where-Object { $_.threshold -gt $R } |
+        Select-Object -First $one).contest
+}
+else {
+    Write-Host $AGC
+}
+exit 0
