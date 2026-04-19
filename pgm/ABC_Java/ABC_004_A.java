1f6260ad7c5f08bbf67aaec629dee840712eda84
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-19 16:55:04 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_004_A.java
@@ -0,0 +1,18 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final int N = Integer.parseInt (reader.readLine());
+        reader.close();
+        final int two = 2;
+        System.out.println (two * N);
+        System.exit (0);
+    }
+}
