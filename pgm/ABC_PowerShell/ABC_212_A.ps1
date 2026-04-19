26f88606e6c0e9666df12e4b15b4c261bd9aa273
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-19 17:10:52 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_212_A.ps1
+++ pgm/ABC_PowerShell/ABC_212_A.ps1
@@ -1,8 +1,8 @@
 $A, $B = [int[]][Console]::ReadLine().Split()
-Set-Variable -Name zero -Value 0 -Option Constant
 Set-Variable -Name Gold -Value "Gold" -Option Constant
 Set-Variable -Name Silver -Value "Silver" -Option Constant
 Set-Variable -Name Alloy -Value "Alloy" -Option Constant
+Set-Variable -Name zero -Value 0 -Option Constant
 if ($A -gt $zero -and $B -gt $zero) {
     Write-Host Alloy
 }
