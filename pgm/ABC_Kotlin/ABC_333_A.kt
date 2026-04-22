50a499f15f4868528f6e52a7c321907d200b8357
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-22 21:21:56 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_333_A.kt
@@ -0,0 +1,18 @@
+// import java.time.*
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+fun main() {
+    val N: Int = readln().toInt()
+    println(N.toString().repeat(N))
+    kotlin.system.exitProcess(0)
+}
