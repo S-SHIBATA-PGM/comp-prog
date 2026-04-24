cc488339015618ddc0ed08e43c30435d16c5250d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-25 00:40:20 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_063_A.r
@@ -0,0 +1,21 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+error <- "error"
+blank <- ""
+space <- " "
+one <- 1
+two <- 2
+ten <- 10
+A <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
+    as.integer()
+B <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
+    as.integer()
+if (A + B < ten) {
+    (A + B) |>
+        cat("\n", sep = blank)
+} else {
+    error |>
+        cat("\n", sep = blank)
+}
+q("no")
