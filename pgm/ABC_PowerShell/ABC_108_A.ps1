dd16ed0d7968096b9c5c0b9ac02b25fa6f34ddfd
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-25 19:12:14 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_108_A.ps1
+++ pgm/ABC_PowerShell/ABC_108_A.ps1
@@ -1,5 +1,5 @@
 $K = [uint32][Console]::ReadLine()
-Set-Variable -Name "two" -value ([uint32]2)
+Set-Variable -Name "two" -value ([uint32]2) -Option Constant
 Write-Host ([Math]::truncate($K / $two) *
     ([Math]::truncate($K / $two) + $K % $two))
 exit 0
