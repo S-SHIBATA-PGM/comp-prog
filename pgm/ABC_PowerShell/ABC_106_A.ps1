de0bfbdbc69a11372757127178f03a14c55b0f8f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-25 19:13:49 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_106_A.ps1
+++ pgm/ABC_PowerShell/ABC_106_A.ps1
@@ -1,4 +1,4 @@
 $A, $B = [uint32[]][Console]::ReadLine().Split()
-Set-Variable -Name "one" -value ([uint32]1)
+Set-Variable -Name "one" -value ([uint32]1) -Option Constant
 Write-Host (($A - $one) * ($B - $one))
 exit 0
