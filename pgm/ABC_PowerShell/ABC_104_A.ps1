5418ce62e3fb69d75efe5b8311199776dc691653
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-25 19:15:31 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_104_A.ps1
+++ pgm/ABC_PowerShell/ABC_104_A.ps1
@@ -1,10 +1,10 @@
 $R = [uint32][Console]::ReadLine()
-Set-Variable -Name "ABC" -value ([string]"ABC")
-Set-Variable -Name "ARC" -value ([string]"ARC")
-Set-Variable -Name "AGC" -value ([string]"AGC")
-Set-Variable -Name "oneThousandTwoHundred" -value ([uint32]1200)
-Set-Variable -Name "twoThousandEightHundred" -value ([uint32]2800)
-Set-Variable -Name "one" -value ([uint32]1)
+Set-Variable -Name "ABC" -value ([string]"ABC") -Option Constant
+Set-Variable -Name "ARC" -value ([string]"ARC") -Option Constant
+Set-Variable -Name "AGC" -value ([string]"AGC") -Option Constant
+Set-Variable -Name "oneThousandTwoHundred" -value ([uint32]1200) -Option Constant
+Set-Variable -Name "twoThousandEightHundred" -value ([uint32]2800) -Option Constant
+Set-Variable -Name "one" -value ([uint32]1) -Option Constant
 $rating = @(
     [PSCustomObject]@{ threshold = $oneThousandTwoHundred; contest = $ABC }
     [PSCustomObject]@{ threshold = $twoThousandEightHundred; contest = $ARC }
