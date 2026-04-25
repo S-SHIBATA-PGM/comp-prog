1df4580023042eb256a7bc667b777c742a93d5a4
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-04-25 19:18:56 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_099_A.ps1
+++ pgm/ABC_PowerShell/ABC_099_A.ps1
@@ -1,7 +1,7 @@
 $N = [uint32][Console]::ReadLine()
-Set-Variable -Name "ABC" -value ([string]"ABC")
-Set-Variable -Name "ABD" -value ([string]"ABD")
-Set-Variable -Name "thousand" -value ([uint32]1000)
+Set-Variable -Name "ABC" -value ([string]"ABC") -Option Constant
+Set-Variable -Name "ABD" -value ([string]"ABD") -Option Constant
+Set-Variable -Name "thousand" -value ([uint32]1000) -Option Constant
 if ($N -lt $thousand) {
     Write-Host $ABC
 }
