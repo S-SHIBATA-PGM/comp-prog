b6cba2bdd9b39e3476d0408e53758e8e0341eb4f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-01 13:45:27 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_324_A.kt
@@ -0,0 +1,28 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.*
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val Yes: String = "Yes"
+const val No: String = "No"
+const val space: String = " "
+const val zero: Int = 0
+
+fun main() {
+    readln()
+    val A = readln().split(space).map { it.toInt() }.toIntArray()
+    if (A.all { it == A[zero] }) {
+        println(Yes)
+    } else {
+        println(No)
+    }
+    kotlin.system.exitProcess(0)
+}
