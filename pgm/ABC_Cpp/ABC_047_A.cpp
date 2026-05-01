6614c6f3b933f8a65fcfdefdb01a314f39aff2e0
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-01 13:42:06 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_047_A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return (f)(begin (ALL), end (ALL), ##__VA_ARGS__);                     \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    const string Yes = "Yes";
+    const string No = "No";
+    const int one = 1;
+    const int two = 2;
+    const int zero = 0;
+    int abc[] = {a, b, c};
+    ALL (sort, abc);
+    if (abc[zero] + abc[one] == abc[two]) {
+        cout << Yes << endl;
+    } else {
+        cout << No << endl;
+    }
+    return 0;
+}
