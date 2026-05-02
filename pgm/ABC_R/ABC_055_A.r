bc9e4d5b459d1b497f6a0c9b49cd01d7e96099d7
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-02 09:51:43 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_055_A.r
@@ -0,0 +1,13 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+blank <- ""
+one <- 1L
+fifteen <- 15L
+two_hundred <- 200L
+eight_hundred <- 800L
+N <- arr[one] |>
+  as.integer()
+(eight_hundred * N - two_hundred * floor(N / 15)) |>
+  cat("\n", sep = blank)
+q("no")
