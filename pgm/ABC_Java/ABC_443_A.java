fc917343c64aca6415005e3461a19ee8c358eca8
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-04 09:29:04 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_443_A.java
@@ -0,0 +1,18 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final String S = reader.readLine();
+        reader.close();
+        final String s = "s";
+        System.out.println (S.concat (s));
+        System.exit (0);
+    }
+}
