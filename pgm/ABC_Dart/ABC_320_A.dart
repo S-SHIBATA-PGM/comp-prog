599a9dfaabc999052f5c27184f58746123c4a956
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-05 06:05:48 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_320_A.dart
@@ -0,0 +1,14 @@
+//  'dart:collection';
+import 'dart:io';
+import 'dart:math';
+// import 'dart:typed_data';
+
+void main() {
+  final [int A, int B] = stdin
+      .readLineSync()!
+      .split(' ')
+      .map(int.parse)
+      .toList();
+  print(pow(A, B) + pow(B, A));
+  exit(0);
+}
