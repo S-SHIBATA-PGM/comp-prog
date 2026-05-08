4e9ce95db7aef0acedda986c83091fdf71b6f10f
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-08 23:34:09 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_317_A.kt
@@ -0,0 +1,22 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.*
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val space: String = " "
+const val one: Int = 1
+
+fun main() {
+    val (N: Int, H: Int, X: Int) = readln().split(space).map { it.toInt() }
+    val P: IntArray = readln().split(space).map { it.toInt() }.toIntArray()
+    println(P.indexOfFirst { H + it >= X } + one)
+    kotlin.system.exitProcess(0)
+}
