6a2059bb2155fce86232ed0d0041d3e9b482cdad
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-16 07:39:01 +0900

    Arrangement.

--- pgm/ABC_Cpp/ABC_033_A.cpp
+++ pgm/ABC_Cpp/ABC_033_A.cpp
@@ -5,7 +5,6 @@
 
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define REP(i, n) FOR (i, 0, n)
-
 #define ALL(f, x, ...)                                                         \
     ([&] (decltype ((x)) ALL) {                                                \
         return f (begin (ALL), end (ALL), ##__VA_ARGS__);                      \
