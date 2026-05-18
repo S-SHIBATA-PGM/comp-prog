91a6d5c41794616ce3f62ed1b15a9556138400d2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-18 22:54:33 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_429_A.java
@@ -0,0 +1,29 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        String[] arr = reader.readLine().split (" ");
+        reader.close();
+        final String OK = "OK";
+        final String too = "Too Many Requests";
+        final int one = 1;
+        final int zero = 0;
+        final int N = Integer.parseInt (arr[zero]);
+        final int M = Integer.parseInt (arr[one]);
+        for (int i = zero; i < N; i++) {
+            if (i < M) {
+                System.out.println (OK);
+            } else {
+                System.out.println (too);
+            }
+        }
+        System.exit (0);
+    }
+}
