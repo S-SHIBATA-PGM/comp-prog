8bb353b934faf5c5e54c3f2c763a9392678849d5
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-19 21:29:36 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_428_A.java
@@ -0,0 +1,26 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        String[] arr = reader.readLine().split (" ");
+        reader.close();
+        final int one = 1;
+        final int two = 2;
+        final int three = 3;
+        final int zero = 0;
+        final int S = Integer.parseInt (arr[zero]);
+        final int A = Integer.parseInt (arr[one]);
+        final int B = Integer.parseInt (arr[two]);
+        final int X = Integer.parseInt (arr[three]);
+        System.out.println (S * A * (X / (A + B)) +
+                            S * Math.min (A, X % (A + B)));
+        System.exit (0);
+    }
+}
