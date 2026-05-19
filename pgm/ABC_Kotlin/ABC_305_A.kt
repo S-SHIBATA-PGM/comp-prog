6cfaf83cb5b57173b1a09fe3c17af4f9ce3254a4
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-05-19 21:38:02 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_305_A.kt
@@ -0,0 +1,21 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.*
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val two: Int = 2
+const val five: Int = 5
+
+fun main() {
+    val N: Int = readln().toInt()
+    println(((N + (five / two)) / five) * five)
+    kotlin.system.exitProcess(0)
+}
