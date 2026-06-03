b6d25c9b1f73a546275fa886c972e0ace514990d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-04 06:26:38 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_289_A.kt
@@ -0,0 +1,25 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.*
+// import java.util.Scanner
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val blank: String = ""
+const val one: Int = 1
+
+fun main() {
+    val s: String = readln()
+    println(s.map { c: Char ->
+        val code: Int = c.code xor one
+        code.toChar()
+    }.joinToString(separator = blank))
+    kotlin.system.exitProcess(0)
+}
