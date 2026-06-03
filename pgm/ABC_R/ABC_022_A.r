96cde6396ee7f5f52e95e5fae6e36e1d0b7746c1
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-04 06:22:41 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_022_A.r
@@ -0,0 +1,23 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+blank <- ""
+space <- " "
+one <- 1L
+two <- 2L
+three <- 3L
+four <- 4L
+N <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
+  as.integer()
+S <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
+  as.integer()
+T <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][three] |>
+  as.integer()
+W <- arr[two] |>
+  as.integer()
+A <- (arr[-c(one, two)]) |>
+  as.integer()
+A <- cumsum(c(W, A))
+sum(A >= S & A <= T) |>
+  cat("\n", sep = blank)
+q("no")
