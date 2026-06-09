e9d5adc307647fac2a06f6fe9078d443c2f04d99
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-09 20:29:53 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_284_A.kt
@@ -0,0 +1,25 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.*
+import java.util.Scanner
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+fun main() {
+    val scanner: Scanner = Scanner(System.`in`)
+    val N: Int = scanner.nextInt()
+    val S: List<String> = List(N) {
+        scanner.next()
+    }
+    S.reversed().forEach { s: String ->
+        println(s)
+    }
+    kotlin.system.exitProcess(0)
+}
