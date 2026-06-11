04f76f8b1f9a50fda69dd6fc3daaebb9e12bd4ae
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-11 21:06:35 +0900

    Arrangement.

--- pgm/ABC_Java/ABC_006_A.java
+++ pgm/ABC_Java/ABC_006_A.java
@@ -13,8 +13,12 @@ public class Main {
         reader.close();
         final String YES = "YES";
         final String NO = "NO";
+        final char cThree = '3';
         final int three = 3;
-        System.out.println (N % three == 0 ? YES : NO);
+        final int zero = 0;
+        System.out.println (
+            String.valueOf (N).indexOf (cThree) > zero || N % three == 0 ? YES
+                                                                         : NO);
         System.exit (0);
     }
 }
