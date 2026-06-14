485c43de65865dd5953ede451e1e22782912488e
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-14 11:07:08 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_402_A.java
@@ -0,0 +1,25 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        BufferedReader reader =
+            new BufferedReader (new InputStreamReader (System.in));
+        final String S = reader.readLine();
+        reader.close();
+        StringBuilder sb = new StringBuilder();
+        int len = S.length();
+        for (int i = 0; i < len; i++) {
+            char c = S.charAt (i);
+            if (Character.isUpperCase (c)) {
+                sb.append (c);
+            }
+        }
+        System.out.println (sb.toString());
+        System.exit (0);
+    }
+}
