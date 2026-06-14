35b57f4e621e4c463d0fb9df53ff106dddfc6de5
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-14 11:14:59 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_279_A.kt
@@ -0,0 +1,29 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.*
+// import java.util.Scanner
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val v: Char = 'v'
+const val one: Int = 1
+const val two: Int = 2
+
+fun main() {
+    val S: String = readln()
+    println(S.sumOf { c: Char ->
+        if (c == v) {
+            one
+        } else {
+            two
+        }
+    })
+    kotlin.system.exitProcess(0)
+}
