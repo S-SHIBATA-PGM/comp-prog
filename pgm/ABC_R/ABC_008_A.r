f25fc6a97f8f42c2fdf78e46daf0252200c909ef
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-20 20:07:04 +0900

    Arrangement.

--- pgm/ABC_R/ABC_008_A.r
+++ pgm/ABC_R/ABC_008_A.r
@@ -3,8 +3,8 @@ arr <- readLines(con = con)
 close(con)
 blank <- ""
 space <- " "
-one <- 1
-two <- 2
+one <- 1L
+two <- 2L
 S <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
   as.integer()
 t <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
