5856261ec07977870dacaaaa73a7da966404740d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-20 20:14:31 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_264_A.kt
@@ -0,0 +1,23 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.*
+// import java.util.Scanner
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val atcoder: String = "atcoder"
+const val space: String = " "
+const val one: Int = 1
+
+fun main() {
+    val (L: Int, R: Int) = readln().split(space).map { it.toInt() }
+    println(atcoder.substring(L - one, R))
+    kotlin.system.exitProcess(0)
+}
