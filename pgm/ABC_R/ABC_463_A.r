ef08ad6a3ddac4294208930ec1de41da8d7aefad
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-26 20:34:45 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_463_A.r
@@ -0,0 +1,23 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+Yes <- "Yes";
+No <- "No";
+blank <- ""
+space <- " "
+one <- 1L
+two <- 2L
+nine <- 9L
+sixteen <- 16L
+X <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
+  as.integer()
+Y <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
+  as.integer()
+if (nine * X == sixteen * Y) {
+  Yes |>
+    cat("\n", sep = blank)
+} else {
+  No |>
+    cat("\n", sep = blank)
+}
+q("no")
