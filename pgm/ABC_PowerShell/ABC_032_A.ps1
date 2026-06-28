fc62c87a0492ecd870df5c442ad2c61657ac75bb
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-28 19:05:34 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_032_A.ps1
@@ -0,0 +1,32 @@
+function Get-Gcd {
+    param (
+        [int64]$a,
+        [int64]$b
+    )
+    $result = [int64]$a
+    $k = [int64]0
+    $n = [int64]$b
+
+    do {
+        $k = [int64]($result % $n)
+        $result = [int64]$n
+        $n = [int64]$k
+    } while ($k -ne 0)
+    return $result
+}
+
+function Get-Lcm {
+    param (
+        [int64]$a,
+        [int64]$b
+    )
+    $g = [int64](Get-Gcd -a $a -b $b)
+    return [int64]($a * [Math]::Truncate($b / $g))
+}
+
+$a = [int64][Console]::ReadLine()
+$b = [int64][Console]::ReadLine()
+$n = [int64][Console]::ReadLine()
+Set-Variable -Name "one" -value ([int64]1) -Option Constant
+$lcm = [int64](Get-Lcm -a $a -b $b)
+Write-Host ([int64]([Math]::Truncate(($lcm + $n - $one) / $lcm) * $lcm))
