511235304c7dfb5c9bb4fe1ea514ac8395baabb1
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-06-30 20:19:18 +0900

    Arrangement.

--- pgm/ABC_R/ABC_459_A.r
+++ pgm/ABC_R/ABC_459_A.r
@@ -1,13 +1,13 @@
 con <- file(description = "stdin", open = "r")
 arr <- readLines(con = con)
 close(con)
-hello_world <- "HelloWorld"
+HelloWorld <- "HelloWorld"
 blank <- ""
 space <- " "
 one <- 1L
 X <- arr[one] |>
   as.integer()
-strsplit(x = hello_world, split = blank, fixed = TRUE)[[one]][-X] |>
+strsplit(x = HelloWorld, split = blank, fixed = TRUE)[[one]][-X] |>
   paste(collapse = blank) |>
   cat("\n", sep = blank)
 q("no")
