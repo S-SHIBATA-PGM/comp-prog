a0e3041b533bb33ed6247ab79bf91ee0a5386873
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-17 22:28:58 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_448_B.java
@@ -0,0 +1,74 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        FastScanner sc = new FastScanner();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        final int one = 1;
+        final int zero = 0;
+        int[] C = new int[M];
+        for (int i = zero; i < M; i++) {
+            C[i] = sc.nextInt();
+        }
+        int[] A = new int[N];
+        int[] B = new int[N];
+        int sum = zero;
+        for (int i = zero; i < N; i++) {
+            A[i] = sc.nextInt();
+            B[i] = sc.nextInt();
+            int idx = A[i] - one;
+            int min = Math.min (B[i], C[idx]);
+            sum += min;
+            C[idx] -= min;
+        }
+        PrintWriter out =
+            new PrintWriter (new BufferedOutputStream (System.out));
+        out.println (sum);
+        out.flush();
+        System.exit (0);
+    }
+    // FastScanner start
+    static class FastScanner {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+        public FastScanner() {
+            din = new DataInputStream (System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read (buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+    }
+    // FastScanner end
+}
