5344a64a5f4bbece20be30cf7048d47b0c3509ce
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-22 21:10:59 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_438_A.r
@@ -0,0 +1,16 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+blank <- ""
+space <- " "
+yn <- "\n"
+one <- 1L
+two <- 2L
+seven <- 7
+D <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
+  as.integer()
+f <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
+  as.integer()
+((f + seven * ((D - f) %/% seven + one) - one) %% D + one) |>
+  cat(yn, sep = blank)
+q("no")
