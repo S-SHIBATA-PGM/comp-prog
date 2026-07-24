e6894c75f3a92957df81de5516e9dc5510152133
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-24 19:30:49 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_231_A.dart
@@ -0,0 +1,12 @@
+// import 'dart:collection';
+// import 'dart:convert';
+import 'dart:io';
+// import 'dart:math';
+// import 'dart:typed_data';
+
+void main() {
+  final double D = double.parse(stdin.readLineSync()!);
+  const double divisor = 100.0;
+  print(D / divisor);
+  exit(0);
+}
