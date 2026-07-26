bcc11a53aa495d537fb3862f903e789224e3adc2
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-27 07:23:11 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_433_A.r
@@ -0,0 +1,35 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+Yes <- "Yes"
+No <- "No"
+blank <- ""
+space <- " "
+yn <- "\n"
+one <- 1
+two <- 2
+three <- 3
+zero <- 0
+X <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
+  as.integer()
+Y <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
+  as.integer()
+Z <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][three] |>
+  as.integer()
+# nolint start
+# 計算過程
+# X + k = Z * (Y + k)
+# X + k = Z * Y + Z * k
+# k - Z * k = Z * Y - X
+# k(1 - Z) = Z * Y - X
+# k(Z - 1) = X - Z * Y
+# k = (X - Z * Y) / (Z - 1)
+# nolint end
+if ((X - Y * Z) >= zero && (X - Y * Z) %% (Z - one) == zero) {
+  Yes |>
+    cat(yn, sep = blank)
+} else {
+  No |>
+    cat(yn, sep = blank)
+}
+q("no")
