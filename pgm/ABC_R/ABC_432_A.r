75bd79b53eca58a64761c7c088b6610bd145da76
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-28 07:58:40 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_432_A.r
@@ -0,0 +1,13 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+blank <- ""
+space <- " "
+yn <- "\n"
+one <- 1
+abc <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]] |>
+  as.integer() |>
+  sort(decreasing = TRUE) |>
+  paste(collapse = blank) |>
+  cat(yn, sep = blank)
+q("no")
