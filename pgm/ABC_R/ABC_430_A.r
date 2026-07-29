18889763736b504fdd88a43d6db9eb5cd37b6b12
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-30 08:22:00 +0900

    Arrangement.

---
+++ pgm/ABC_R/ABC_430_A.r
@@ -0,0 +1,28 @@
+con <- file(description = "stdin", open = "r")
+arr <- readLines(con = con)
+close(con)
+Yes <- "Yes"
+No <- "No"
+blank <- ""
+space <- " "
+yn <- "\n"
+one <- 1
+two <- 2
+three <- 3
+four <- 4
+A <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
+  as.integer()
+B <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
+  as.integer()
+C <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][three] |>
+  as.integer()
+D <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][four] |>
+  as.integer()
+if (A <= C && D < B) {
+  Yes |>
+    cat(yn, sep = blank)
+} else {
+  No |>
+    cat(yn, sep = blank)
+}
+q("no")
