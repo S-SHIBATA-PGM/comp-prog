62e5a4e969329086e7550ec88c070f2d5639ea1d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-30 08:30:40 +0900

    Arrangement.

---
+++ pgm/ABC_PHP/ABC_437_A.php
@@ -0,0 +1,9 @@
+<?php
+$stream = fopen('php://stdin', 'r');
+$line = fgets($stream);
+fclose($stream);
+const space = ' ';
+const twelve = 12;
+[$A, $B] = array_map('intval', explode(space, trim($line)));
+echo $A * twelve + $B . PHP_EOL;
+exit(0);
