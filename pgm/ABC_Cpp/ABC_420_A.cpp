fbf3e93c18e9d81b549a738ce04645e82c2949ac
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-07-30 08:23:20 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_420_A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return f (begin (ALL), end (ALL), ##__VA_ARGS__);                      \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    ios::sync_with_stdio (false);
+    cin.tie (nullptr);
+    int X, Y;
+    cin >> X >> Y;
+    constexpr string_view yn = "\n";
+    constexpr int one = 1;
+    cout << unsigned ((chrono::year {chrono::year_month_day {
+                           floor<chrono::days> (chrono::system_clock::now())}
+                                         .year()} /
+                           chrono::month {one} / chrono::day {one} +
+                       chrono::months {X + Y - one})
+                          .month())
+         << yn;
+    return 0;
+}
