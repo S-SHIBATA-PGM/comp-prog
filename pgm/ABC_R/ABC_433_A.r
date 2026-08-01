2fb188f260c3ba2e17d03ae84d7aee095ee7f8c4
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-01 17:45:20 +0900

    Arrangement.

--- pgm/ABC_R/ABC_433_A.r
+++ pgm/ABC_R/ABC_433_A.r
@@ -6,10 +6,10 @@ No <- "No"
 blank <- ""
 space <- " "
 yn <- "\n"
-one <- 1
-two <- 2
-three <- 3
-zero <- 0
+one <- 1L
+two <- 2L
+three <- 3L
+zero <- 0L
 X <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][one] |>
   as.integer()
 Y <- strsplit(x = arr[one], split = space, fixed = TRUE)[[one]][two] |>
