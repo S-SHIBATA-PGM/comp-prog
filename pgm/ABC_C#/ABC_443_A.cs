ba9200a9721358c6efff205ce452b43287b2d710
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-03 21:00:45 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_443_A.cs
@@ -0,0 +1,18 @@
+using System;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Text;
+// using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string S = Console.ReadLine();
+        const string s = "s";
+        Console.WriteLine(string.Concat(S, s));
+        Environment.Exit(0);
+    }
+}
