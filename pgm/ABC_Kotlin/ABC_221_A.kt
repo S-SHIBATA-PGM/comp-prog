332861fcae2b9904653552a3c0061ba928463aa6
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-04 20:37:06 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_221_A.kt
@@ -0,0 +1,23 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+
+import kotlin.math.*
+
+const val space: String = " "
+const val thirtyTwo: Double = 32.0
+
+fun main() {
+    var (A: Double, B: Double) = readln().split(space).map { it.toDouble() }
+    println(thirtyTwo.pow(A - B).toInt())
+    kotlin.system.exitProcess(0)
+}
