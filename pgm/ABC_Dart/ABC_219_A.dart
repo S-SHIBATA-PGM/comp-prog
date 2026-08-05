8fa39edd64dd51cf606fe465b257b90411c72ac0
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-06 07:59:18 +0900

    Arrangement.

---
+++ pgm/ABC_Dart/ABC_219_A.dart
@@ -0,0 +1,17 @@
+// import 'dart:collection';
+// import 'dart:convert';
+import 'dart:io';
+// import 'dart:math';
+// import 'dart:typed_data';
+
+void main() {
+  final int X = int.parse(stdin.readLineSync()!);
+  const String expert = "expert";
+  final List<int> rank = [40, 70, 90];
+  final int? next = rank.cast<int?>().firstWhere(
+    (r) => r! > X,
+    orElse: () => null,
+  );
+  print(next != null ? next - X : expert);
+  exit(0);
+}
