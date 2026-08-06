c223db52d79454f733791f2921b8be5b6e318893
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-07 07:45:24 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_462_A.ps1
@@ -0,0 +1,14 @@
+$S = [Console]::ReadLine()
+Set-Variable -Name cZero -value ([char]'0') -Option Constant
+Set-Variable -Name cNine -value ([char]'9') -Option Constant
+Set-Variable -Name zero -value ([uint32]0) -Option Constant
+$len = $S.Length
+$sb = [System.Text.StringBuilder]::new($len)
+for ($i = $zero; $i -lt $len; $i++) {
+    $c = $S[$i]
+    if ($c -ge $cZero -and $c -le $cNine) {
+        [void]$sb.Append($c)
+    }
+}
+Write-Host $sb.ToString()
+exit 0
