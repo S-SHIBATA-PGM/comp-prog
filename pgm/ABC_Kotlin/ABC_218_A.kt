95f009dc1134055eb55db8abfad91aa7ce54e9d4
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-07 07:48:01 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_218_A.kt
@@ -0,0 +1,30 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val Yes: String = "Yes"
+const val No: String = "No"
+const val circle: Char = 'o'
+const val one: Int = 1
+
+fun main() {
+    val N = readln().toInt()
+    val S = readln()
+    if (S[N - one] == circle) {
+        println(Yes)
+    } else {
+        println(No)
+    }
+    kotlin.system.exitProcess(0)
+}
