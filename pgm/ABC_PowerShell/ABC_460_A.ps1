f442f6c5158cc4f0de9fb3f97169089f57052000
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-09 08:46:33 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_460_A.ps1
@@ -0,0 +1,10 @@
+$N, $M = [uint32[]][Console]::ReadLine().Split()
+Set-Variable -Name one -value ([uint32]1) -Option Constant
+Set-Variable -Name zero -value ([uint32]0) -Option Constant
+$cnt = $zero
+while ($M -gt $zero) {
+    $cnt += $one
+    $M = $N % $M
+}
+Write-Host $cnt
+exit 0
