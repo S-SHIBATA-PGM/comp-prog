1f30517684ff4ac252c9b19f519d9ab159598e5b
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-09 08:48:40 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_216_A.kt
@@ -0,0 +1,32 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val minus: String = "-"
+const val dot: String = "."
+const val plus: String = "+"
+const val blank: String = ""
+const val two: Int = 2
+const val six: Int = 6
+
+fun main() {
+    val (X, Y) = readln().split(dot).map { it.toInt() }
+    val sign: String = when {
+        Y <= two -> minus
+        Y <= six -> blank
+        else -> plus
+    }
+    println("$X$sign")
+    kotlin.system.exitProcess(0)
+}
