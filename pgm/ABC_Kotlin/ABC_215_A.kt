c805ed7c83979a9f6e784cce82941b98de71a8ad
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-10 09:56:48 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_215_A.kt
@@ -0,0 +1,28 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+
+// import kotlin.math.*
+
+const val AC: String = "AC"
+const val WA: String = "WA"
+const val hello: String = "Hello,World!"
+
+fun main() {
+    val S = readln()
+    if (S == hello) {
+        println(AC)
+    } else {
+        println(WA)
+    }
+    kotlin.system.exitProcess(0)
+}
