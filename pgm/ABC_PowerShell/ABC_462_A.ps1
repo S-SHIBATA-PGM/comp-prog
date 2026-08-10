b60f93d2700c3af799f78e5fa9bd11ef285a9ef3
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-10 09:47:13 +0900

    Arrangement.

--- pgm/ABC_PowerShell/ABC_462_A.ps1
+++ pgm/ABC_PowerShell/ABC_462_A.ps1
@@ -1,7 +1,7 @@
 $S = [Console]::ReadLine()
-Set-Variable -Name cZero -value ([char]'0') -Option Constant
-Set-Variable -Name cNine -value ([char]'9') -Option Constant
-Set-Variable -Name zero -value ([uint32]0) -Option Constant
+Set-Variable -Name "cZero" -value ([char]'0') -Option Constant
+Set-Variable -Name "cNine" -value ([char]'9') -Option Constant
+Set-Variable -Name "zero" -value ([uint32]0) -Option Constant
 $len = $S.Length
 $sb = [System.Text.StringBuilder]::new($len)
 for ($i = $zero; $i -lt $len; $i++) {
