dac43b6c0f26845161b4642573a9443be8ba55b7
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-12 08:51:04 +0900

    Arrangement.

---
+++ pgm/ABC_PHP/ABC_426_A.php
@@ -0,0 +1,13 @@
+<?php
+$stream = fopen('php://stdin', 'r');
+$line = fgets($stream);
+fclose($stream);
+const Yes = 'Yes';
+const No = 'No';
+const space = ' ';
+[$X, $Y] = explode(space, trim($line));
+$OS = ['Ocelot', 'Serval', 'Lynx'];
+echo (array_search($Y, $OS, true) <= array_search($X, $OS, true)
+    ? Yes
+    : No) . PHP_EOL;
+exit(0);
