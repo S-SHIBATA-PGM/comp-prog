b68e30c44cd4f2f898c41e5c4879da71c44af53d
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-11 10:26:04 +0900

    Arrangement.

---
+++ pgm/ABC_PHP/ABC_427_A.php
@@ -0,0 +1,14 @@
+<?php
+$stream = fopen('php://stdin', 'r');
+$line = fgets($stream);
+const blank = '';
+const one = 1;
+const two = 2;
+$S = trim($line);
+echo substr_replace(
+    $S,
+    blank,
+    intdiv(strlen($S) + one, two) - one,
+    one
+) . PHP_EOL;
+exit(0);
