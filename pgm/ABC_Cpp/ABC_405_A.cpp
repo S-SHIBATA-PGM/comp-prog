aa7d228f2536151dfa13489be13cd0e507626790
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-15 09:22:10 +0900

    Arrangement.

---
+++ pgm/ABC_Cpp/ABC_405_A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define __STDC_FORMAT_MACROS
+#define p64 PRId64
+
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define REP(i, n) FOR (i, 0, n)
+#define ALL(f, x, ...)                                                         \
+    ([&] (decltype ((x)) ALL) {                                                \
+        return f (begin (ALL), end (ALL), ##__VA_ARGS__);                      \
+    }) (x)
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    ios::sync_with_stdio (false);
+    cin.tie (nullptr);
+    int R, X;
+    cin >> R >> X;
+    constexpr string_view Yes = "Yes";
+    constexpr string_view No = "No";
+    constexpr string_view yn = "\n";
+    constexpr int lower1 = 1600;
+    constexpr int lower2 = 1200;
+    constexpr int upper1 = 2999;
+    constexpr int upper2 = 2399;
+    constexpr int one = 1;
+    constexpr int zero = 0;
+    constexpr int lower[] = {lower1, lower2};
+    constexpr int upper[] = {upper1, upper2};
+    cout << (lower[X - 1] <= R && R <= upper[X - 1] ? Yes : No) << yn;
+    return 0;
+}
