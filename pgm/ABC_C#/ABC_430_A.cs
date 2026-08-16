3c00c9eb6c80705c3e5853e2bbd3d4614e693004
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-16 09:51:38 +0900

    Arrangement.

---
+++ pgm/ABC_C#/ABC_430_A.cs
@@ -0,0 +1,27 @@
+using System;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using System.Text;
+// using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string[] arr = Console.ReadLine().Split(' ');
+        const string Yes = "Yes";
+        const string No = "No";
+        const int one = 1;
+        const int two = 2;
+        const int three = 3;
+        const int zero = 0;
+        int A = int.Parse(arr[zero]);
+        int B = int.Parse(arr[one]);
+        int C = int.Parse(arr[two]);
+        int D = int.Parse(arr[three]);
+        Console.WriteLine(A <= C && D < B ? Yes : No);
+        Environment.Exit(0);
+    }
+}
