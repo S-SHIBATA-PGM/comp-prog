cae2fcb77606532950a4a8d2a11940a2e05dbcc0
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-17 21:27:36 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_208_A.kt
@@ -0,0 +1,32 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.math.BigDecimal
+// import java.math.RoundingMode
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+// import java.util.TreeMap
+
+// import kotlin.math.*
+
+const val Yes: String = "Yes"
+const val No: String = "No"
+const val space: String = " "
+const val six: Int = 6
+
+fun main() {
+    val (A: Int, B: Int) = readln().split(space).map { it.toInt() }
+    if (A <= B && B <= A * six) {
+        println(Yes)
+    } else {
+        println(No)
+    }
+    kotlin.system.exitProcess(0)
+}
