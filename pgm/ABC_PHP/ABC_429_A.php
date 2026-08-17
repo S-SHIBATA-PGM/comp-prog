529b483feb1a7f112c0a9de9f35f643017766b4c
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-17 21:19:38 +0900

    Arrangement.

--- pgm/ABC_PHP/ABC_429_A.php
+++ pgm/ABC_PHP/ABC_429_A.php
@@ -5,8 +5,9 @@ fclose($stream);
 const OK = 'OK';
 const Too = 'Too Many Requests';
 const space = ' ';
+const one = 1;
 [$N, $M] = array_map('intval', explode(space, trim($line)));
-for ($i = 1; $i <= $N; $i++) {
+for ($i = one; $i <= $N; $i++) {
     echo ($i <= $M ? OK : Too) . PHP_EOL;
 }
 exit(0);
