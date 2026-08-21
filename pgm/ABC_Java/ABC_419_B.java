5605c9963bd570de2acc45a6ff253a2103775ca3
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-21 20:52:10 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_419_B.java
@@ -0,0 +1,73 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        FastScanner sc = new FastScanner();
+        final int Q = sc.nextInt();
+        final int one = 1;
+        final int two = 2;
+        Queue<Integer> pq = new PriorityQueue<>();
+        PrintWriter out =
+            new PrintWriter (new BufferedOutputStream (System.out));
+        for (int i = 0; i < Q; i++) {
+            int query = sc.nextInt();
+            if (query == one) {
+                int x = sc.nextInt();
+                pq.add (x);
+            } else if (query == two) {
+                out.println (pq.poll());
+            }
+        }
+        sc.close();
+        out.flush();
+        System.exit (0);
+    }
+    // FastScanner start
+    static class FastScanner {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+        public FastScanner() {
+            din = new DataInputStream (System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read (buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+    }
+    // FastScanner end
+}
