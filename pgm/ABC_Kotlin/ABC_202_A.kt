28f793565765a3d0b470e995df541e1327de1289
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-23 08:38:13 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_202_A.kt
@@ -0,0 +1,27 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.math.BigDecimal
+// import java.math.RoundingMode
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+// import java.util.TreeMap
+
+// import kotlin.math.*
+
+const val space: String = " "
+const val three: Int = 3
+const val seven: Int = 7
+
+fun main() {
+    val (a: Int, b: Int, c: Int) = readln().split(space).map { it.toInt() }
+    println(seven * three - a - b - c)
+    kotlin.system.exitProcess(0)
+}
