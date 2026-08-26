e4249f4024360d757e8fc00704089af72f023121
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-27 07:36:07 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_198_A.kt
@@ -0,0 +1,25 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.math.BigDecimal
+// import java.math.RoundingMode
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+// import java.util.TreeMap
+
+// import kotlin.math.*
+
+const val one: Int = 1
+
+fun main() {
+    val N: Int = readln().toInt()
+    println(N - one)
+    kotlin.system.exitProcess(0)
+}
