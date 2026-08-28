4e9fa7b4e4329a89c119414ce3a13f3ca0b79b06
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-28 21:41:54 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_412_B.java
@@ -0,0 +1,72 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        FastScanner sc = new FastScanner();
+        final String S = sc.nextString();
+        final String T = sc.nextString();
+        sc.close();
+        final String Yes = "Yes";
+        final String No = "No";
+        final int one = 1;
+        for (int i = one; i < S.length(); i++) {
+            char c = S.charAt (i);
+            if (Character.isUpperCase (c)) {
+                char pre = S.charAt (i - one);
+                if (T.indexOf (pre) == -one) {
+                    System.out.println (No);
+                    return;
+                }
+            }
+        }
+        System.out.println (Yes);
+        System.exit (0);
+    }
+    // FastScanner start
+    static class FastScanner {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+        public FastScanner() {
+            din = new DataInputStream (System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read (buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+        public String nextString() throws IOException {
+            byte c = read();
+            while (Character.isWhitespace (c)) {
+                c = read();
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.append ((char)c);
+            c = read();
+            while (!Character.isWhitespace (c)) {
+                builder.append ((char)c);
+                c = read();
+            }
+            return builder.toString();
+        }
+    }
+    // FastScanner end
+}
