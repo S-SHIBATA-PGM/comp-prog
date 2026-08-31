1b9b6513096b1e5faa026628c77cbd6cded9cafc
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-08-31 20:16:02 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_409_B.java
@@ -0,0 +1,74 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        FastScanner sc = new FastScanner();
+        final int N = sc.nextInt();
+        Integer[] A = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        sc.close();
+        final int one = 1;
+        final int zero = 0;
+        Arrays.sort (A, Collections.reverseOrder());
+        int num = zero;
+        for (int i = zero; i < N; i++) {
+            if (A[i] >= i + one) {
+                num = i + one;
+            } else {
+                break;
+            }
+        }
+        System.out.println (num);
+        System.exit (0);
+    }
+    // FastScanner start
+    static class FastScanner {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+        public FastScanner() {
+            din = new DataInputStream (System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read (buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+        public int nextInt() throws IOException {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+    }
+    // FastScanner end
+}
