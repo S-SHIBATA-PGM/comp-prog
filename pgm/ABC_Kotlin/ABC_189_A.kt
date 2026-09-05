c85ccf10f5d7e9e8fae581fccb419cf57df53db7
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-09-05 10:31:25 +0900

    Arrangement.

---
+++ pgm/ABC_Kotlin/ABC_189_A.kt
@@ -0,0 +1,31 @@
+// import java.io.BufferedOutputStream
+// import java.io.BufferedReader
+// import java.io.DataInputStream
+// import java.io.FileInputStream
+// import java.io.IOException
+// import java.io.InputStreamReader
+// import java.io.PrintWriter
+// import java.lang.StringBuilder
+// import java.math.BigDecimal
+// import java.math.RoundingMode
+// import java.time.LocalTime
+// import java.time.format.DateTimeFormatter
+// import java.util.Scanner
+// import java.util.StringTokenizer
+// import java.util.TreeMap
+
+// import kotlin.math.*
+
+const val Won: String = "Won"
+const val Lost: String = "Lost"
+const val one: Int = 1
+
+fun main() {
+    val C: String = readln()
+    if (C.toSet().size == one) {
+        println(Won)
+    } else {
+        println(Lost)
+    }
+    kotlin.system.exitProcess(0)
+}
