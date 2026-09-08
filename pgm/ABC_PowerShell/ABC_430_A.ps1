082b2e2731a8657172c2ea861c1d9484661eb892
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-09-09 07:38:50 +0900

    Arrangement.

---
+++ pgm/ABC_PowerShell/ABC_430_A.ps1
@@ -0,0 +1,10 @@
+$A, $B, $C, $D = [uint32[]][Console]::ReadLine().Split()
+Set-Variable -Name "Yes" -Value ([string]"Yes") -Option Constant
+Set-Variable -Name "No" -Value ([string]"No") -Option Constant
+if ($A -le $C -and $D -lt $B) {
+    Write-Host ($Yes)
+}
+else {
+    Write-Host ($No)
+}
+exit 0
