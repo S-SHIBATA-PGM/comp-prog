4bbc524fec21a06af9c25ee4cef620c17b3ba6ef
S-SHIBATA-PGM <ss.2436.gn.bd@gmail.com>
2026-09-10 07:47:48 +0900

    Arrangement.

---
+++ pgm/ABC_Java/ABC_400_B.java
@@ -0,0 +1,77 @@
+import java.io.*;
+// import java.math.*;
+// import java.time.*;
+// import java.util.*;
+// import java.util.Map.*;
+// import java.util.stream.*;
+
+public class Main {
+    public static void main (String[] args) throws Exception {
+        FastScanner sc = new FastScanner();
+        final long N = sc.nextLong();
+        final long M = sc.nextLong();
+        sc.close();
+        final String inf = "inf";
+        final long limit = 1000000000L;
+        final int one = 1;
+        long sum = one;
+        long term = one;
+        for (int i = one; i <= M; i++) {
+            if (term > limit / N) {
+                System.out.println (inf);
+                return;
+            }
+            term *= N;
+            sum += term;
+            if (sum > limit) {
+                System.out.println (inf);
+                return;
+            }
+        }
+        System.out.println (sum);
+        System.exit (0);
+    }
+    // FastScanner start
+    static class FastScanner {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+        public FastScanner() {
+            din = new DataInputStream (System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        private byte read() throws IOException {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+        public void close() throws IOException {
+            if (din == null)
+                return;
+            din.close();
+        }
+        private void fillBuffer() throws IOException {
+            bytesRead = din.read (buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+        public long nextLong() throws IOException {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+    }
+    // FastScanner end
+}
